87a22d01ed8df9e23f86b9e8dd0ae49f3dbaad88
StanislavSt <s.staev1@gmail.com>
2020-12-20 22:32:05 +0200

    feat(about.tsx): use custom namespaces

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,10 @@
 import AboutPage from 'components/about/AboutPage'
+import { GetStaticProps } from 'next'
+
+export const getStaticProps: GetStaticProps = async () => ({
+  props: {
+    i18nNamespaces: ['common', 'about'],
+  },
+})
 
 export default AboutPage
