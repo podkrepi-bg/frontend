83a05eb135c2632bbddad09df873a70c760ffca8
StanislavSt <s.staev1@gmail.com>
2020-12-23 00:10:38 +0200

    fix: allow automatic static optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
 import IndexPage from 'components/IndexPage'
+import { GetStaticProps } from 'next'
+import { getTranslations } from '../i18n'
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: {
+    i18nResources: await getTranslations(locale, ['common']),
+  },
+})
 
 export default IndexPage
