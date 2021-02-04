98cecd8c3eff14a2d632aac7519137efbda8cc01
StanislavSt <s.staev1@gmail.com>
2021-02-04 22:21:03 +0100

    feat: create AlertStore

---
+++ src/stores/AlertStore.ts
@@ -0,0 +1,50 @@
+import { action, computed, makeObservable, observable } from 'mobx'
+
+interface Alert {
+  id: number
+  show: boolean
+  type: AlertType
+  message: string
+  duration?: number
+}
+type AlertType = 'error' | 'warning' | 'info' | 'success'
+
+export class AlertStoreImpl {
+  alerts: Alert[] = []
+
+  constructor() {
+    makeObservable(this, {
+      alerts: observable,
+      show: action,
+      hide: action,
+      clear: action,
+      getAlerts: computed,
+    })
+  }
+
+  show(message: string, type: AlertType, duration = 3000) {
+    this.clear()
+    const alert: Alert = {
+      id: +new Date(),
+      show: true,
+      message,
+      type,
+      duration,
+    }
+    this.alerts.push(alert)
+  }
+
+  hide() {
+    if (this.alerts.length > 0) this.alerts[0].show = false
+  }
+
+  clear() {
+    this.alerts = []
+  }
+
+  get getAlerts() {
+    return this.alerts
+  }
+}
+
+export const AlertStore = new AlertStoreImpl()
