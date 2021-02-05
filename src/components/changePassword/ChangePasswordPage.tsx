4cdee0fb0d91b01a02f5e4b516dfc01c7fe9c561
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-02-05 21:12:00 +0200

    handle and validate forms on login, register, change-password and forgot-password pages

--- src/components/changePassword/ChangePasswordPage.tsx
+++ src/components/changePassword/ChangePasswordPage.tsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Container, Grid, TextField, Button } from '@material-ui/core'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 import Layout from 'components/layout/Layout'
 
 export default function ChangePasswordPage() {
   const { t } = useTranslation()
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
 
-  function handleSubmit(event: React.FormEvent) {
-    event.preventDefault()
-  }
+  const ChangePasswordSchema = yup.object().shape({
+    password: yup
+      .string()
+      .min(6, t('auth:validation.password-min', { count: 6 }))
+      .required(t('auth:validation.required')),
+    confirmPassword: yup
+      .string()
+      .required(t('auth:validation.required'))
+      .oneOf([yup.ref('password'), null], t('auth:validation.password-match')),
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: ChangePasswordSchema,
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: (values) => {
+      return
+    },
+  })
 
   return (
     <Layout title={t('nav.changePassword')}>
       <Container maxWidth="xs">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
               <TextField
@@ -26,8 +46,11 @@ export default function ChangePasswordPage() {
                 name="password"
                 size="small"
                 variant="outlined"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
+                error={!!formik.errors && !!formik.errors.password}
+                helperText={formik.errors && formik.errors.password}
+                value={formik.values.password}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -38,8 +61,11 @@ export default function ChangePasswordPage() {
                 name="confirmPassword"
                 size="small"
                 variant="outlined"
-                value={confirmPassword}
-                onChange={(event) => setConfirmPassword(event.target.value)}
+                error={!!formik.errors && !!formik.errors.confirmPassword}
+                helperText={formik.errors && formik.errors.confirmPassword}
+                value={formik.values.confirmPassword}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
               />
             </Grid>
             <Grid item xs={12}>
