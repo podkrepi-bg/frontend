40cbb1b13a54e431c8272eeb540825620b735717
StanislavSt <s.staev1@gmail.com>
2021-02-05 16:35:33 +0100

    fix(SnackBar): don't close on clickaway

--- src/components/layout/Snackbar.tsx
+++ src/components/layout/Snackbar.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import { Snackbar } from '@material-ui/core'
+import { Snackbar, SnackbarCloseReason } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
-import { AlertStore } from '../../stores/AlertStore'
+import { AlertStore } from 'stores/AlertStore'
 
 function SnackBar() {
   const { getAlerts } = AlertStore
 
+  const handleSnackBarClose = (_event: React.SyntheticEvent, reason: SnackbarCloseReason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    AlertStore.hide()
+  }
   const handleClose = () => AlertStore.hide()
-  const handleExited = () => AlertStore.clear()
 
   return (
     <>
       {getAlerts.map(({ id, show, duration, type, message }) => {
         return (
-          <Snackbar
-            key={id}
-            open={show}
-            autoHideDuration={duration}
-            onClose={handleClose}
-            onExited={handleExited}>
+          <Snackbar key={id} open={show} autoHideDuration={duration} onClose={handleSnackBarClose}>
             <Alert severity={type} onClose={handleClose}>
               {message}
             </Alert>
