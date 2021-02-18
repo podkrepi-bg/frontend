0693b0ec357b3ebb5ae6b48b6d0704b931be60eb
Ilko Kacharov <kachar136@gmail.com>
2021-02-18 13:47:08 +0200

    Use ssg on login page and redirect home if user is authenticated

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
-import { GetStaticProps } from 'next'
-import { providers } from 'next-auth/client'
+import { GetServerSideProps } from 'next'
+import { getSession, providers } from 'next-auth/client'
 
+import { routes } from 'common/routes'
 import LoginPage from 'components/auth/login/LoginPage'
 import { serverSideTranslations } from 'common/useNextLocale'
 
@@ -8,11 +9,20 @@ type UnboxPromise<T extends Promise<any>> = T extends Promise<infer U> ? U : nev
 
 export type LoginPageProps = { providers: UnboxPromise<ReturnType<typeof providers>> }
 
-export const getStaticProps: GetStaticProps<LoginPageProps> = async ({ locale }) => ({
-  props: {
-    i18nResources: await serverSideTranslations(locale, ['common', 'auth', 'validation']),
-    providers: await providers(),
-  },
-})
+export const getServerSideProps: GetServerSideProps<LoginPageProps> = async (ctx) => {
+  const session = await getSession(ctx)
+
+  if (session) {
+    ctx.res.statusCode = 302
+    ctx.res.setHeader('Location', routes.index)
+  }
+
+  return {
+    props: {
+      i18nResources: await serverSideTranslations(ctx.locale, ['common', 'auth', 'validation']),
+      providers: await providers(),
+    },
+  }
+}
 
 export default LoginPage
