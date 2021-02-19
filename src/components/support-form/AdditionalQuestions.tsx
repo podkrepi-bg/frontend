ee9372a7eeaebb20ec1e39960d1b5e832249c662
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-02-19 10:40:26 +0200

    init dynamic support form

---
+++ src/components/support-form/AdditionalQuestions.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AdditionalQuestions({ formik }: { formik: any }) {
+  return (
+    <div>
+      {Object.entries(formik.values.roles)
+        .filter(([_, value]) => value)
+        .map(([key, _]) => (
+          <h2 key={key}>{key}</h2>
+        ))}
+    </div>
+  )
+}
---
+++ src/components/support-form/StepperIcon.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx'
+import SettingsIcon from '@material-ui/icons/Settings'
+import GroupAddIcon from '@material-ui/icons/GroupAdd'
+import VideoLabelIcon from '@material-ui/icons/VideoLabel'
+import { StepIconProps } from '@material-ui/core/StepIcon'
+
+const useStepIconStyles = makeStyles({
+  root: {
+    backgroundColor: '#ccc',
+    zIndex: 1,
+    color: '#fff',
+    width: 50,
+    height: 50,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  active: {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+  },
+  completed: {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+  },
+})
+
+export default function StepIcon(props: StepIconProps) {
+  const classes = useStepIconStyles()
+  const { active, completed } = props
+
+  const icons: { [index: string]: React.ReactElement } = {
+    1: <SettingsIcon />,
+    2: <GroupAddIcon />,
+    3: <VideoLabelIcon />,
+    4: <VideoLabelIcon />,
+  }
+
+  return (
+    <div
+      className={clsx(classes.root, {
+        [classes.active]: active,
+        [classes.completed]: completed,
+      })}>
+      {icons[String(props.icon)]}
+    </div>
+  )
+}
