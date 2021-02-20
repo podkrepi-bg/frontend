7b91389255f295e4c29079c82961efe8a7fd7dd8
Ilko Kacharov <kachar136@gmail.com>
2021-02-20 07:27:15 +0200

    Upgrade theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,9 +10,10 @@ export default class CustomDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poiret+One&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
         <body>
