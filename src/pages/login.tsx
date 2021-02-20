f087470a037a0f2498fcf7bad41ecf6b449e3180
Ilko Kacharov <kachar136@gmail.com>
2021-02-20 11:08:35 +0200

    Trigger warning when using any as type

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,7 @@ import { routes } from 'common/routes'
 import LoginPage from 'components/auth/login/LoginPage'
 import { serverSideTranslations } from 'common/useNextLocale'
 
-type UnboxPromise<T extends Promise<any>> = T extends Promise<infer U> ? U : never
+type UnboxPromise<T extends Promise<unknown>> = T extends Promise<infer U> ? U : never
 
 export type LoginPageProps = { providers: UnboxPromise<ReturnType<typeof providers>> }
 
