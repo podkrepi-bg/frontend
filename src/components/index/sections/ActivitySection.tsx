ccaaf0d7431e1f5c0ec0476b1fc24ab9389fa9b3
Ani Kalpachka <ani.kalpachka@scalefocus.com>
2021-02-23 19:55:58 +0200

    fix homepage margins, icons, remove unnecessary textAlign properties

--- src/components/index/sections/ActivitySection.tsx
+++ src/components/index/sections/ActivitySection.tsx
@@ -1,14 +1,24 @@
 import { Grid, Typography, Box } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    heading: {
+      marginBottom: '40px',
+    },
+  }),
+)
 
 export default function ActivitySection() {
+  const classes = useStyles()
   const { t } = useTranslation()
 
   return (
     <Box component="section" mb={10} textAlign="center">
       <Grid container direction="column">
         <Grid item>
-          <Typography variant="h5" paragraph>
+          <Typography variant="h5" className={classes.heading}>
             {t('index:activity-section.heading')}
           </Typography>
           <Typography variant="subtitle1" paragraph>
--- src/components/index/sections/TeamSection.tsx
+++ src/components/index/sections/TeamSection.tsx
@@ -1,14 +1,24 @@
 import { Grid, Typography, Box } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    heading: {
+      marginBottom: '40px',
+    },
+  }),
+)
 
 export default function TeamSection() {
+  const classes = useStyles()
   const { t } = useTranslation()
 
   return (
     <Box component="section" mb={10} textAlign="center">
       <Grid container direction="column" alignItems="center" justify="center">
         <Grid item>
-          <Typography variant="h5" paragraph>
+          <Typography variant="h5" className={classes.heading}>
             {t('index:team-section.heading')}
           </Typography>
           <Typography variant="subtitle1" paragraph>
