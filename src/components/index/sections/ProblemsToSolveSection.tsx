3fcb47b6a231c62bd112d027ee0d1526913b912d
Ilko Kacharov <kachar136@gmail.com>
2021-02-23 10:16:45 +0200

    Fix warning for responsive props used on container

--- src/components/index/sections/ProblemsToSolveSection.tsx
+++ src/components/index/sections/ProblemsToSolveSection.tsx
@@ -9,7 +9,7 @@ import TimelinessIcon from '../../icons/problems-to-solve-icons/TimelinessIcon'
 const useStyles = makeStyles((theme) =>
   createStyles({
     icon: {
-      fontSize: theme.spacing(10)
+      fontSize: theme.spacing(10),
     },
   }),
 )
@@ -26,19 +26,19 @@ export default function SupportUsSection() {
       <Grid container direction="row">
         <Box display="flex" justifyContent="space-between" flexWrap="wrap" width="100%">
           <Box display="flex" alignItems="center" flexBasis="25%">
-            <CommissionsIcon className={classes.icon}/>
+            <CommissionsIcon className={classes.icon} />
             <Typography variant="subtitle1" paragraph>
               {t('index:problems-to-solve-section.high-commissions')}
             </Typography>
           </Box>
           <Box display="flex" alignItems="center" flexBasis="25%">
-            <TransparencyIcon className={classes.icon}/>
+            <TransparencyIcon className={classes.icon} />
             <Typography variant="subtitle1" paragraph>
               {t('index:problems-to-solve-section.low-transparency')}
             </Typography>
           </Box>
           <Box display="flex" alignItems="center" flexBasis="25%">
-            <TimelinessIcon className={classes.icon}/>
+            <TimelinessIcon className={classes.icon} />
             <Typography variant="subtitle1" paragraph>
               {t('index:problems-to-solve-section.timeliness')}
             </Typography>
