629041282107a363649e25db642d12b1f56ece57
Ilko Kacharov <kachar136@gmail.com>
2021-02-24 18:58:14 +0200

    Use ssg instead of ssp

--- src/pages/change-password.tsx
+++ src/pages/change-password.tsx
@@ -1,8 +1,8 @@
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'common/useNextLocale'
 import Page from 'components/auth/changePassword/ChangePasswordPage'
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
   props: {
     i18nResources: await serverSideTranslations(locale, ['common', 'auth', 'validation']),
   },
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,8 +1,8 @@
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import RegisterPage from 'components/auth/register/RegisterPage'
 import { serverSideTranslations } from 'common/useNextLocale'
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
   props: {
     i18nResources: await serverSideTranslations(locale, ['common', 'auth', 'validation']),
   },
