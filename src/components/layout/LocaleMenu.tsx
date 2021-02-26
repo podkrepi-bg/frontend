45f5650725b056ec5be29eea938b910f17c4349c
Ilko Kacharov <kachar136@gmail.com>
2021-02-26 23:47:07 +0200

    Add LocaleMenu to app bar

---
+++ src/components/layout/LocaleMenu.tsx
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/router'
+import { useCallback, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Button, Menu, MenuItem } from '@material-ui/core'
+
+export default function LocaleMenu() {
+  const router = useRouter()
+  const { t } = useTranslation()
+  const [anchorEl, setAnchorEl] = useState<Element | null>(null)
+
+  const handleMenu = (event: React.MouseEvent) => setAnchorEl(event.currentTarget)
+  const handleClose = () => setAnchorEl(null)
+  const changeLang = useCallback(
+    (locale: string) => (event: React.MouseEvent) => {
+      event.preventDefault()
+      // Same route different language
+      router.push(router.route, undefined, { locale })
+      setAnchorEl(null)
+    },
+    [],
+  )
+
+  if (!router.locale) {
+    return null
+  }
+
+  return (
+    <>
+      <Button variant="text" size="small" onClick={handleMenu}>
+        {router.locale}
+      </Button>
+      <Menu
+        open={Boolean(anchorEl)}
+        keepMounted
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}>
+        <MenuItem href={router.route} component="a" onClick={changeLang('bg')}>
+          {t('BG')}
+        </MenuItem>
+        <MenuItem href={router.route} component="a" onClick={changeLang('en')}>
+          {t('EN')}
+        </MenuItem>
+      </Menu>
+    </>
+  )
+}
--- src/components/layout/nav/PublicMenu.tsx
+++ src/components/layout/nav/PublicMenu.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
 
-import LinkButton from 'components/common/LinkButton'
 import { routes } from 'common/routes'
+import LinkButton from 'components/common/LinkButton'
+import LocaleMenu from 'components/layout/LocaleMenu'
 
 export default function PublicMenu() {
   const { t } = useTranslation()
   return (
     <Grid container justify="flex-end" direction="row" wrap="nowrap" spacing={2}>
+      <Grid item>
+        <LocaleMenu />
+      </Grid>
       <Grid item>
         <LinkButton variant="text" href={routes.login}>
           {t('nav.login')}
