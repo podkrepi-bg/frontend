1f22f889b4e76b738e85a7d7a43edf5e4e20462a
Ilko Kacharov <kachar136@gmail.com>
2021-02-27 11:02:19 +0200

    Update color schemes of team pie chart
    Via https://coolors.co/f94144-f3722c-f8961e-f9844a-f9c74f-90be6d-43aa8b-4d908e-577590-277da1

--- src/components/chart/pieData.ts
+++ src/components/chart/pieData.ts
@@ -3,18 +3,18 @@ import { Options } from 'highcharts'
 export type TeamPieItem = { id: string; y: number; color: string; name?: string }
 
 export const teamPieData: Array<TeamPieItem> = [
-  { id: 'back-end', y: 19.8, color: 'rgb(243, 88, 115)' },
-  { id: 'frontend', y: 12.1, color: 'rgb(155, 110, 160)' },
-  { id: 'full-stack', y: 11.1, color: 'rgb(112, 131, 173)' },
-  { id: 'dev-ops', y: 10.6, color: 'rgb(155, 108, 229)' },
-  { id: 'qa', y: 8.3, color: 'rgb(192, 152, 130)' },
-  { id: 'management', y: 12.0, color: 'rgb(13, 228, 97)' },
-  { id: 'marketing', y: 9.8, color: 'rgb(190, 216, 24)' },
-  { id: 'ngo', y: 5.0, color: 'rgb(109, 173, 41)' },
-  { id: 'design', y: 4.7, color: 'rgb(255, 164, 230)' },
-  { id: 'law', y: 2.2, color: 'rgb(242, 222, 208)' },
-  { id: 'accounting', y: 2.2, color: 'rgb(209, 200, 185)' },
-  { id: 'others', y: 2.2, color: 'rgb(243, 186, 181)' },
+  { id: 'others', y: 2.2, color: '#F94144' },
+  { id: 'law', y: 2.2, color: '#F3722C' },
+  { id: 'accounting', y: 2.2, color: '#F8961E' },
+  { id: 'design', y: 4.7, color: '#F9844A' },
+  { id: 'ngo', y: 5.0, color: '#F9C74F' },
+  { id: 'qa', y: 8.3, color: '#90BE6D' },
+  { id: 'marketing', y: 9.8, color: '#43AA8B' },
+  { id: 'dev-ops', y: 10.6, color: '#4D908E' },
+  { id: 'full-stack', y: 11.1, color: '#577590' },
+  { id: 'management', y: 12.0, color: '#277DA1' },
+  { id: 'frontend', y: 12.1, color: '#219EBC' },
+  { id: 'back-end', y: 19.8, color: '#023047' },
 ]
 
 export const teamPieOptions: Options = {
