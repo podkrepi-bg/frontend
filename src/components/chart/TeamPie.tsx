a0e515e647661fe71f001fb24f850c01c40c3abc
Nikola Andreev <Nikola.Andreev@netcetera.com>
2021-02-27 08:23:28 +0100

    [75] Replace nivo pie with highcharts pie

--- src/components/chart/TeamPie.tsx
+++ src/components/chart/TeamPie.tsx
@@ -1,70 +1,28 @@
 import React from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import { teamPieData, teamPieOptions, TeamPieItem } from 'components/chart/pieData'
 import { useTranslation } from 'react-i18next'
-import { ResponsivePie } from '@nivo/pie'
-import { teamPieData, TeamPieData } from 'components/chart/pieData'
+import handleViewport from 'react-in-viewport'
 
-const styles = {
-  height: '380px',
-  width: '99%',
-  paddingTop: '20px',
+export type TeamPieProps = {
+  inViewport: boolean
+  forwardedRef: React.RefObject<HTMLDivElement>
+  enterCount: number
 }
 
-const TeamPie = () => {
+const TeamPie = ({ inViewport, forwardedRef, enterCount }: TeamPieProps) => {
   const { t } = useTranslation()
-  teamPieData.forEach((item: TeamPieData): void => {
-    item.label = t(`index:team-chart-section.data.${item.id}`)
+  teamPieData.forEach((item: TeamPieItem): void => {
+    item.name = t(`index:team-chart-section.data.${item.id}`)
   })
+  const runAnimation: boolean = inViewport || enterCount > 0
 
   return (
-    <div style={styles}>
-      <ResponsivePie
-        data={teamPieData}
-        margin={{ top: 30, right: 100, bottom: 30, left: 100 }}
-        innerRadius={0.4}
-        padAngle={0.7}
-        radialLabelsLinkDiagonalLength={20}
-        radialLabelsLinkHorizontalLength={5}
-        sliceLabelsRadiusOffset={0.65}
-        colors={{ scheme: 'set2' }}
-        radialLabel="label"
-        isInteractive={false}
-        defs={[
-          {
-            id: 'dots',
-            type: 'patternDots',
-            background: 'inherit',
-            color: 'rgba(255, 255, 255, 0.3)',
-            size: 4,
-            padding: 1,
-            stagger: true,
-          },
-          {
-            id: 'lines',
-            type: 'patternLines',
-            background: 'inherit',
-            color: 'rgba(255, 255, 255, 0.3)',
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10,
-          },
-        ]}
-        fill={[
-          { match: { id: 'back-end' }, id: 'dots' },
-          { match: { id: 'frontend' }, id: 'lines' },
-          { match: { id: 'full-stack' }, id: 'dots' },
-          { match: { id: 'dev-ops' }, id: 'lines' },
-          { match: { id: 'qa' }, id: 'dots' },
-          { match: { id: 'ngo' }, id: 'lines' },
-          { match: { id: 'design' }, id: 'dots' },
-          { match: { id: 'law' }, id: 'lines' },
-          { match: { id: 'management' }, id: 'dots' },
-          { match: { id: 'accounting' }, id: 'lines' },
-          { match: { id: 'marketing' }, id: 'dots' },
-          { match: { id: 'others' }, id: 'lines' },
-        ]}
-      />
+    <div ref={forwardedRef}>
+      {runAnimation && <HighchartsReact highcharts={Highcharts} options={teamPieOptions} />}
     </div>
   )
 }
 
-export default TeamPie
+export default handleViewport(TeamPie, {}, { disconnectOnLeave: true })
--- src/components/chart/pieData.ts
+++ src/components/chart/pieData.ts
@@ -1,18 +1,52 @@
-import { DefaultRawDatum } from '@nivo/pie'
+import { Options } from 'highcharts'
 
-export type TeamPieData = DefaultRawDatum & { label?: string }
+export type TeamPieItem = { id: string; y: number; color: string; name?: string }
 
-export const teamPieData: TeamPieData[] = [
-  { id: 'back-end', value: 19.8 },
-  { id: 'frontend', value: 12.1 },
-  { id: 'full-stack', value: 11.1 },
-  { id: 'dev-ops', value: 10.6 },
-  { id: 'qa', value: 8.3 },
-  { id: 'ngo', value: 5.0 },
-  { id: 'design', value: 4.7 },
-  { id: 'law', value: 2.2 },
-  { id: 'management', value: 12.0 },
-  { id: 'accounting', value: 2.2 },
-  { id: 'marketing', value: 9.8 },
-  { id: 'others', value: 2.2 },
+export const teamPieData: Array<TeamPieItem> = [
+  { id: 'back-end', y: 19.8, color: 'rgb(243, 88, 115)' },
+  { id: 'frontend', y: 12.1, color: 'rgb(155, 110, 160)' },
+  { id: 'full-stack', y: 11.1, color: 'rgb(112, 131, 173)' },
+  { id: 'dev-ops', y: 10.6, color: 'rgb(155, 108, 229)' },
+  { id: 'qa', y: 8.3, color: 'rgb(192, 152, 130)' },
+  { id: 'management', y: 12.0, color: 'rgb(13, 228, 97)' },
+  { id: 'marketing', y: 9.8, color: 'rgb(190, 216, 24)' },
+  { id: 'ngo', y: 5.0, color: 'rgb(109, 173, 41)' },
+  { id: 'design', y: 4.7, color: 'rgb(255, 164, 230)' },
+  { id: 'law', y: 2.2, color: 'rgb(242, 222, 208)' },
+  { id: 'accounting', y: 2.2, color: 'rgb(209, 200, 185)' },
+  { id: 'others', y: 2.2, color: 'rgb(243, 186, 181)' },
 ]
+
+export const teamPieOptions: Options = {
+  chart: {
+    type: 'pie',
+    backgroundColor: '',
+  },
+  credits: {
+    enabled: false,
+  },
+  title: {
+    text: '',
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+      },
+      innerSize: '35%',
+    },
+  },
+  series: [
+    {
+      type: 'pie',
+      name: '',
+      data: teamPieData,
+      animation: {
+        duration: 2000,
+      },
+    },
+  ],
+}
