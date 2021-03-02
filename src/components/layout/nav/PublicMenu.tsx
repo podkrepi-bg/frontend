613987fe0a2b185bc33abbeb9f7d6205e64fdaf9
Ilko Kacharov <kachar136@gmail.com>
2021-03-02 13:48:17 +0200

    Added static language button and remove auth buttons
    Closes #85 and #88

--- src/components/layout/nav/PublicMenu.tsx
+++ src/components/layout/nav/PublicMenu.tsx
@@ -5,8 +5,11 @@ import { useTranslation } from 'react-i18next'
 import { routes } from 'common/routes'
 import LinkButton from 'components/common/LinkButton'
 
-export default function PublicMenu() {
+export default function PublicMenu({ disableAuth = false }: { disableAuth?: boolean }) {
   const { t } = useTranslation()
+  if (disableAuth) {
+    return null
+  }
   return (
     <>
       <Grid item>
