3d8015259fc24d8420afd3dc2744ac83e6b3b234
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-03-06 22:10:18 +0200

    Add spacing between columns

--- src/components/about-project/sections/WhatIsDone.tsx
+++ src/components/about-project/sections/WhatIsDone.tsx
@@ -34,6 +34,7 @@ const useStyles = makeStyles((theme: Theme) =>
     checkedLine: {
       display: 'flex',
       marginBottom: theme.spacing(1.5),
+      marginRight: theme.spacing(2),
     },
   }),
 )
