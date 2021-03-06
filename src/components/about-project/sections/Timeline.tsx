bb83a78359da6da8872c16d0f240a336d7a5ae8a
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-03-06 21:34:23 +0200

    Add sections Timeline and SelfFinancing

--- src/components/about-project/sections/Timeline.tsx
+++ src/components/about-project/sections/Timeline.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core'
+import { Timeline as TimelineMaterial } from '@material-ui/lab'
+import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite'
+import CodeIcon from '@material-ui/icons/Code'
+import TelegramIcon from '@material-ui/icons/Telegram'
+import UpdateIcon from '@material-ui/icons/Update'
+
+import TimelineItem from './TimelineItem'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     heading: {
       marginBottom: theme.spacing(5),
       color: theme.palette.primary.dark,
+      fontSize: theme.spacing(5),
     },
     container: {
       marginBottom: theme.spacing(12),
       textAlign: 'center',
+    },
+    content: {
+      fontSize: theme.spacing(2.5),
+      maxWidth: theme.spacing(20),
     },
   }),
 )
@@ -18,13 +30,35 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function Timeline() {
   const { t } = useTranslation()
   const classes = useStyles()
+
   return (
     <Grid container direction="column" component="section" className={classes.container}>
-      <Typography variant="h5" component="h2" className={classes.heading}>
-        {'Какво предстои'}
+      <Typography variant="h4" component="h2" className={classes.heading}>
+        {t('about-project:timeline')}
       </Typography>
       <Grid item>
-        <Typography variant="body2">{'Timeline:'}</Typography>
+        <TimelineMaterial align="alternate">
+          <TimelineItem Icon={CodeIcon}>
+            <Typography variant="body2" component="span" className={classes.content}>
+              {t('about-project:hackaton')}
+            </Typography>
+          </TimelineItem>
+          <TimelineItem Icon={PlayCircleFilledWhiteIcon}>
+            <Typography variant="body2" component="span" className={classes.content}>
+              {t('about-project:mvpLaunch')}
+            </Typography>
+          </TimelineItem>
+          <TimelineItem Icon={UpdateIcon}>
+            <Typography variant="body2" component="span" className={classes.content}>
+              {t('about-project:betaLaunch')}
+            </Typography>
+          </TimelineItem>
+          <TimelineItem Icon={TelegramIcon} lastItem={true}>
+            <Typography variant="body2" component="span" className={classes.content}>
+              {t('about-project:officialLaunch')}
+            </Typography>
+          </TimelineItem>
+        </TimelineMaterial>
       </Grid>
     </Grid>
   )
