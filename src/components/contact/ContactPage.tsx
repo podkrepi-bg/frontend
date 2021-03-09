222210aa768ce367e0104627425cf3d6d141da9d
Ilko Kacharov <kachar136@gmail.com>
2021-03-10 00:43:35 +0200

    Make sure contact form is max sm

--- src/components/contact/ContactPage.tsx
+++ src/components/contact/ContactPage.tsx
@@ -13,6 +13,8 @@ export default function RegisterPage() {
     <Layout title={t('nav.contacts')}>
       <Container maxWidth="md">
         <ContactInfo />
+      </Container>
+      <Container maxWidth="sm">
         <ContactForm />
       </Container>
     </Layout>
