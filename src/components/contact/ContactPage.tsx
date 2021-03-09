b3aeb058335f874f9cd2baee272f4b3088a61891
Ani Kalpachka <ani.kalpachka@scalefocus.com>
2021-03-09 21:19:22 +0200

    Add Contact page styles

--- src/components/contact/ContactPage.tsx
+++ src/components/contact/ContactPage.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { Container } from '@material-ui/core'
 
 import Layout from 'components/layout/Layout'
+import ContactInfo from 'components/contact/ContactInfo'
 import ContactForm from 'components/contact/ContactForm'
 
 export default function RegisterPage() {
@@ -10,7 +11,8 @@ export default function RegisterPage() {
 
   return (
     <Layout title={t('nav.contacts')}>
-      <Container maxWidth="xs">
+      <Container maxWidth="md">
+        <ContactInfo />
         <ContactForm />
       </Container>
     </Layout>
