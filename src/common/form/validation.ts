862b4e0d07729709a4e15b233f0d0481994391a6
Ilko Kacharov <kachar136@gmail.com>
2021-03-10 17:21:03 +0200

    Lower the limit of chars of name validator
    Closes #108

--- src/common/form/validation.ts
+++ src/common/form/validation.ts
@@ -59,4 +59,4 @@ setLocale({
 export const phoneRegex = /^\+?\d+$/
 export const noNumbersRegex = /^[^\d!@#$%^&*()\\/'"_-]*$/gi
 export const phone = string().trim().matches(phoneRegex, customValidators.phone).min(10).max(25)
-export const name = string().trim().matches(noNumbersRegex, customValidators.name).min(5).max(50)
+export const name = string().trim().matches(noNumbersRegex, customValidators.name).min(2).max(50)
