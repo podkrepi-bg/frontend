811a1676059d8a3e26c35d66a2d8a225f71c0c3e
Ani Kalpachka <ani.kalpachka@scalefocus.com>
2021-03-10 20:25:22 +0200

    apply requested changes on the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,20 +6,12 @@ import { useTranslation } from 'react-i18next'
 import PodkrepiLogo from 'components/brand/PodkrepiLogo'
 import { routes } from 'common/routes'
 
-import { Instagram } from '@material-ui/icons'
-import { Facebook } from '@material-ui/icons'
-import { Twitter } from '@material-ui/icons'
-import { LinkedIn } from '@material-ui/icons'
-import { YouTube } from '@material-ui/icons'
+import { Instagram, Facebook, Twitter, LinkedIn, YouTube } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     footer: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      paddingTop: theme.spacing(11),
-      paddingBottom: theme.spacing(7),
+      padding: theme.spacing(7),
       backgroundColor: theme.palette.primary.dark,
     },
     footer__left: {
@@ -44,7 +36,7 @@ const useStyles = makeStyles((theme) =>
       color: theme.palette.primary.main,
     },
     footer__link: {
-      fontSize: '18px',
+      fontSize: theme.spacing(2),
       padding: theme.spacing(0.5, 0, 0.5, 0),
     },
     footer__link__inner: {
@@ -57,37 +49,37 @@ const footerItems = [
   [
     {
       label: 'footerLabels.aboutUs',
-      href: 'footerLinks.aboutUs',
-      external: true,
+      href: routes.about,
+      external: false,
     },
     {
       label: 'footerLabels.aboutProject',
-      href: 'footerLinks.aboutProject',
-      external: true,
+      href: routes.aboutProject,
+      external: false,
     },
   ],
   [
     {
       label: 'footerLabels.contact',
-      href: 'footerLinks.contact',
-      external: true,
+      href: routes.contact,
+      external: false,
     },
     {
       label: 'footerLabels.support',
-      href: 'footerLinks.support',
-      external: true,
+      href: routes.support,
+      external: false,
     },
     {
       label: 'footerLabels.terms',
-      href: 'footerLinks.terms',
-      external: true,
+      href: routes.terms,
+      external: false,
     },
   ],
   [
     {
       label: 'footerLabels.gdpr',
-      href: 'footerLinks.gdpr',
-      external: true,
+      href: routes.gdpr,
+      external: false,
     },
   ],
 ]
@@ -107,69 +99,61 @@ export default function Footer() {
   const { locale } = useRouter()
   const classes = useStyles()
   return (
-    <footer className={classes.footer}>
-      <Container>
-        <div>
-          <Grid container>
-            <Grid item xs={6}>
-              <div className={classes.footer__left}>
-                <Link href={routes.index}>
-                  <a className={classes.footer__logo}>
-                    <PodkrepiLogo locale={locale} size="large" variant="fixed" />
-                  </a>
-                </Link>
-
-                <div className={classes.footer__socialContainer}>
-                  <Link href={socialLinks.facebook}>
-                    <a target="_blank">
-                      <Facebook className={classes.footer__socialLink} />
+    <Container component="footer" className={classes.footer} maxWidth="xl">
+      <Grid container>
+        <Grid item xs={6}>
+          <div className={classes.footer__left}>
+            <Link href={routes.index}>
+              <a className={classes.footer__logo}>
+                <PodkrepiLogo locale={locale} size="large" variant="fixed" />
+              </a>
+            </Link>
+            <div className={classes.footer__socialContainer}>
+              <Link href={socialLinks.facebook}>
+                <a target="_blank">
+                  <Facebook className={classes.footer__socialLink} />
+                </a>
+              </Link>
+              <Link href={socialLinks.twitter}>
+                <a target="_blank">
+                  <Twitter className={classes.footer__socialLink} />
+                </a>
+              </Link>
+              <Link href={socialLinks.linkdin}>
+                <a target="_blank">
+                  <LinkedIn className={classes.footer__socialLink} />
+                </a>
+              </Link>
+              <Link href={socialLinks.youtube}>
+                <a target="_blank">
+                  <YouTube className={classes.footer__socialLink} />
+                </a>
+              </Link>
+              <Link href={socialLinks.instagram}>
+                <a target="_blank">
+                  <Instagram className={classes.footer__socialLink} />
+                </a>
+              </Link>
+            </div>
+            <span className={classes.footer__copyrights}>{t('footerCopyrights')}</span>
+          </div>
+        </Grid>
+        {footerItems.map((subarr, i) => (
+          <Grid key={Math.random() * i} item xs={2}>
+            <ul>
+              {subarr.map(({ label, href, external }, j) => (
+                <li key={Math.random() * j} className={classes.footer__link}>
+                  <Link href={t(href)}>
+                    <a target={external ? '_blank' : ''} className={classes.footer__link__inner}>
+                      {t(label)}
                     </a>
                   </Link>
-                  <Link href={socialLinks.twitter}>
-                    <a target="_blank">
-                      <Twitter className={classes.footer__socialLink} />
-                    </a>
-                  </Link>
-                  <Link href={socialLinks.linkdin}>
-                    <a target="_blank">
-                      <LinkedIn className={classes.footer__socialLink} />
-                    </a>
-                  </Link>
-                  <Link href={socialLinks.youtube}>
-                    <a target="_blank">
-                      <YouTube className={classes.footer__socialLink} />
-                    </a>
-                  </Link>
-                  <Link href={socialLinks.instagram}>
-                    <a target="_blank">
-                      <Instagram className={classes.footer__socialLink} />
-                    </a>
-                  </Link>
-                </div>
-
-                <span className={classes.footer__copyrights}>{t('footerCopyrights')}</span>
-              </div>
-            </Grid>
-            {footerItems.map((subarr, i) => (
-              <Grid key={Math.random() * i} item xs={2}>
-                <ul>
-                  {subarr.map(({ label, href, external }, j) => (
-                    <li key={Math.random() * j} className={classes.footer__link}>
-                      <Link href={t(href)}>
-                        <a
-                          target={external ? '_blank' : ''}
-                          className={classes.footer__link__inner}>
-                          {t(label)}
-                        </a>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </Grid>
-            ))}
+                </li>
+              ))}
+            </ul>
           </Grid>
-        </div>
-      </Container>
-    </footer>
+        ))}
+      </Grid>
+    </Container>
   )
 }
