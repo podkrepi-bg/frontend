91a5871eec8c5146d566356022c3be5c587aab0b
Ilko Kacharov <kachar136@gmail.com>
2021-03-12 16:04:11 +0200

    Rework forms so they use formik context better
    Closes #113
    Closes #102

--- src/components/common/form/FormTextField.tsx
+++ src/components/common/form/FormTextField.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { FormikValues } from 'formik'
-import { TextField, TextFieldProps } from '@material-ui/core'
+import { useField } from 'formik'
 import { useTranslation } from 'next-i18next'
+import { TextField, TextFieldProps } from '@material-ui/core'
 
 import { translateError } from 'common/form/useForm'
 import { TranslatableField } from 'common/form/validation'
-import { useFormContext } from 'components/common/form/FormContext'
 
 export type RegisterFormProps = {
   type: string
@@ -13,29 +12,20 @@ export type RegisterFormProps = {
   name: string
 } & TextFieldProps
 
-export default function FormTextField<T extends FormikValues>({
-  label,
-  name,
-  ...textFieldProps
-}: RegisterFormProps) {
+export default function FormTextField({ label, name, ...textFieldProps }: RegisterFormProps) {
   const { t } = useTranslation()
-  const { formik } = useFormContext<T>()
-  const helperText = formik.touched[name]
-    ? translateError(formik.errors[name] as TranslatableField, t)
-    : ''
+  const [field, meta] = useField(name)
+  const helperText = meta.touched ? translateError(meta.error as TranslatableField, t) : ''
   return (
     <TextField
       fullWidth
       label={t(label)}
-      name={name}
       size="small"
       variant="outlined"
-      error={Boolean(formik.errors[name]) && Boolean(formik.touched[name])}
+      error={Boolean(meta.error) && Boolean(meta.touched)}
       helperText={helperText}
-      value={formik.values[name]}
-      onBlur={formik.handleBlur}
-      onChange={formik.handleChange}
       {...textFieldProps}
+      {...field}
     />
   )
 }
