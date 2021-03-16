dda46f6c5b6b5c125720c5d06f650136c99badae
Ilko Kacharov <kachar136@gmail.com>
2021-03-16 02:13:29 +0200

    Refactor go rest api so it uses config for db conn with ssl

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,9 +1,49 @@
 package database
 
 import (
+	"strconv"
+
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var (
-	DBConn *gorm.DB
-)
+type DatabaseConfig struct {
+	Host     string
+	Username string
+	Password string
+	Port     int
+	Database string
+	RootCert string
+	Cert     string
+	Key      string
+}
+
+type Database struct {
+	*gorm.DB
+}
+
+func New(config *DatabaseConfig) (*Database, error) {
+	var db *gorm.DB
+	var err error
+	dsn := "host=" + config.Host +
+		" user=" + config.Username +
+		" password=" + config.Password +
+		" dbname=" + config.Database +
+		" port=" + strconv.Itoa(config.Port) +
+		" TimeZone=UTC connect_timeout=0" +
+		" sslmode=verify-ca" +
+		" sslrootcert=" + config.RootCert +
+		" sslcert=" + config.Cert +
+		" sslkey=" + config.Key
+	println(dsn)
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return &Database{db}, err
+}
+
+type BaseHandler struct {
+	db *gorm.DB
+}
+
+func NewBaseHandler(db *gorm.DB) *BaseHandler {
+	return &BaseHandler{db: db}
+}
