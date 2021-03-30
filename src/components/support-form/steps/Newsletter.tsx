be87782f0b02000b5feb8afae285af6ae2d840cf
Dimitar <dimitar.nizamov@klevret.com>
2021-03-31 00:55:25 +0300

    Added a modal popup to double confirm newsletter option

--- src/components/support-form/steps/Newsletter.tsx
+++ src/components/support-form/steps/Newsletter.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'next-i18next'
 import { FormControl, FormLabel } from '@material-ui/core'
 import CheckboxField from 'components/common/form/CheckboxField'
+import ConfirmationDialog from 'components/common/ConfirmationDialog'
 
 export default function Newsletter() {
   const { t } = useTranslation()
 
+  const [isOpen, updateIsOpen] = useState(false)
+
+  const [isChecked, updateIsChecked] = useState(false)
+
+  const handleCancel = () => {
+    updateIsOpen(false)
+    updateIsChecked(false)
+  }
+
+  const handleConfirm = () => {
+    updateIsOpen(false)
+    updateIsChecked(true)
+  }
+
+  const handleClick = () => {
+    updateIsOpen(true)
+  }
+
   return (
     <FormControl component="fieldset">
       <FormLabel component="legend">{t('common:support-form.steps.newsletter.title')}</FormLabel>
-      <CheckboxField label="common:support-form.steps.newsletter.label" name="newsletter" />
+      <CheckboxField
+        label={t('common:support-form.steps.newsletter.label')}
+        name="newsletter"
+        preChecked={isChecked}
+        onClick={handleClick}
+      />
+      <ConfirmationDialog
+        title={t('common:support-form.steps.newsletter.confirm.title')}
+        content={t('common:support-form.steps.newsletter.confirm.content')}
+        confirmButtonLabel={t('common:support-form.steps.newsletter.confirm.confirmButtonLabel')}
+        cancelButtonLabel={t('common:support-form.steps.newsletter.confirm.cancelButtonLabel')}
+        handleConfirm={handleConfirm}
+        handleCancel={handleCancel}
+        isOpen={isOpen}></ConfirmationDialog>
     </FormControl>
   )
 }
