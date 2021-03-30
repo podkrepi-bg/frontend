238e8efc8e1f17a8a7c7549c63b1c686930b9083
Dimitar <dimitar.nizamov@klevret.com>
2021-03-31 00:55:25 +0300

    Added a modal popup to double confirm newsletter option

--- src/components/common/form/CheckboxField.tsx
+++ src/components/common/form/CheckboxField.tsx
@@ -3,21 +3,26 @@ import { useField } from 'formik'
 import { useTranslation } from 'react-i18next'
 
 import { TranslatableField, translateError } from 'common/form/validation'
+import { MouseEventHandler } from 'react'
 
-export type AcceptTermsFieldProps = {
+export type CheckboxFieldProps = {
   name: string
   label: string
+  onClick: MouseEventHandler
+  preChecked?: boolean
 }
 
-export default function CheckboxField({ name, label }: AcceptTermsFieldProps) {
+export default function CheckboxField({ name, label, preChecked, onClick }: CheckboxFieldProps) {
   const { t } = useTranslation()
   const [field, meta] = useField(name)
   const helperText = meta.touched ? translateError(meta.error as TranslatableField, t) : ''
+  const checked =
+    preChecked === undefined ? Boolean(field.value) : Boolean(field.value && preChecked)
   return (
     <FormControl required component="fieldset" error={Boolean(meta.error) && Boolean(meta.touched)}>
       <FormControlLabel
         label={typeof label === 'string' ? t(label) : label}
-        control={<Checkbox color="primary" checked={Boolean(field.value)} {...field} />}
+        control={<Checkbox color="primary" onClick={onClick} checked={checked} {...field} />}
       />
       {Boolean(meta.error) && <FormHelperText error>{helperText}</FormHelperText>}
     </FormControl>
