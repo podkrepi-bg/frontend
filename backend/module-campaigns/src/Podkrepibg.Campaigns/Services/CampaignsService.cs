cf8bd99280fb4e20d32d07684d617db84b5d01cc
Diana Karcheva <dianakarcheva@hotmail.com>
2021-04-01 16:41:39 +0300

    DB Layer; Extened proto files

--- backend/module-campaigns/src/Podkrepibg.Campaigns/Services/CampaignsService.cs
+++ backend/module-campaigns/src/Podkrepibg.Campaigns/Services/CampaignsService.cs
@@ -1,26 +1,46 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
+using Podkrepibg.Campaigns.Data;
+using Podkrepibg.Nomenclatures;
 
-namespace Podkrepibg.Campaigns
+namespace Podkrepibg.Campaigns.Services
 {
-    public class CampaignsService : Campaigns.CampaignsBase
-    {
-        private readonly ILogger<CampaignsService> _logger;
-        public CampaignsService(ILogger<CampaignsService> logger)
-        {
-            _logger = logger;
-        }
+  public class CampaignsService : Campaigns.CampaignsBase
+  {
+    private readonly ILogger<CampaignsService> _logger;
+    private readonly CampaignsContext _db;
 
-        public override Task<CreateCampaignResponse> CreateCampaign(CreateCampaignRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(
-              new CreateCampaignResponse
-              {
-                  CampaignId = Guid.NewGuid().ToString(),
-                  ErrorCode = 0
-              });
-        }
+    public CampaignsService(ILogger<CampaignsService> logger, CampaignsContext db)
+    {
+      _logger = logger;
+      _db = db;
     }
+
+    public override Task<NewCampaignReply> NewCampaign(CreateCampaignRequest request, ServerCallContext context)
+    {
+      return Task.FromResult(
+        new NewCampaignReply
+        {
+          BeneficiaryId = request.BeneficiaryId,
+          CampaignId = Guid.NewGuid().ToString(),
+          InitiatorId = request.InitiatorId,
+          OperatorId = request.OperatorId,
+          State = CampaignState.Draft
+        });
+    }
+
+    public override Task<CampaignTypesReply> CampaignTypes(Empty request, ServerCallContext context)
+    {
+      return base.CampaignTypes(request, context);
+    }
+
+    public override Task<FilterCampaignsReply> FilterCampaigns(FilterCampaignsRequest request, ServerCallContext context)
+    {
+      return base.FilterCampaigns(request, context);
+    }
+  }
 }
