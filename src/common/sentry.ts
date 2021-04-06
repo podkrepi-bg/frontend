267778c67179032ad50a449f1f39cac9af2472a8
Ilko Kacharov <kachar136@gmail.com>
2021-04-06 11:49:35 +0300

    Clean up and enable sentry only in pre-build node env

--- src/common/sentry.ts
+++ src/common/sentry.ts
@@ -3,16 +3,6 @@ import { RewriteFrames } from '@sentry/integrations'
 
 // https://github.com/vercel/next.js/tree/canary/examples/with-sentry
 export const initSentry = () => {
-  console.log('INIT SENTRY 1')
-  console.log({
-    NODE_ENV: process.env.NODE_ENV,
-    SENTRY_DSN: process.env.SENTRY_DSN,
-    DEPLOY_TAG: process.env.DEPLOY_TAG,
-    NEXT_IS_SERVER: process.env.NEXT_IS_SERVER,
-    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    NEXT_PUBLIC_DEPLOY_TAG: process.env.NEXT_PUBLIC_DEPLOY_TAG,
-    NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR: process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
-  })
   if (process.env.SENTRY_DSN) {
     const integrations = []
     if (process.env.NEXT_IS_SERVER === 'true' && process.env.SENTRY_SERVER_ROOT_DIR) {
@@ -32,9 +22,8 @@ export const initSentry = () => {
         }),
       )
     }
-    console.log('INIT SENTRY 2')
     Sentry.init({
-      enabled: true, // process.env.NODE_ENV === 'production',
+      enabled: process.env.NODE_ENV === 'production',
       integrations,
       dsn: process.env.SENTRY_DSN,
       release: process.env.DEPLOY_TAG,
