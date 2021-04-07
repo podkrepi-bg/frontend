558431146f5763f4ffbd431671b9c3da1a31994b
Ani Kalpachka <ani_kalpachka888@abv.bg>
2021-04-07 09:58:33 +0300

    Change homepage design

--- src/components/index/sections/Jumbotron.tsx
+++ src/components/index/sections/Jumbotron.tsx
@@ -1,11 +1,8 @@
 import React, { RefObject } from 'react'
-
 import { useTranslation } from 'react-i18next'
-
 import { Grid, Typography } from '@material-ui/core'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
-
 import { routes } from 'common/routes'
 import LinkButton from 'components/common/LinkButton'
 import Typewriter from '../helpers/Typewriter'
@@ -13,17 +10,19 @@ import Typewriter from '../helpers/Typewriter'
 const useStyles = makeStyles((theme) =>
   createStyles({
     container: {
-      position: 'relative',
       height: '730px',
       padding: theme.spacing(15, 1, 0, 1),
       marginBottom: theme.spacing(12),
+      marginTop: theme.spacing(6),
       textAlign: 'center',
-      backgroundImage: 'url(img/header-image.png)',
+      backgroundImage: 'url(img/jumbotron-background-image-mobile.jpg)',
       backgroundPosition: 'center',
       backgroundSize: 'cover',
       color: theme.palette.common.white,
-      [theme.breakpoints.up('md')]: {
-        paddingTop: theme.spacing(31),
+      [theme.breakpoints.up('sm')]: {
+        marginTop: theme.spacing(14),
+        paddingTop: theme.spacing(19),
+        backgroundImage: 'url(img/jumbotron-background-image-desktop.jpg)',
       },
       [theme.breakpoints.up(1600)]: {
         height: '950px',
@@ -31,27 +30,45 @@ const useStyles = makeStyles((theme) =>
     },
     title: {
       color: theme.palette.common.white,
-      textShadow: '0px 2px 3px #000',
-      fontWeight: 600,
-      marginBottom: theme.spacing(1),
+      fontWeight: 500,
+      marginBottom: theme.spacing(4),
       [theme.breakpoints.down('xs')]: {
         fontSize: theme.typography.pxToRem(45),
       },
     },
     subTitle: {
-      marginTop: theme.spacing(3),
-      textShadow: '0px 2px 2px #000',
-      fontWeight: 600,
+      marginTop: theme.spacing(2),
+      fontWeight: 400,
+    },
+    aboutProjectButton: {
+      color: theme.palette.common.white,
+      border: `2px solid ${theme.palette.primary.main}`,
+      borderRadius: theme.spacing(3),
+      padding: theme.spacing(1.5, 5),
+      fontWeight: 500,
+      fontSize: theme.typography.pxToRem(15),
+      width: theme.spacing(27),
+      margin: theme.spacing(2),
     },
     podkrepiButton: {
       color: theme.palette.common.white,
-      borderColor: theme.palette.common.white,
+      backgroundColor: theme.palette.primary.main,
+      border: `2px solid ${theme.palette.primary.main}`,
+      borderRadius: theme.spacing(3),
       padding: theme.spacing(1.5, 5),
-      fontWeight: 600,
+      fontWeight: 500,
       fontSize: theme.typography.pxToRem(15),
+      width: theme.spacing(27),
+      margin: theme.spacing(2),
+      '&::after': {
+        content: '"❤"',
+        paddingLeft: theme.spacing(0.5),
+        fontSize: theme.typography.pxToRem(11),
+        height: theme.spacing(2),
+      },
     },
     scrollButton: {
-      marginTop: theme.spacing(8),
+      marginTop: theme.spacing(7),
       [theme.breakpoints.up(1600)]: {
         marginTop: theme.spacing(15),
       },
@@ -97,6 +114,12 @@ export default function Jumbotron({ scrollTo }: JumbotronProps) {
         <Typewriter />
       </Grid>
       <Grid item>
+        <LinkButton
+          href={routes.aboutProject}
+          variant="outlined"
+          className={classes.aboutProjectButton}>
+          {t('index:jumbotron.about-project-button')}
+        </LinkButton>
         <LinkButton href={routes.support} variant="outlined" className={classes.podkrepiButton}>
           {t('index:jumbotron.support-us-button')}
         </LinkButton>
