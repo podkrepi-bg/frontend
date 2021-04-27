b70f1f1f6d968fcb8fed00dde150883e80f5faf3
Andrey Marchev <xenryy@gmail.com>
2021-04-27 23:06:19 +0300

    use correct schema when talking to psql

--- db/migrations/campaign/V4.0.0.1__table_campaigns.sql
+++ db/migrations/campaign/V4.0.0.1__table_campaigns.sql
@@ -1,22 +1,22 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS "CampaignTypes" (
+CREATE TABLE IF NOT EXISTS campaign.CampaignTypes (
     "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
     "Name" character varying(50) NOT NULL,
-    "DescriptionKey" character varying(50) NULL,
+    "Description" character varying(50) NULL,
     CONSTRAINT "PK_CampaignTypes" PRIMARY KEY ("Id")
 );
 
-CREATE TABLE IF NOT EXISTS "CampaignSubTypes" (
+CREATE TABLE IF NOT EXISTS campaign.CampaignSubTypes (
     "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
     "Name" character varying(50) NOT NULL,
-    "DescriptionKey" character varying(50) NULL,
+    "Description" character varying(50) NULL,
     "CampaignTypeId" uuid NOT NULL,
     CONSTRAINT "PK_CampaignSubTypes" PRIMARY KEY ("Id"),
-    CONSTRAINT "FK_CampaignSubTypes_CampaignTypes_CampaignTypeId" FOREIGN KEY ("CampaignTypeId") REFERENCES "CampaignTypes" ("Id") ON DELETE CASCADE
+    CONSTRAINT "FK_CampaignSubTypes_CampaignTypes_CampaignTypeId" FOREIGN KEY ("CampaignTypeId") REFERENCES campaign.CampaignTypes ("Id") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "Campaigns" (
+CREATE TABLE IF NOT EXISTS campaign.Campaigns (
     "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
     "State" integer NOT NULL,
     "InitiatorId" uuid NOT NULL,
@@ -24,9 +24,9 @@ CREATE TABLE IF NOT EXISTS "Campaigns" (
     "OperatorId" uuid NOT NULL,
     "CampaignTypeId" uuid NOT NULL,
     "CampaignSubTypeId" uuid NOT NULL,
-    "TitleKey" character varying(50) NOT NULL,
-    "ShortDescriptionKey" character varying(50) NOT NULL,
-    "FullDescriptionKey" character varying(50) NULL,
+    "Title" character varying(50) NOT NULL,
+    "ShortDescription" character varying(50) NOT NULL,
+    "FullDescription" character varying(50) NULL,
     "TargetAmount" numeric NOT NULL,
     "Currency" integer NOT NULL,
     "CreationDate" timestamptz DEFAULT now()NOT NULL,
@@ -35,14 +35,14 @@ CREATE TABLE IF NOT EXISTS "Campaigns" (
     "Recurring" boolean NOT NULL,
     "OptionalDetails" text NOT NULL,
     CONSTRAINT "PK_Campaigns" PRIMARY KEY ("Id"),
-    CONSTRAINT "FK_Campaigns_CampaignSubTypes_CampaignSubTypeId" FOREIGN KEY ("CampaignSubTypeId") REFERENCES "CampaignSubTypes" ("Id") ON DELETE CASCADE,
-    CONSTRAINT "FK_Campaigns_CampaignTypes_CampaignTypeId" FOREIGN KEY ("CampaignTypeId") REFERENCES "CampaignTypes" ("Id") ON DELETE CASCADE
+    CONSTRAINT "FK_Campaigns_CampaignSubTypes_CampaignSubTypeId" FOREIGN KEY ("CampaignSubTypeId") REFERENCES campaign.CampaignSubTypes ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_Campaigns_CampaignTypes_CampaignTypeId" FOREIGN KEY ("CampaignTypeId") REFERENCES campaign.CampaignTypes ("Id") ON DELETE CASCADE
 );
 
-CREATE INDEX "IX_Campaigns_CampaignSubTypeId" ON "Campaigns" ("CampaignSubTypeId");
+CREATE INDEX "IX_Campaigns_CampaignSubTypeId" ON campaign.Campaigns ("CampaignSubTypeId");
 
-CREATE INDEX "IX_Campaigns_CampaignTypeId" ON "Campaigns" ("CampaignTypeId");
+CREATE INDEX "IX_Campaigns_CampaignTypeId" ON campaign.Campaigns ("CampaignTypeId");
 
-CREATE INDEX "IX_CampaignSubTypes_CampaignTypeId" ON "CampaignSubTypes" ("CampaignTypeId");
+CREATE INDEX "IX_CampaignSubTypes_CampaignTypeId" ON campaign.CampaignSubTypes ("CampaignTypeId");
 
 COMMIT;
