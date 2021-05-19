030363cbfe4db9efc32f9d1519632cd2367280f8
Andrey Marchev <xenryy@gmail.com>
2021-05-19 16:49:38 +0300

    make migration sql script a new version, after the original one

--- db/migrations/campaign/V4.0.0.1__table_campaigns.sql
+++ db/migrations/campaign/V4.0.0.1__table_campaigns.sql
@@ -1,51 +1,48 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS campaign.campaign_types (
-    id                  uuid NOT NULL DEFAULT (gen_random_uuid()),
-    name                character varying(50) NOT NULL,
-    description         character varying(200) NULL,
-
-    CONSTRAINT "PK_CampaignTypes" PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS "CampaignTypes" (
+    "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
+    "Name" character varying(50) NOT NULL,
+    "DescriptionKey" character varying(50) NULL,
+    CONSTRAINT "PK_CampaignTypes" PRIMARY KEY ("Id")
 );
 
-CREATE TABLE IF NOT EXISTS campaign.campaign_subtypes (
-    id                  uuid NOT NULL DEFAULT (gen_random_uuid()),
-    name                character varying(50) NOT NULL,
-    description         character varying(200) NULL,
-    campaign_type_id    uuid NOT NULL,
-
-    CONSTRAINT "PK_CampaignSubTypes" PRIMARY KEY (id),
-    CONSTRAINT "FK_CampaignSubTypes_CampaignTypes_CampaignTypeId" FOREIGN KEY (campaign_type_id) REFERENCES campaign.campaign_types (id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS "CampaignSubTypes" (
+    "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
+    "Name" character varying(50) NOT NULL,
+    "DescriptionKey" character varying(50) NULL,
+    "CampaignTypeId" uuid NOT NULL,
+    CONSTRAINT "PK_CampaignSubTypes" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_CampaignSubTypes_CampaignTypes_CampaignTypeId" FOREIGN KEY ("CampaignTypeId") REFERENCES "CampaignTypes" ("Id") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS campaign.campaigns (
-    id                  uuid NOT NULL DEFAULT (gen_random_uuid()),
-    state               integer NOT NULL,
-    initiator_id        uuid NOT NULL,
-    beneficiary_id      uuid NOT NULL,
-    operator_id         uuid NOT NULL,
-    campaign_type_id    uuid NOT NULL,
-    campaign_subtype_id uuid NULL,
-    title               character varying(200) NOT NULL,
-    short_description   character varying(500) NULL,
-    full_description    text NULL,
-    target_amount       numeric NULL,
-    currency            integer NULL,
-    deadline            timestamptz NULL,
-    optional_details    jsonb NULL,
-    created_at          timestamptz DEFAULT now() NOT NULL,
-    updated_at          timestamptz,
-    deleted_at          timestamptz,
-
-    CONSTRAINT "PK_Campaigns" PRIMARY KEY (id),
-    CONSTRAINT "FK_Campaigns_CampaignSubTypes_CampaignSubTypeId" FOREIGN KEY (campaign_subtype_id) REFERENCES campaign.campaign_subtypes (id) ON DELETE CASCADE,
-    CONSTRAINT "FK_Campaigns_CampaignTypes_CampaignTypeId" FOREIGN KEY (campaign_type_id) REFERENCES campaign.campaign_types (id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS "Campaigns" (
+    "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
+    "State" integer NOT NULL,
+    "InitiatorId" uuid NOT NULL,
+    "BeneficiaryId" uuid NOT NULL,
+    "OperatorId" uuid NOT NULL,
+    "CampaignTypeId" uuid NOT NULL,
+    "CampaignSubTypeId" uuid NOT NULL,
+    "TitleKey" character varying(50) NOT NULL,
+    "ShortDescriptionKey" character varying(50) NOT NULL,
+    "FullDescriptionKey" character varying(50) NULL,
+    "TargetAmount" numeric NOT NULL,
+    "Currency" integer NOT NULL,
+    "CreationDate" timestamptz DEFAULT now()NOT NULL,
+    "Verified" boolean NOT NULL,
+    "Deadline" timestamptz NULL,
+    "Recurring" boolean NOT NULL,
+    "OptionalDetails" text NOT NULL,
+    CONSTRAINT "PK_Campaigns" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_Campaigns_CampaignSubTypes_CampaignSubTypeId" FOREIGN KEY ("CampaignSubTypeId") REFERENCES "CampaignSubTypes" ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_Campaigns_CampaignTypes_CampaignTypeId" FOREIGN KEY ("CampaignTypeId") REFERENCES "CampaignTypes" ("Id") ON DELETE CASCADE
 );
 
-CREATE INDEX "IX_Campaigns_CampaignSubTypeId" ON campaign.campaigns (campaign_subtype_id);
+CREATE INDEX "IX_Campaigns_CampaignSubTypeId" ON "Campaigns" ("CampaignSubTypeId");
 
-CREATE INDEX "IX_Campaigns_CampaignTypeId" ON campaign.campaigns (campaign_type_id);
+CREATE INDEX "IX_Campaigns_CampaignTypeId" ON "Campaigns" ("CampaignTypeId");
 
-CREATE INDEX "IX_CampaignSubTypes_CampaignTypeId" ON campaign.campaign_subtypes (campaign_type_id);
+CREATE INDEX "IX_CampaignSubTypes_CampaignTypeId" ON "CampaignSubTypes" ("CampaignTypeId");
 
 COMMIT;
