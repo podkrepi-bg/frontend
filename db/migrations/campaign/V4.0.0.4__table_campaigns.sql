b84dabe422611652668853f42e9d7bf872635a13
mayapeneva <maya.peneva@gmail.com>
2021-06-06 23:15:08 +0300

    CreateBeneficiary: fixed last db migration

--- db/migrations/campaign/V4.0.0.4__table_campaigns.sql
+++ db/migrations/campaign/V4.0.0.4__table_campaigns.sql
@@ -3,7 +3,7 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS campaign.beneficiaries (
     id uuid NOT NULL DEFAULT (gen_random_uuid()),
     name character varying(100) NOT NULL,
-    type uuid NOT NULL,
+    type integer NOT NULL,
     iso2country_code integer NOT NULL,
     city character varying(50) NOT NULL,
     email character varying(100) NULL,
