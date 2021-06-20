86661059e17e93bca10db2a524c23a65c2755356
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-06-20 11:14:02 +0300

    Add styles for Typography Link button.

--- frontend/src/components/common/Typography.tsx
+++ frontend/src/components/common/Typography.tsx
@@ -1,16 +1,42 @@
-import { PropsWithChildren } from 'react'
-import { Typography as MaterialTypography, TypographyProps } from '@material-ui/core'
+import { PropsWithChildren, useState } from 'react'
 
+import { Typography as MaterialTypography, TypographyProps } from '@material-ui/core'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import LinkIcon from '@material-ui/icons/Link'
+
+import LinkIconButton from 'components/common/LinkIconButton'
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    linkIcon: {
+      transform: 'rotate(-45deg)',
+      color: theme.palette.primary.main,
+      '&:hover': {
+        color: theme.palette.primary.dark,
+      },
+    },
+    hideLinkIcon: {
+      visibility: 'hidden',
+    },
+  }),
+)
 
 type TypographyParams = PropsWithChildren<TypographyProps> & { id?: string; component?: string }
 
 export default function Typography({ children, id, ...props }: TypographyParams) {
+  const [linkIconIsShown, setlinkIconIsShown] = useState(false)
+  const classes = useStyles()
+
   return (
-    <div id={id}>
+    <div
+      id={id}
+      onMouseEnter={() => setlinkIconIsShown(true)}
+      onMouseLeave={() => setlinkIconIsShown(false)}>
       <MaterialTypography {...props}>
         {children}
-        <LinkIcon />
+        <LinkIconButton href={`#${id}`} className={linkIconIsShown ? '' : classes.hideLinkIcon}>
+          <LinkIcon className={classes.linkIcon} />
+        </LinkIconButton>
       </MaterialTypography>
     </div>
   )
