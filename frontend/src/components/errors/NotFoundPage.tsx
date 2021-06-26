22f021a239136dc36a399cd8687948f39a8386f6
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-06-26 20:16:24 +0300

    Add 404 SVG illustration.

--- frontend/src/components/errors/NotFoundPage.tsx
+++ frontend/src/components/errors/NotFoundPage.tsx
@@ -1,8 +1,10 @@
-import Layout from 'components/layout/Layout'
 import { useTranslation } from 'next-i18next'
+
+import Layout from 'components/layout/Layout'
+import NotFoundIllustration from './assets/NotFoundIllustration'
 
 export default function NotFoundPage() {
   const { t } = useTranslation()
 
-  return <p>{t('about:about.title')}</p>
+  return <NotFoundIllustration /> // <p>{t('about:about.title')}</p>
 }
