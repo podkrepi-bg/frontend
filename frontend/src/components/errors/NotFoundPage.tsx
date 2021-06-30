10fd4232f75b533709cb3abdbb1b8e804532227f
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-06-30 18:29:40 +0300

    Add locale support for 404 page back button.

--- frontend/src/components/errors/NotFoundPage.tsx
+++ frontend/src/components/errors/NotFoundPage.tsx
@@ -1,7 +1,10 @@
 import { useTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
 
-import { Grid, Typography } from '@material-ui/core'
-import { createStyles, makeStyles } from '@material-ui/core/styles'
+import { Button, Grid, Typography } from '@material-ui/core'
+
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 
 import NotFoundIllustration from './assets/NotFoundIllustration'
 
@@ -10,22 +13,34 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       flexGrow: 1,
       height: '100vh',
+      margin: 0,
     },
   }),
 )
 
 export default function NotFoundPage() {
   const { t } = useTranslation()
+  const router = useRouter()
+
   const classes = useStyles()
 
   return (
     <Grid container className={classes.root}>
-      <Grid container direction="column" alignItems="center" justify="center">
-        <Grid item>
+      <Grid container direction="column" alignItems="center" justify="center" spacing={4} xs={12}>
+        <Grid container item xs={8} sm={6} lg={4}>
           <NotFoundIllustration />
         </Grid>
         <Grid item>
           <Typography variant="body2">{t('errors:404Message')}</Typography>
+        </Grid>
+        <Grid container item alignItems="center" justify="center">
+          <Button
+            size="large"
+            startIcon={<ArrowBackIcon />}
+            variant="outlined"
+            onClick={() => router.replace('/')}>
+            {t('errors:backButtonLabel')}
+          </Button>
         </Grid>
       </Grid>
     </Grid>
