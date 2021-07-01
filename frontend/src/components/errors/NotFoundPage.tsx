b522cbc7dc787e69f6f2ddeaca5dde92f10f506f
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-07-01 10:35:48 +0300

    Wrap 404 Page in the Layout component.

--- frontend/src/components/errors/NotFoundPage.tsx
+++ frontend/src/components/errors/NotFoundPage.tsx
@@ -2,10 +2,10 @@ import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 
 import { Button, Grid, Typography } from '@material-ui/core'
-
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 
+import Layout from 'components/layout/Layout'
 import NotFoundIllustration from './assets/NotFoundIllustration'
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -26,24 +26,26 @@ export default function NotFoundPage() {
   const classes = useStyles()
 
   return (
-    <Grid container className={classes.root}>
-      <Grid container direction="column" alignItems="center" justify="center" spacing={4} xs={12}>
-        <Grid container item xs={8} sm={6} lg={4}>
-          <NotFoundIllustration />
-        </Grid>
-        <Grid item>
-          <Typography variant="body2">{t('errors:404Message')}</Typography>
-        </Grid>
-        <Grid container item alignItems="center" justify="center">
-          <Button
-            size="large"
-            startIcon={<ArrowBackIcon />}
-            variant="outlined"
-            onClick={() => router.replace('/')}>
-            {t('errors:backButtonLabel')}
-          </Button>
+    <Layout maxWidth={false} disableOffset disableGutters>
+      <Grid container className={classes.root} xs={12}>
+        <Grid container direction="column" alignItems="center" justify="center" spacing={4} xs={12}>
+          <Grid container item xs={8} sm={6} lg={4}>
+            <NotFoundIllustration />
+          </Grid>
+          <Grid item>
+            <Typography variant="body2">{t('errors:404Message')}</Typography>
+          </Grid>
+          <Grid container item alignItems="center" justify="center">
+            <Button
+              size="large"
+              startIcon={<ArrowBackIcon />}
+              variant="outlined"
+              onClick={() => router.replace('/')}>
+              {t('errors:backButtonLabel')}
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </Layout>
   )
 }
