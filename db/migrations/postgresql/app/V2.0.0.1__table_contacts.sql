09413bcdaf5a723b3f58238b80c3b8c0938fec5d
Christian-Vasilev <KristianVasilevv@gmail.com>
2021-08-26 19:28:14 +0300

    Added migrations for app

---
+++ db/migrations/postgresql/app/V2.0.0.1__table_contacts.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS app.contacts (
+    "id"            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "email"         CITEXT,
+    "first_name"    varchar,
+    "last_name"     varchar,
+    "phone"         varchar,
+    "company"       varchar,
+    "message"       TEXT,
+    "created_at"    TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    "updated_at"    TIMESTAMPTZ,
+    "deleted_at"    TIMESTAMPTZ,
+
+    CHECK (LENGTH(email) > 2)
+);
+
+COMMIT;
