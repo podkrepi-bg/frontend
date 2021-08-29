9a507e4d5fa2eac1ff4126498bf8e15599bd7934
Ivan Milchev <ivan.milchev321@gmail.com>
2021-08-29 20:31:49 +0200

    added deployment file for front-end fixed apiUrl to be retrieved from env vars

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -25,7 +25,7 @@ const moduleExports = {
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     services: {
-      apiUrl: 'http://api.podkrepi.localhost:5000',
+      apiUrl: process.env.API_URL + ':' + process.env.REST_API_PORT,
     },
   },
   publicRuntimeConfig: {
