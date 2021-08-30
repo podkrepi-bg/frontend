dae44a3371d3443892fcfc7967107c6f05ee836a
Ilko Kacharov <kachar136@gmail.com>
2021-08-30 22:13:35 +0300

    Clean jwt secret and next auth env vars

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -21,7 +21,6 @@ const moduleExports = {
     NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
   },
   serverRuntimeConfig: {
-    JWT_SECRET: process.env.JWT_SECRET,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     services: {
