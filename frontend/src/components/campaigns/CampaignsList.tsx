b1e997a4791ba312a91fc7c49ecac36f7abdfa9b
Christian-Vasilev <KristianVasilevv@gmail.com>
2021-09-04 01:25:27 +0300

    remove alter for user setting search_path for all schemas

--- frontend/src/components/campaigns/CampaignsList.tsx
+++ frontend/src/components/campaigns/CampaignsList.tsx
@@ -6,7 +6,7 @@ export default function CampaignsList() {
   return (
     <Grid container justify="center" spacing={2}>
       {[...Array(9).keys()].map((id) => (
-        <Grid key={id} item xs={12} sm={6} lg={4}>
+        <Grid key={id} item xs={12} sm={8} lg={6}>
           <Box textAlign="center">
             <CampaignCard id={id} />
           </Box>
