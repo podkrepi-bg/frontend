4d380d7ad0c21dc798ebd376918108734e0cef52
Ilko Kacharov <kachar136@gmail.com>
2021-09-17 22:17:16 +0300

    Remove apollo from the scene

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,13 +5,6 @@ import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { appWithTranslation, useTranslation } from 'next-i18next'
 
-// Apollo
-import withApollo from 'next-with-apollo'
-import { useApollo } from 'gql/apollo-client'
-import { getDataFromTree } from '@apollo/react-ssr'
-import { ApolloProvider } from '@apollo/react-hooks'
-import ApolloClient, { InMemoryCache } from 'apollo-boost'
-
 // MaterialUI
 import { LinearProgress } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/core/styles'
@@ -21,7 +14,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr'
 
 const {
-  publicRuntimeConfig: { GRAPHQL_URL, keycloakConfig },
+  publicRuntimeConfig: { keycloakConfig },
 } = getConfig()
 
 import theme from 'common/theme'
@@ -30,7 +23,6 @@ import useGTM from 'common/util/useGTM'
 import 'styles/global.scss'
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  const apollo = useApollo(pageProps)
   const router = useRouter()
   const { i18n } = useTranslation()
   const { initialize, trackEvent } = useGTM()
@@ -70,7 +62,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   }, [i18n.language])
 
   return (
-    <ApolloProvider client={apollo}>
+    <React.Fragment>
       <Head>
         <title>Podkrepi.bg</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
@@ -78,26 +70,16 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <SSRKeycloakProvider
+        {/* <SSRKeycloakProvider
           LoadingComponent={<LinearProgress />}
           onEvent={(e, err) => console.log(e, err)}
           keycloakConfig={keycloakConfig}
-          persistor={SSRCookies(pageProps?.keyCookies ?? {})}>
-          <Component {...pageProps} />
-        </SSRKeycloakProvider>
+          persistor={SSRCookies(pageProps?.keyCookies ?? {})}> */}
+        <Component {...pageProps} />
+        {/* </SSRKeycloakProvider> */}
       </ThemeProvider>
-    </ApolloProvider>
+    </React.Fragment>
   )
 }
 
-const TranslatedApp = appWithTranslation(CustomApp)
-// export default TranslatedApp
-export default withApollo(
-  ({ initialState }) => {
-    return new ApolloClient({
-      uri: GRAPHQL_URL,
-      cache: new InMemoryCache().restore(initialState || {}),
-    })
-  },
-  { getDataFromTree },
-)(TranslatedApp)
+export default appWithTranslation(CustomApp)
--- frontend/src/pages/campaigns/index.tsx
+++ frontend/src/pages/campaigns/index.tsx
@@ -2,21 +2,21 @@ import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import CampaignsPage from 'components/campaigns/CampaignsPage'
-import { addApolloState, createApolloClient } from 'gql/apollo-client'
-import { ListCampaign, ListCampaigns } from 'gql/query/campaigns/ListCampaigns'
+// import { addApolloState, createApolloClient } from 'gql/apollo-client'
+// import { ListCampaign, ListCampaigns } from 'gql/query/campaigns/ListCampaigns'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const client = createApolloClient()
-  await client.query<ListCampaign>({ query: ListCampaigns })
+  // const client = createApolloClient()
+  // await client.query<ListCampaign>({ query: ListCampaigns })
   return {
-    props: addApolloState(client, {
+    props: {
       ...(await serverSideTranslations(locale ?? 'bg', [
         'common',
         'auth',
         'validation',
         'campaigns',
       ])),
-    }),
+    },
   }
 }
 
