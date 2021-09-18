2d8deacdb0c2162fc301be78e08f5594d23ac6d5
Georgi Naumov <gonaumov@gmail.com>
2021-09-18 15:45:24 +0300

    Add bank account details below support us form

--- src/components/support-form/steps/Roles.tsx
+++ src/components/support-form/steps/Roles.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { useField } from 'formik'
 import { useTranslation } from 'next-i18next'
-import { FormControl, FormGroup, FormHelperText, Grid, Typography } from '@material-ui/core'
+import {
+  Divider,
+  FormControl,
+  FormGroup,
+  FormHelperText,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 
 import Role from './Role'
+import { BankTransfer } from '../../common/BankTransfer'
 
 export default function Roles() {
   const [, { error }] = useField('roles')
@@ -11,10 +19,26 @@ export default function Roles() {
   return (
     <Grid container spacing={6} justify="center">
       <Grid item xs={12} md={8}>
+        <Typography variant="h4" align="center" paragraph>
+          {t('support:steps.role.subtitle')}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Typography variant="h5" paragraph>
+          {t('support:steps.role.first-subtitle')}
+        </Typography>
+        <Divider />
+        <div>
+          <BankTransfer />
+        </div>
+        {t('common:or')}
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Typography variant="h5" paragraph>
+          {t('support:steps.role.second-subtitle')}
+        </Typography>
+        <Divider />
         <FormControl fullWidth required error={!!error} component="fieldset">
-          <Typography variant="h4" align="center" paragraph>
-            {t('support:steps.role.subtitle')}
-          </Typography>
           <FormGroup>
             <Role name="roles.benefactor" label={t('support:steps.role.fields.benefactor.title')} />
             <Role label={t('support:steps.role.fields.volunteer.title')} name="roles.volunteer" />
