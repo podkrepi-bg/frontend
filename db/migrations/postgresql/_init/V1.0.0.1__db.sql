969c807802e44abc365df0465f82b9559f587ba3
Ilko Kacharov <kachar136@gmail.com>
2021-09-18 10:24:45 +0300

    Remove db migrations from frontend repo

--- db/migrations/postgresql/_init/V1.0.0.1__db.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE DATABASE "app";
--- POSTGRESQL does not support CREATE IF NOT EXISTS, it needs to be faked
--- CREATE USER IF NOT EXISTS %s WITH PASSWORD NULL;
--- CREATE USER IF NOT EXISTS %s WITH PASSWORD '%s';
--- GRANT ALL ON DATABASE %s TO %s;
--- GRANT admin TO %s;
-
