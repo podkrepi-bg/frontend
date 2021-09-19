99e836e5d9297cd0c3b318f1f6926613bfa3e1e9
Georgi Naumov <gonaumov@gmail.com>
2021-09-19 08:10:08 +0300

    Disable sentry reports for localhost

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,6 +8,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://e25f62860a394934878c2e21306a6b66@o540074.ingest.sentry.io/5657969',
+  blacklistUrls: [/localhost/, /127.0.0.1/],
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
