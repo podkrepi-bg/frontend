bd7989e76ad58f241c0e21af4a6072b2436274f0
Ilko Kacharov <kachar136@gmail.com>
2021-10-18 15:52:24 +0300

    Added keycloak roles and authorization

--- src/components/admin/AdminPage.tsx
+++ src/components/admin/AdminPage.tsx
@@ -1,47 +1,15 @@
 import React from 'react'
 import { useTranslation } from 'next-i18next'
 import { Container } from '@material-ui/core'
-import { DataGrid } from '@mui/x-data-grid'
+import { KeycloakInstance } from 'keycloak-js'
 import { useKeycloak } from '@react-keycloak/ssr'
 
 import { routes } from 'common/routes'
+import { isAdmin } from 'common/util/roles'
 import Layout from 'components/layout/Layout'
 import LinkButton from 'components/common/LinkButton'
-import { KeycloakInstance } from 'keycloak-js'
 
-const columns = [
-  { field: 'id', headerName: 'ID', hidden: true },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 200,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 200,
-    editable: true,
-  },
-  {
-    field: 'email',
-    headerName: 'Email',
-    width: 200,
-    editable: true,
-  },
-]
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', email: 'john@example.com' },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', email: 'john@example.com' },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', email: 'john@example.com' },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', email: 'john@example.com' },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', email: 'john@example.com' },
-  { id: 6, lastName: 'Melisandre', firstName: null, email: 'john@example.com' },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', email: 'john@example.com' },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', email: 'john@example.com' },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', email: 'john@example.com' },
-]
+import InfoRequestGrid from './InfoRequestGrid'
 
 export default function AdminPage() {
   const { t } = useTranslation()
@@ -49,6 +17,10 @@ export default function AdminPage() {
 
   if (!keycloak?.authenticated) {
     return <Layout title={t('nav.admin.index')}>Not authenticated</Layout>
+  }
+
+  if (!isAdmin(keycloak)) {
+    return <Layout title={t('nav.admin.index')}>Not authorized</Layout>
   }
 
   return (
@@ -62,14 +34,7 @@ export default function AdminPage() {
         </LinkButton>
 
         <div style={{ height: '50vh', width: '100%', marginTop: '1rem' }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            disableSelectionOnClick
-          />
+          <InfoRequestGrid />
         </div>
       </Container>
     </Layout>
