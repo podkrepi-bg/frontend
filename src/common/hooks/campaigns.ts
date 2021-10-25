460c8d18297dc4c1fabf76b33b12b15005248227
Ilko Kacharov <kachar136@gmail.com>
2021-10-25 18:14:36 +0300

    Added stripe integration via api

--- src/common/hooks/campaigns.ts
+++ src/common/hooks/campaigns.ts
@@ -1,15 +1,16 @@
 import { useQuery } from 'react-query'
 
+import { endpoints } from 'common/api-endpoints'
 import { CampaignResponse, CampaignType } from 'gql/campaigns'
 
 export function useCampaignList() {
-  return useQuery<CampaignResponse[]>('/campaign/list')
+  return useQuery<CampaignResponse[]>(endpoints.campaign.listCampaigns.url)
 }
 
 export function useCampaignTypesList() {
-  return useQuery<CampaignType[]>('/campaign-type/list')
+  return useQuery<CampaignType[]>(endpoints.campaignType.listCampaignTypes.url)
 }
 
 export function useViewCampaign(slug: string) {
-  return useQuery<{ campaign: CampaignResponse }>(`/campaign/${slug}`)
+  return useQuery<{ campaign: CampaignResponse }>(endpoints.campaign.viewCampaign(slug).url)
 }
