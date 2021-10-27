f6d85d2ed354c6fe2d72446f657029c60badb7e3
atanasster <atanasster@gmail.com>
2021-10-26 23:27:28 -0400

    fix: ref types

--- src/components/index/IndexPage.tsx
+++ src/components/index/IndexPage.tsx
@@ -9,13 +9,14 @@ import TeamSection from './sections/TeamSection'
 import TeamChartSection from './sections/TeamChartSection'
 import MissionVisionGoalsSection from './sections/MissionVisionGoalsSection'
 import SupportUsSection from './sections/SupportUsSection'
+import { RefObject } from 'react-transition-group/node_modules/@types/react'
 
 export default function IndexPage() {
   const scrollElement = useRef<HTMLAnchorElement>(null)
   return (
     <Layout maxWidth={false} disableOffset disableGutters>
       <Jumbotron scrollTo={scrollElement} />
-      <Container innerRef={scrollElement} maxWidth="md">
+      <Container ref={scrollElement as RefObject<HTMLDivElement>} maxWidth="md">
         <ActivitySection />
         <TeamSection />
         <TeamChartSection />
