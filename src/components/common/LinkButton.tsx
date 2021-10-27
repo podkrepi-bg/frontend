f47cc28616c34abd9934e86516c98070a870829b
atanasster <atanasster@gmail.com>
2021-10-26 23:20:47 -0400

    fix: link buttons ref types errors

--- src/components/common/LinkButton.tsx
+++ src/components/common/LinkButton.tsx
@@ -2,13 +2,13 @@ import React, { forwardRef, Ref } from 'react'
 import Link, { LinkProps } from 'next/link'
 import { Button, ButtonProps } from '@mui/material'
 
-export type LinkRef = HTMLAnchorElement
+export type LinkRef = HTMLButtonElement
 export type NextLinkProps = Omit<ButtonProps, 'href'> &
   Pick<LinkProps, 'href' | 'as' | 'prefetch' | 'locale'>
 
 const NextLink = ({ href, as, prefetch, locale, ...props }: LinkProps, ref: Ref<LinkRef>) => (
   <Link href={href} as={as} prefetch={prefetch} locale={locale} passHref>
-    <Button buttonRef={ref} {...props} />
+    <Button ref={ref} {...props} />
   </Link>
 )
 
