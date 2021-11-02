aba7c9b379a34f189897bc15ea6639e77e257906
StanislavSt <s.staev1@gmail.com>
2021-11-02 13:20:32 +0100

    refactor: setupfile and test-utils

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,6 +1,5 @@
 import { render, RenderOptions } from '@testing-library/react'
 import { ThemeProvider } from '@mui/material/styles'
-import '@testing-library/jest-dom'
 import theme from 'common/theme'
 
 const Providers = ({ children }: { children: React.ReactElement }) => {
@@ -8,19 +7,6 @@ const Providers = ({ children }: { children: React.ReactElement }) => {
 }
 const customRender = (ui: React.ReactElement, options: RenderOptions = {}) =>
   render(ui, { wrapper: Providers, ...options })
-
-jest.mock('next-i18next', () => ({
-  // this mock makes sure any components using the translate hook can use it without a warning being shown
-  useTranslation: () => {
-    return {
-      t: (translation: string) => translation,
-      i18n: {
-        language: 'en',
-        changeLanguage: () => jest.fn(),
-      },
-    }
-  },
-}))
 
 export * from '@testing-library/react'
 
