ddba19802601351ba8749fe81ee6bbfcc2b6deb0
Ilko Kacharov <kachar136@gmail.com>
2021-11-03 08:51:14 +0200

    Update the legal entity component name

--- src/components/common/form/LegalEntityField.tsx
+++ src/components/common/form/LegalEntityField.tsx
@@ -7,12 +7,12 @@ export type AcceptTermsFieldProps = {
   name: string
 }
 
-export default function CompanyField({ name }: AcceptTermsFieldProps) {
+export default function LegalEntityField({ name }: AcceptTermsFieldProps) {
   const { t } = useTranslation()
   return (
     <CheckboxField
       name={name}
-      label={<Typography variant="body2">{t('validation:is-company')}</Typography>}
+      label={<Typography variant="body2">{t('validation:legal-entity')}</Typography>}
     />
   )
 }
