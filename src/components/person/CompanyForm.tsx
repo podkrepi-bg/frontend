26b09f19856155c5f2cd69c101833e68a1cbbd1f
Ilko Kacharov <kachar136@gmail.com>
2021-11-03 08:51:14 +0200

    Update the legal entity component name

--- src/components/person/CompanyForm.tsx
+++ src/components/person/CompanyForm.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 import { useField } from 'formik'
 import { Grid } from '@mui/material'
 
-import CompanyField from 'components/common/form/CompanyField'
 import FormTextField from 'components/common/form/FormTextField'
+import LegalEntityField from 'components/common/form/LegalEntityField'
 
 export default function CompanyForm() {
   const [field] = useField('legalEntity')
   if (!field.value) {
     return (
       <Grid item xs={12}>
-        <CompanyField name="legalEntity" />
+        <LegalEntityField name="legalEntity" />
       </Grid>
     )
   }
   return (
     <>
       <Grid item xs={12}>
-        <CompanyField name="legalEntity" />
+        <LegalEntityField name="legalEntity" />
       </Grid>
       <Grid item xs={12} sm={6}>
         <FormTextField type="text" label="Име на фирма" name="companyName" autoComplete="company" />
