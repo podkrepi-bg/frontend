bd01935ce4a3501df12bc7550334bdf36efd9aa7
StanislavSt <s.staev1@gmail.com>
2021-11-07 19:26:55 +0100

    feat(CampgainsPage): wrap in Feature flag

--- src/components/campaigns/CampaignsPage.tsx
+++ src/components/campaigns/CampaignsPage.tsx
@@ -5,24 +5,27 @@ import { Box, Container } from '@mui/material'
 import { routes } from 'common/routes'
 import Layout from 'components/layout/Layout'
 import LinkButton from 'components/common/LinkButton'
+import Feature, { Features } from 'components/common/Feature'
 
 import CampaignsList from './CampaignsList'
 
 export default function CampaignsPage() {
   const { t } = useTranslation()
   return (
-    <Layout
-      title={t('nav.campaigns.index')}
-      githubUrl="https://github.com/podkrepi-bg/frontend/tree/master/src/components/campaigns/CampaignsPage.tsx"
-      figmaUrl="https://www.figma.com/file/MmvFKzUv6yE5U2wrOpWtwS/Podkrepi.bg?node-id=5100%3A21216">
-      <Container maxWidth="lg">
-        <Box textAlign="center" marginBottom={4}>
-          <LinkButton href={routes.campaigns.create} variant="outlined" size="large">
-            {t('nav.campaigns.create')}
-          </LinkButton>
-        </Box>
-        <CampaignsList />
-      </Container>
-    </Layout>
+    <Feature name={Features.CAMPAIGN}>
+      <Layout
+        title={t('nav.campaigns.index')}
+        githubUrl="https://github.com/podkrepi-bg/frontend/tree/master/src/components/campaigns/CampaignsPage.tsx"
+        figmaUrl="https://www.figma.com/file/MmvFKzUv6yE5U2wrOpWtwS/Podkrepi.bg?node-id=5100%3A21216">
+        <Container maxWidth="lg">
+          <Box textAlign="center" marginBottom={4}>
+            <LinkButton href={routes.campaigns.create} variant="outlined" size="large">
+              {t('nav.campaigns.create')}
+            </LinkButton>
+          </Box>
+          <CampaignsList />
+        </Container>
+      </Layout>
+    </Feature>
   )
 }
