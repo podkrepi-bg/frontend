05239ba2c9798ac19fa7e3342692137d7767d068
Ilko Kacharov <kachar136@gmail.com>
2021-12-06 09:56:23 +0200

    Show profile menu in all cases

--- src/components/layout/nav/PublicMenu.tsx
+++ src/components/layout/nav/PublicMenu.tsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import { Grid } from '@mui/material'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
+import { AccountCircle } from '@mui/icons-material'
+import { Grid, IconButton, Menu, MenuItem } from '@mui/material'
 
 import { routes } from 'common/routes'
-import LinkButton from 'components/common/LinkButton'
+import { useSession } from 'common/util/useSession'
 
-export default function PublicMenu({ disableAuth = false }: { disableAuth?: boolean }) {
+export default function PrivateMenu() {
+  const router = useRouter()
   const { t } = useTranslation()
-  if (disableAuth) {
+  const { session } = useSession()
+  const [anchorEl, setAnchorEl] = useState<Element | null>(null)
+
+  const handleMenu = (event: React.MouseEvent) => setAnchorEl(event.currentTarget)
+  const handleClose = () => setAnchorEl(null)
+  const goto = (url: string) => (event: React.MouseEvent) => {
+    event.preventDefault()
+    setAnchorEl(null)
+    router.push(url)
+  }
+
+  if (session) {
     return null
   }
+
   return (
-    <>
-      <Grid item>
-        <LinkButton variant="text" href={routes.login}>
+    <Grid item>
+      <IconButton onClick={handleMenu} size="large">
+        <AccountCircle color="info" />
+      </IconButton>
+      <Menu
+        open={Boolean(anchorEl)}
+        keepMounted
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}>
+        <MenuItem href={routes.login} component="a" onClick={goto(routes.login)}>
           {t('nav.login')}
-        </LinkButton>
-      </Grid>
-      <Grid item>
-        <LinkButton variant="outlined" href={routes.register}>
+        </MenuItem>
+        <MenuItem href={routes.register} component="a" onClick={goto(routes.register)}>
           {t('nav.register')}
-        </LinkButton>
-      </Grid>
-    </>
+        </MenuItem>
+      </Menu>
+    </Grid>
   )
 }
