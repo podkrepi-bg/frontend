21acb73b870759aa0a092078ddeba74107ed2709
Ivan Kraev <ivankraev2@abv.bg>
2022-01-28 01:55:12 +0200

    sds

--- src/components/admin/tasks/Grid.tsx
+++ src/components/admin/tasks/Grid.tsx
@@ -1,8 +1,169 @@
 import { DataGrid } from '@mui/x-data-grid'
-import { columns } from './gridColumns'
-import { data } from './fakeData'
+import { GridColumns } from '@mui/x-data-grid'
+import StarIcon from '@mui/icons-material/Star'
+import CircleIcon from '@mui/icons-material/Circle'
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import { useMutation } from 'react-query'
+import axios from 'axios'
+import { useCarsList } from 'common/hooks/useCarsList'
+import { QueryClient } from 'react-query'
+import { useContext } from 'react'
+import { ModalContext } from 'context/ModalContext'
+export default function TasksGrid({ value }: any) {
+  console.log(value)
 
-export default function TasksGrid() {
+  const { carData, setData }: any = useContext(ModalContext)
+  const deleteCar = (id: any) => {
+    return axios.delete(`http://localhost:5010/api/car/${id}`)
+  }
+  const { isLoading, isError, isSuccess, isIdle, mutate } = useMutation(deleteCar, {
+    onSuccess: (data: any) => {
+      useCarsList()
+      setData((prevstate: any) => prevstate.filter((car: any) => car.id !== data.id))
+    },
+  })
+
+  const types = [
+    { icon: <StarIcon color="action" /> },
+    { icon: <CircleIcon color="warning" /> },
+    { icon: <StarIcon color="info" /> },
+    { icon: <CircleIcon color="success" /> },
+    { icon: <StarIcon color="error" /> },
+  ]
+  const random = (arr: any): any => {
+    return arr[Math.floor(Math.random() * types.length)].icon
+  }
+
+  const columns: GridColumns = [
+    { field: 'id', headerName: 'ID', hide: true },
+    {
+      field: 'type',
+      headerName: 'тип',
+      width: 50,
+      renderCell: () => {
+        return (
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            {random(types)}
+          </div>
+        )
+      },
+    },
+    {
+      field: 'brand',
+      headerName: 'Марка',
+      width: 280,
+      renderCell: (cellValues) => {
+        return (
+          <div style={{ fontWeight: 'bold', width: '100%', textAlign: 'left' }}>
+            {cellValues.value}
+          </div>
+        )
+      },
+    },
+    {
+      field: 'model',
+      headerName: 'Модел',
+      width: 80,
+      renderCell: (cellValues) => {
+        return (
+          <div style={{ fontWeight: 'bold', width: '100%', textAlign: 'center' }}>
+            {cellValues.value}
+          </div>
+        )
+      },
+    },
+    {
+      field: 'year',
+      headerName: 'Година',
+      width: 80,
+      renderCell: (cellValues) => {
+        return (
+          <div style={{ fontWeight: 'bold', width: '100%', textAlign: 'center' }}>
+            {cellValues.value}
+          </div>
+        )
+      },
+    },
+
+    {
+      field: 'engine',
+      headerName: 'Двигател',
+      width: 100,
+      renderCell: (cellValues) => {
+        return (
+          <div style={{ fontWeight: 'bold', width: '100%', textAlign: 'center' }}>
+            {cellValues.value}
+          </div>
+        )
+      },
+    },
+    {
+      field: 'price',
+      headerName: 'Цена',
+      width: 80,
+      renderCell: (cellValues) => {
+        return (
+          <div style={{ fontWeight: 'bold', width: '100%', textAlign: 'center' }}>
+            {cellValues.value}
+          </div>
+        )
+      },
+    },
+    {
+      field: 'others',
+      headerName: 'редактиране',
+      width: 140,
+      renderCell: (values: any) => {
+        return (
+          <div
+            style={{
+              width: '80%',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-evenly',
+            }}>
+            {
+              <EditIcon
+                sx={{ cursor: 'pointer' }}
+                color="action"
+                onClick={() => {
+                  console.log(values.id)
+                }}
+              />
+            }
+            {
+              <DeleteIcon
+                sx={{ cursor: 'pointer', opacity: 0.9 }}
+                color="error"
+                onClick={() => {
+                  mutate(values.id)
+                }}
+              />
+            }
+          </div>
+        )
+      },
+    },
+  ]
+  const { data } = useCarsList()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+      },
+    },
+  })
+
+  const state = queryClient.getQueryState('cars')
   return (
     <DataGrid
       style={{
@@ -11,7 +172,7 @@ export default function TasksGrid() {
         height: 'calc(100vh - 400px)',
         border: 'none',
       }}
-      rows={data || []}
+      rows={value || []}
       columns={columns}
       pageSize={5}
       autoHeight
--- src/pages/admin/panel/tasks.tsx
+++ src/pages/admin/panel/tasks.tsx
@@ -1,10 +1,15 @@
 import Tasks from 'components/admin/tasks/Tasks'
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
+import axios, { AxiosResponse } from 'axios'
+import { dehydrate, QueryClient } from 'react-query'
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const response = await fetch('http://localhost:5010/api/car')
+  const data = await response.json()
+
   return {
     props: {
+      cars: { data },
       ...(await serverSideTranslations(locale ?? 'bg', [
         'common',
         'auth',
