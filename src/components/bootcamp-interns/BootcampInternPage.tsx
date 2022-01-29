75254e27e95fb31ed9ea3467b7a16be57b698ced
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-01-29 15:43:12 +0200

    created edit page , form and the functionality to edit any of the bootcamp interns

--- src/components/bootcamp-interns/BootcampInternPage.tsx
+++ src/components/bootcamp-interns/BootcampInternPage.tsx
@@ -1,14 +1,11 @@
 import BootcampInternGrid from './BootcampInternGrid'
-import BootcampInternCreateForm from './BootcampInternCreateForm'
 import { Grid } from '@mui/material'
 import MyLayout from './MyLayout'
-import BootcampModal from './BootcampModal'
 
 export default function BootcampInternPage(props: any) {
   return (
     <MyLayout>
       <Grid container>
-        {/* <IconsMenu /> */}
         <BootcampInternGrid />
       </Grid>
     </MyLayout>
