9bc39b49f07072f2e723b65e774e6c1231e9aa8a
Ivan <ivankraev2@abv.bg>
2022-01-30 11:16:12 +0200

    update edit page

--- src/pages/admin/panel/tasks/edit/[id].tsx
+++ src/pages/admin/panel/tasks/edit/[id].tsx
@@ -1,8 +1,7 @@
-import axios from 'axios'
 import EditForm from 'components/admin/tasks/EditForm'
 import { GetServerSideProps } from 'next'
 import { dehydrate, QueryClient } from 'react-query'
-export const getServerSideProps: GetServerSideProps = async ({ locale, params }: any) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }: any) => {
   const fetchCar = async () => {
     const res = await fetch(`http://localhost:5010/api/car/${params.id}`)
     return res.json()
