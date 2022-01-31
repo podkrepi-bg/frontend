5b56847de259964619deca7f9452be7aef948a33
dplamenov <dimitar.plamenov@gmail.com>
2022-01-31 20:08:22 +0200

    Update layout.tsx

--- src/pages/bootcamp-dimitar/layout.tsx
+++ src/pages/bootcamp-dimitar/layout.tsx
@@ -1,32 +1,66 @@
 import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
+import Snackbar from 'components/layout/Snackbar'
 import MenuIcon from '@mui/icons-material/Menu'
-import AccountCircle from '@mui/icons-material/AccountCircle'
-import MenuItem from '@mui/material/MenuItem'
-import Menu from '@mui/material/Menu'
-import Drawer from '@mui/material/Drawer'
-import Divider from '@mui/material/Divider'
-import ListItem from '@mui/material/ListItem'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
-import { Container, Link, List } from '@mui/material'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import AppsIcon from '@mui/icons-material/Apps'
+import PeopleIcon from '@mui/icons-material/People'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import { makeStyles } from '@mui/styles'
+import { useTranslation } from 'next-i18next'
+import { ExpandLess, ExpandMore } from '@mui/icons-material'
+import {
+  AppBar,
+  Collapse,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Container,
+  Typography,
+} from '@mui/material'
+// import { makeStyles } from '@mui/styles'
 
-function CustomLayout({ children, title }: { children: any; title: string }) {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+const useStyles = makeStyles({
+  appBar: {
+    backgroundColor: '#fff',
+  },
+  logo: {
+    height: 40,
+    width: 'auto',
+    marginRight: 10,
+  },
+  closeNavBtn: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    padding: '8px 0',
+  },
+  submenu: {},
+})
+
+function CustomLayout({ children }: { children: any }) {
+  const classes = useStyles()
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false)
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
+  const [open, setOpen] = React.useState(false)
+  const [openMore, setOpenMore] = React.useState(false)
+  const { t } = useTranslation()
+
+  const toggleDrawer = () => {
+    setOpen(!open)
+  }
+  const toggleMore = () => {
+    setOpenMore(!openMore)
   }
 
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
+  const navLinks = [
+    { icon: <PeopleIcon />, name: 'All bootcampers', href: '/bootcamp-dimitar' },
+    { icon: <PersonAddIcon />, name: 'Create', href: '/bootcamp-dimitar/create' },
+    { icon: <AppsIcon />, name: 'Other', href: '#' },
+  ]
   return (
     <>
       <AppBar position="static">
@@ -46,62 +80,46 @@ function CustomLayout({ children, title }: { children: any; title: string }) {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Admin panel
           </Typography>
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit">
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}>
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleClose}>My account</MenuItem>
-            </Menu>
-          </div>
         </Toolbar>
       </AppBar>
       <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
         <List>
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <Link href="/bootcamp-dimitar">All</Link>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <Link href="/bootcamp-dimitar/create">Create</Link>
-          </ListItem>
+          <div className={classes.closeNavBtn}>
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
           <Divider />
+          {navLinks.map((item) => (
+            <ListItemButton key={item.name} component="a" href={item.href} color="black">
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          ))}
+          <ListItemButton onClick={toggleMore}>
+            <ListItemText primary={t('bootcamp:nav.more')} />
+            {openMore ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={openMore} timeout="auto" unmountOnExit>
+            <List className={classes.submenu}>
+              <ListItemButton>
+                <ListItemText primary="Option 1" />
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText primary="Option 2" />
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText primary="Option 3" />
+              </ListItemButton>
+            </List>
+          </Collapse>
         </List>
       </Drawer>
-      <Container maxWidth="lg">
-        <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
-          {title}
-        </Typography>
-        {children}
-      </Container>
+      <Container maxWidth="lg">{children}</Container>
       <footer style={{ position: 'fixed', bottom: 0, width: '100%' }}>
         <p style={{ textAlign: 'center' }}>footer text</p>
       </footer>
+      <Snackbar></Snackbar>
     </>
   )
 }
