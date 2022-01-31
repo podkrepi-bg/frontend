eff4ce9215dbf2b254ef3068a0a122496c95a8dd
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-01-31 15:13:25 +0200

    change the way I get the data to the edit page from props to a new fetch data or a cached one if it's still not stale, work with react-query and its superpowers...

--- src/components/bootcamp-interns/editPage/EditBootcampInternPage.tsx
+++ src/components/bootcamp-interns/editPage/EditBootcampInternPage.tsx
@@ -1,12 +1,16 @@
 import MyLayout from '../MyLayout'
 import { Grid } from '@mui/material'
 import BootcampInternEditForm from '../BootcampInternEditForm'
+import { UseBaseQueryResult, useQuery } from 'react-query'
+import { BootcampIntern } from 'lib/interfaces/BootcampIntern'
+import { useFetchBootcampIntern } from 'common/hooks/bootcampIntern'
+import { queryFn } from 'common/rest'
 
-export default function EditBootcampInternPage(props: any) {
+export default function EditBootcampInternPage() {
   return (
     <MyLayout>
       <Grid container>
-        <BootcampInternEditForm intern={props.intern} />
+        <BootcampInternEditForm />
       </Grid>
     </MyLayout>
   )
