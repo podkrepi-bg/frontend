babdc519724a5f8586f15e55e65cd97911814bff
quantum-grit <quantum-grit@gmail.com>
2022-02-01 16:00:04 +0200

    enabled static page generation by switching to getStaticProps for pages that don't load database content

--- src/pages/terms-of-service.tsx
+++ src/pages/terms-of-service.tsx
@@ -1,8 +1,8 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import TermsOfServicePage from 'components/terms-of-service/TermsOfServicePage'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
     ...(await serverSideTranslations(locale ?? 'bg', ['common'])),
   },
