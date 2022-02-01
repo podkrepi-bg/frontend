66a1b6c89efabb6e389fd4321d93566603e9bd40
Ivan Kraev <ivankraev2@abv.bg>
2022-02-01 18:08:17 +0200

    fixed problem - content now shrink when drawer is open

--- src/components/admin/navigation/LayoutPanel.tsx
+++ src/components/admin/navigation/LayoutPanel.tsx
@@ -15,7 +15,7 @@ const Main = styled('main', { shouldForwardProp: (prop: any) => prop !== 'open' 
   open?: boolean
 }>(({ theme, open }: any) => ({
   flexGrow: 1,
-  padding: theme.spacing(2),
+  padding: theme.spacing(3),
   transition: theme.transitions.create('margin', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
