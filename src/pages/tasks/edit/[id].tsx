e24b741c0f9506611325ed7b1c966366f6c361cb
Ivan Kraev <ivankraev2@abv.bg>
2022-02-02 19:57:26 +0200

    refactoring and organizing code almost done

--- src/pages/tasks/edit/[id].tsx
+++ src/pages/tasks/edit/[id].tsx
@@ -1,13 +1,15 @@
 import EditForm from 'components/admin/tasks/EditForm'
 import { GetServerSideProps } from 'next'
 import { dehydrate, QueryClient } from 'react-query'
-export const getServerSideProps: GetServerSideProps = async ({ params }: any) => {
+import { ParsedUrlQuery } from 'querystring'
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { id }: ParsedUrlQuery = query
   const fetchCar = async () => {
-    const res = await fetch(`http://localhost:5010/api/car/${params.id}`)
+    const res = await fetch(`http://localhost:5010/api/car/${id}`)
     return res.json()
   }
   const client = new QueryClient()
-  await client.prefetchQuery(`/car/${params.id}`, fetchCar)
+  await client.prefetchQuery(`/car/${id}`, fetchCar)
   return {
     props: {
       dehydratedState: dehydrate(client),
