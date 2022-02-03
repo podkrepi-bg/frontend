c3111c6308b1f9bd6d15fe9c46dafec5a39085ef
Ilko Kacharov <kachar136@gmail.com>
2022-02-03 16:44:28 +0200

    Disable FAQ page while it is WIP

--- src/components/faq/ContactUs.tsx
+++ src/components/faq/ContactUs.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Box, Link, Typography } from '@mui/material'
-import { routes } from '../../common/routes'
+import { Box, Typography } from '@mui/material'
+
+import { routes } from 'common/routes'
+import LinkButton from 'components/common/LinkButton'
 
 const ContactUs = () => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-      <Typography sx={{ mb: 2 }} variant={'h6'}>
+      <Typography sx={{ mb: 2 }} variant="h6">
         Не намерихте информацията, която търсите?
       </Typography>
-      <Link href={routes.contact} variant={'h6'}>
+      <LinkButton href={routes.contact} variant="contained" color="primary">
         Свържете се с нас!
-      </Link>
+      </LinkButton>
     </Box>
   )
 }
