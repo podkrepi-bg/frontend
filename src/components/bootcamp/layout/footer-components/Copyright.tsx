cbf470a7ce6eabdbf4fca715998146feca8f585d
Pepi Petroff <74186271+PepiPetrov@users.noreply.github.com>
2022-02-03 14:14:17 +0200

    Delete src/components/bootcamp directory

--- src/components/bootcamp/layout/footer-components/Copyright.tsx
+++
@@ -1,8 +0,0 @@
-import { Container, Typography } from "@mui/material";
-
-export default function Copyright() {
-    return <Container>
-        <Typography variant="body1">&copy; This webpage is made by Petar Petrov</Typography>
-        <Typography variant="body1">Licensed under MIT</Typography>
-    </Container>
-}
