5b594bd5a4b663496a112cae72ba7a79fd7a09bd
dplamenov <dimitar.plamenov@gmail.com>
2022-02-04 11:27:52 +0200

    create actions column in data grid
    create actions column in data grid

--- src/pages/admin/coordinator/index.tsx
+++ src/pages/admin/coordinator/index.tsx
@@ -1,7 +1,7 @@
 import Layout from 'components/admin/Layout'
 import { DataGrid, GridColumns, GridRowId } from '@mui/x-data-grid'
 import { useCoordinatorList } from 'common/hooks/coordinators'
-import { Button, IconButton } from '@mui/material'
+import { Box, Button, IconButton, Modal, Typography } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import PageviewIcon from '@mui/icons-material/Pageview'
@@ -9,7 +9,7 @@ import Link from 'next/link'
 import React from 'react'
 import { CoordinatorResponse } from 'gql/coordinator'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
-import DeleteSelectedModal from 'components/admin/DeleteSelected'
+import DeleteSelectedModal from 'components/admin/DeleteSelectedModal'
 import { useMutation } from 'react-query'
 import { AlertStore } from 'stores/AlertStore'
 import { deleteCoordinator } from 'common/rest'
@@ -17,17 +17,60 @@ import { ApiErrors } from 'common/api-errors'
 import { AxiosError, AxiosResponse } from 'axios'
 import router from 'next/router'
 
+const style = {
+  position: 'absolute' as const,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+}
+
 function Coordinator() {
   const { data = [] } = useCoordinatorList()
   const [selectedRows, setSelectedRows] = React.useState<CoordinatorResponse[]>([])
   const [selectionModel, setSelectionModel] = React.useState<GridRowId[]>([])
   const [isDeleteSelectedModalOpen, setIsDeleteSelectedModalOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false)
+  const [row, setRow] = React.useState<CoordinatorResponse>()
+  const [rowToDelete, setRowToDelete] = React.useState<{ id: string }>()
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = React.useState(false)
 
   const mutation = useMutation<AxiosResponse<CoordinatorResponse>, AxiosError<ApiErrors>, any>({
     mutationFn: deleteCoordinator,
     onError: () => AlertStore.show('error', 'error'),
     onSuccess: () => AlertStore.show('success', 'success'),
   })
+
+  const handleOpen = (row: any) => {
+    return () => {
+      setOpen(true)
+      setRow(row)
+    }
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleDeleteModalOpen = (row: any) => {
+    return () => {
+      setRowToDelete(row)
+      setIsDeleteModalOpen(true)
+    }
+  }
+
+  const handleDeleteModalClose = () => {
+    setIsDeleteModalOpen(false)
+  }
+
+  const deleteHandler = () => {
+    mutation.mutateAsync(rowToDelete?.id as string).then(() => {
+      handleDeleteModalClose()
+      router.reload()
+    })
+  }
 
   const columns: GridColumns = [
     {
@@ -47,6 +90,28 @@ function Coordinator() {
       headerName: 'Company',
       valueGetter: (p) => p.row.person.company,
       flex: 2,
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      renderCell: (cellValues) => {
+        return (
+          <>
+            <IconButton size="small" sx={{ mr: 1 }} onClick={handleOpen(cellValues.row)}>
+              <PageviewIcon />
+            </IconButton>
+            <Link href={`/admin/coordinator/${cellValues.row.id}/edit`}>
+              <IconButton size="small" sx={{ mr: 1 }}>
+                <EditIcon />
+              </IconButton>
+            </Link>
+            <IconButton size="small" sx={{ mr: 1 }} onClick={handleDeleteModalOpen(cellValues.row)}>
+              <DeleteIcon />
+            </IconButton>
+          </>
+        )
+      },
+      minWidth: 150,
     },
   ]
 
@@ -95,6 +160,39 @@ function Coordinator() {
         isOpen={isDeleteSelectedModalOpen}
         deleteHandler={deleteSelectedHandler}
         handleDeleteModalClose={closeDeleteSelectedHandler}></DeleteSelectedModal>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {row?.person.firstName} {row?.person.lastName}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {row?.person.company}
+          </Typography>
+        </Box>
+      </Modal>
+      <Modal
+        open={isDeleteModalOpen}
+        onClose={handleDeleteModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Delete?
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <Button variant="contained" onClick={deleteHandler}>
+              Yes
+            </Button>
+            <Button variant="contained" onClick={handleDeleteModalClose}>
+              No
+            </Button>
+          </Typography>
+        </Box>
+      </Modal>
     </Layout>
   )
 }
