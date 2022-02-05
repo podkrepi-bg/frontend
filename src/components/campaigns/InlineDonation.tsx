bab8cb3c3d847f5b059005e62d739af1e8c90b16
ani <ani.kalpachka@scalefocus.com>
2022-02-05 12:18:19 +0200

    Revert Donation Price List

--- src/components/campaigns/InlineDonation.tsx
+++ src/components/campaigns/InlineDonation.tsx
@@ -1,9 +1,12 @@
+import { useCallback, useMemo, useState } from 'react'
 import { useTranslation } from 'next-i18next'
 import { CampaignResponse } from 'gql/campaigns'
+import { baseUrl, routes } from 'common/routes'
 import { money } from 'common/util/money'
+import { useSinglePriceList, useDonationSession } from 'common/hooks/donations'
 import LinkButton from 'components/common/LinkButton'
 import CampaignProgress from './CampaignProgress'
-import { Grid, Theme, Typography } from '@mui/material'
+import { Grid, List, ListItem, ListItemText, Theme, Typography } from '@mui/material'
 import makeStyles from '@mui/styles/makeStyles'
 import createStyles from '@mui/styles/createStyles'
 import ShareIcon from '@mui/icons-material/Share'
@@ -29,6 +32,10 @@ const useStyles = makeStyles((theme: Theme) =>
     shareButton: {
       padding: theme.spacing(0.75, 0),
     },
+    DonationPriceList: {
+      display: 'contents',
+      textAlignLast: 'center',
+    },
   }),
 )
 
@@ -40,38 +47,40 @@ export default function InlineDonation({ campaign }: Props) {
   const classes = useStyles()
   const { t } = useTranslation()
 
+  const [showDonationPriceList, setDonationPriceList] = useState(false)
+  const onClick = () => setDonationPriceList(true)
+
   const target = campaign.targetAmount
   const summary = campaign.summary.find(() => true)
   const reached = summary ? summary.reachedAmount : 0
+  const { data: prices } = useSinglePriceList()
+  const mutation = useDonationSession()
 
-  // const { data: prices } = useSinglePriceList()
-  // const mutation = useDonationSession()
+  const donate = useCallback(
+    async (priceId: string) => {
+      const { data } = await mutation.mutateAsync({
+        mode: 'payment',
+        priceId,
+        campaignId: campaign.id,
+        successUrl: `${baseUrl}${routes.campaigns.viewCampaignBySlug(campaign.slug)}`,
+        cancelUrl: `${baseUrl}${routes.campaigns.viewCampaignBySlug(campaign.slug)}`,
+      })
+      console.log(data)
+      console.log(data.session.url)
+      if (data.session.url) {
+        window.location.href = data.session.url
+      }
+    },
+    [mutation],
+  )
 
-  // const donate = useCallback(
-  //   async (priceId: string) => {
-  //     const { data } = await mutation.mutateAsync({
-  //       mode: 'payment',
-  //       priceId,
-  //       campaignId: campaign.id,
-  //       successUrl: `${baseUrl}${routes.campaigns.viewCampaignBySlug(campaign.slug)}`,
-  //       cancelUrl: `${baseUrl}${routes.campaigns.viewCampaignBySlug(campaign.slug)}`,
-  //     })
-  //     console.log(data)
-  //     console.log(data.session.url)
-  //     if (data.session.url) {
-  //       window.location.href = data.session.url
-  //     }
-  //   },
-  //   [mutation],
-  // )
-
-  // const sortedPrices = useMemo(() => {
-  //   if (!prices) return []
-  //   return prices?.sort((a, b) => {
-  //     if (a.unit_amount === null || b.unit_amount === null) return 0
-  //     return a.unit_amount - b.unit_amount
-  //   })
-  // }, [prices])
+  const sortedPrices = useMemo(() => {
+    if (!prices) return []
+    return prices?.sort((a, b) => {
+      if (a.unit_amount === null || b.unit_amount === null) return 0
+      return a.unit_amount - b.unit_amount
+    })
+  }, [prices])
 
   return (
     <Grid container mt={5} p={3} className={classes.inlineDonationWrapper}>
@@ -107,26 +116,29 @@ export default function InlineDonation({ campaign }: Props) {
         <LinkButton
           fullWidth
           href="#"
+          onClick={onClick}
           variant="contained"
           color="secondary"
           startIcon={<Favorite color="action" />}>
           {t('common:support')}
         </LinkButton>
+        {showDonationPriceList && (
+          <List className={classes.DonationPriceList}>
+            {sortedPrices.map((price, index) => {
+              if (!price) return null
+              return (
+                <ListItem button key={index}>
+                  <ListItemText
+                    onClick={() => donate(price.id)}
+                    primary={`${(price.unit_amount ?? 100) / 100} лв.`}
+                    secondary={price.metadata.title}
+                  />
+                </ListItem>
+              )
+            })}
+          </List>
+        )}
       </Grid>
-      {/* <List>
-        {sortedPrices.map((price, index) => {
-          if (!price) return null
-          return (
-            <ListItem button key={index}>
-              <ListItemText
-                onClick={() => donate(price.id)}
-                primary={`${(price.unit_amount ?? 100) / 100} лв.`}
-                secondary={price.metadata.title}
-              />
-            </ListItem>
-          )
-        })}
-      </List> */}
       {/* <pre>{JSON.stringify(prices, null, 2)}</pre> */}
     </Grid>
   )
