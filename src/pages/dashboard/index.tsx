728abc6e37dd4173fad3e92e2be7f0d9949572bb
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-07 14:47:07 +0200

    dashboard page prefetch country list

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,26 @@
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
+import { dehydrate, QueryClient } from 'react-query'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+import { queryFn } from 'common/rest'
+import { endpoints } from 'common/api-endpoints'
 import DashboardPage from 'components/dashboard/DashboardPage'
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale ?? 'bg', ['common', 'dashboard'])),
-  },
-})
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const client = new QueryClient()
+  await client.prefetchQuery(endpoints.country.countryList.url, queryFn)
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'bg', [
+        'common',
+        'auth',
+        'dashboard',
+        'validation',
+      ])),
+      dehydratedState: dehydrate(client),
+    },
+  }
+}
 
 export default DashboardPage
