90e884bbb54770dc08ef6e10c7a2599b942d5d36
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-07 16:21:00 +0200

    dashboard url page fix

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -8,16 +8,11 @@ import DashboardPage from 'components/dashboard/DashboardPage'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   const client = new QueryClient()
-  await client.prefetchQuery(endpoints.country.countryList.url, queryFn)
+  await client.prefetchQuery(endpoints.country.listCountries.url, queryFn)
 
   return {
     props: {
-      ...(await serverSideTranslations(locale ?? 'bg', [
-        'common',
-        'auth',
-        'dashboard',
-        'validation',
-      ])),
+      ...(await serverSideTranslations(locale ?? 'bg', ['common', 'country', 'dashboard'])),
       dehydratedState: dehydrate(client),
     },
   }
