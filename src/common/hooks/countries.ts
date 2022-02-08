83383629d5576adfc9b23102049dcc70b7377fe8
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-08 13:57:48 +0200

    added useCountry hook

--- src/common/hooks/countries.ts
+++ src/common/hooks/countries.ts
@@ -1,8 +1,19 @@
 import { useQuery } from 'react-query'
+import { useKeycloak } from '@react-keycloak/ssr'
+import { KeycloakInstance } from 'keycloak-js'
 
 import { endpoints } from 'common/api-endpoints'
 import { CountryResponse } from 'gql/countries'
+import { authQueryFnFactory } from 'common/rest'
 
 export function useCountriesList() {
   return useQuery<CountryResponse[]>(endpoints.country.listCountries.url)
+}
+
+export function useCountry(id: string) {
+  const { keycloak } = useKeycloak<KeycloakInstance>()
+  return useQuery<CountryResponse>(
+    endpoints.country.getCountry.url + '/' + id,
+    authQueryFnFactory<CountryResponse>(keycloak?.token),
+  )
 }
