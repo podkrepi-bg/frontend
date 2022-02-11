9a8359167dc79ff4837797a23a92efd5eb2ac391
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-11 14:17:01 +0200

    dashboard layout update

--- src/components/dashboard/DashboardPage.tsx
+++ src/components/dashboard/DashboardPage.tsx
@@ -1,10 +1,5 @@
 import DashboardLayout from './DashboardLayout'
-import CountryGrid from './country/CountryGrid'
 
 export default function DashboardPage() {
-  return (
-    <DashboardLayout>
-      <CountryGrid />
-    </DashboardLayout>
-  )
+  return <DashboardLayout />
 }
