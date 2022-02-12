8dbc0acd29e2571518a027329585bb02b557aba8
PepiPetrov <pepi_petrov@outlook.com>
2022-02-12 16:41:21 +0200

    bugfix

--- src/components/admin/campaign-types/SearchById.tsx
+++ src/components/admin/campaign-types/SearchById.tsx
@@ -1,7 +1,7 @@
 import { Container } from '@mui/material'
 import MainLayout from './navigation/MainLayout'
 import GenericGrid from './PersonGrid'
-import { usePersonList } from 'common/hooks/person'
+import { useCampaignTypesList } from 'common/hooks/campaign-types'
 import { useRouter } from 'next/router'
 import AppBarMenu from './navigation/AppBarMenu'
 import BankAccountsBottomAppbar from './PersonBottomAppBar'
@@ -12,11 +12,9 @@ export default function BootcampPage() {
   const { query } = useRouter()
   const queryId = (Array.isArray(query.id) ? query.id[0] : query.id) || ''
 
-  const info = usePersonList()
-  const bootcampers = info.data?.filter(
-    (x) =>
-      x.firstName.toLocaleLowerCase().includes(queryId.toLocaleLowerCase()) ||
-      x.lastName.toLocaleLowerCase().includes(queryId.toLocaleLowerCase()),
+  const info = useCampaignTypesList()
+  const bootcampers = info.data?.filter((x) =>
+    x.name.toLocaleLowerCase().includes(queryId.toLocaleLowerCase()),
   )
 
   return (
