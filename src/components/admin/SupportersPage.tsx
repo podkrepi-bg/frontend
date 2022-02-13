897908bfc193353bc187fcbdb8f7007ff3669f5d
quantum-grit <quantum-grit@gmail.com>
2022-02-13 17:26:15 +0200

    added Admin menu, showing only when authenticated

--- src/components/admin/SupportersPage.tsx
+++ src/components/admin/SupportersPage.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Container } from '@mui/material'
+import { Container, Typography } from '@mui/material'
+import { useTranslation } from 'next-i18next'
 
 import Layout from 'components/layout/Layout'
-
 import SupportersGrid from './SupportersGrid'
 
 export default function SupportersPage() {
+  const { t } = useTranslation()
+
   return (
     <Layout>
       <Container maxWidth="lg">
+        <Typography variant="h3">{t('nav.admin.supporters')}</Typography>
         <SupportersGrid />
       </Container>
     </Layout>
