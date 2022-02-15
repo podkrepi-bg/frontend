9eeedd517c02e289b914b7c0cde6c59eda29071d
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-15 14:10:29 +0200

    added headings

--- src/components/dashboard/country/CountryEditPage.tsx
+++ src/components/dashboard/country/CountryEditPage.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'next-i18next'
+
 import { useCountry } from 'common/hooks/countries'
 
 import DashboardLayout from '../DashboardLayout'
@@ -5,10 +7,11 @@ import CountryForm from './CountryForm'
 
 type Props = { id: string }
 export default function CountryEditPage({ id }: Props) {
+  const { t } = useTranslation('country')
   const { data } = useCountry(id)
 
   return (
-    <DashboardLayout>
+    <DashboardLayout title={t('headings.edit-country')}>
       <CountryForm initialValues={data} id={id} />
     </DashboardLayout>
   )
