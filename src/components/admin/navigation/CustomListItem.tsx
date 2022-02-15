29c236cbfdc84bf01b5eceadc957e88ff5101029
Ilko Kacharov <kachar136@gmail.com>
2022-02-16 01:40:40 +0200

    Update selected option of admin menu

--- src/components/admin/navigation/CustomListItem.tsx
+++ src/components/admin/navigation/CustomListItem.tsx
@@ -5,10 +5,10 @@ type Props = {
   icon: React.ReactNode
 } & ListItemProps
 
-function CustomListItem({ label, icon, ...props }: Props) {
+function CustomListItem({ label, icon, selected, ...props }: Props) {
   return (
     <ListItem disablePadding {...props}>
-      <ListItemButton sx={{ borderRadius: '20px' }}>
+      <ListItemButton selected={selected} sx={{ borderRadius: '20px' }}>
         <ListItemIcon title={label}>{icon}</ListItemIcon>
         {label && <ListItemText primary={label} />}
       </ListItemButton>
