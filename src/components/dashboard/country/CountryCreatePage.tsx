5cbc210e3e73bfd2a55326b7dc016dbc6bd7d191
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-15 14:10:29 +0200

    added headings

--- src/components/dashboard/country/CountryCreatePage.tsx
+++ src/components/dashboard/country/CountryCreatePage.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from 'next-i18next'
+
 import DashboardLayout from '../DashboardLayout'
 import CountryForm from './CountryForm'
 
 export default function CountryCreatePage() {
+  const { t } = useTranslation('country')
+
   return (
-    <DashboardLayout>
+    <DashboardLayout title={t('headings.add-country')}>
       <CountryForm />
     </DashboardLayout>
   )
--- src/components/dashboard/country/CountryMainPage.tsx
+++ src/components/dashboard/country/CountryMainPage.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from 'next-i18next'
+
 import DashboardLayout from '../DashboardLayout'
 import CountryGrid from './CountryGrid'
 
 export default function CountryMainPage() {
+  const { t } = useTranslation('country')
+
   return (
-    <DashboardLayout>
+    <DashboardLayout title={t('headings.countries')}>
       <CountryGrid />
     </DashboardLayout>
   )
