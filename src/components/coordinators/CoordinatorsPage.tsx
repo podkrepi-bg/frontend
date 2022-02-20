e2ce94fd7311b6d67c75b418c27472b3e7d425a3
dplamenov <dimitar.plamenov@gmail.com>
2022-02-20 16:34:44 +0200

    coordinators
    coordinators

--- src/components/coordinators/CoordinatorsPage.tsx
+++ src/components/coordinators/CoordinatorsPage.tsx
@@ -2,16 +2,16 @@ import AdminLayout from 'components/admin/navigation/AdminLayout'
 import AdminContainer from 'components/admin/navigation/AdminContainer'
 
 import CoordinatorsGrid from './CoordinatorsGrid'
-// import BankAccountsModal from './BankAccountsModal'
-// import BankAccountsDetails from './BankAccountsDetails'
+import CoordinatorModal from './CoordinatorModal'
+import CoordinatorDetails from './CoordinatorDetails'
 import CoordinatorBottomAppbar from './CoordinatorBottomAppbar'
 
 export default function BankAccountsPage() {
   return (
     <AdminLayout>
-      {/* <BankAccountsModal>
-        <BankAccountsDetails />
-      </BankAccountsModal> */}
+      <CoordinatorModal>
+        <CoordinatorDetails />
+      </CoordinatorModal>
       <AdminContainer title={'Координатори'}>
         <CoordinatorBottomAppbar />
         <CoordinatorsGrid />
