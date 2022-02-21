4386cf6e001c77da33036619c1ff1a8100ef0871
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-21 11:15:56 +0200

    deleted dashboard files

--- src/components/dashboard/DashboardPage.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from 'next-i18next'
-
-import DashboardLayout from './DashboardLayout'
-
-export default function DashboardPage() {
-  const { t } = useTranslation('country')
-
-  return <DashboardLayout title={t('headings.home')} />
-}
