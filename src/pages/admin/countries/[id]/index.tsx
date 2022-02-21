3e21284a8066a60e6f4609370a0588380e4052cf
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-21 16:58:39 +0200

    refactoring + added keycloak to edit page

--- src/pages/admin/countries/[id]/index.tsx
+++ src/pages/admin/countries/[id]/index.tsx
@@ -2,18 +2,30 @@ import { GetServerSideProps } from 'next'
 import { dehydrate, QueryClient } from 'react-query'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-import { queryFn } from 'service/restRequests'
+import { keycloakInstance } from 'middleware/auth/keycloak'
+import { authQueryFnFactory } from 'service/restRequests'
+import { endpoints } from 'service/apiEndpoints'
+
 import CountryEditPage from 'components/countries/CountryEditPage'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale, query }) => {
-  const { id } = query
+export const getServerSideProps: GetServerSideProps = async (params) => {
+  const { id } = params.query
   const client = new QueryClient()
-  await client.prefetchQuery(`/country/${id}`, queryFn)
+  const keycloak = keycloakInstance(params)
+
+  await client.prefetchQuery(
+    endpoints.country.editCountry(`${id}`).url,
+    authQueryFnFactory(keycloak.token),
+  )
 
   return {
     props: {
       id,
-      ...(await serverSideTranslations(locale ?? 'bg', ['common', 'validation', 'countries'])),
+      ...(await serverSideTranslations(params.locale ?? 'bg', [
+        'common',
+        'validation',
+        'countries',
+      ])),
       dehydratedState: dehydrate(client),
     },
   }
