c93c33d87ff62793cf1058eca2b8df119c0e5dbc
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-24 14:48:34 +0200

    bankAccounts: removed unused modals from main page

--- src/components/bankaccounts/BankAccountsPage.tsx
+++ src/components/bankaccounts/BankAccountsPage.tsx
@@ -1,17 +1,12 @@
 import AdminLayout from 'components/admin/navigation/AdminLayout'
 import AdminContainer from 'components/admin/navigation/AdminContainer'
 
-import BankAccountsGrid from './BankAccountsGrid'
-import BankAccountsModal from './BankAccountsModal'
-import BankAccountsDetails from './BankAccountsDetails'
+import BankAccountsGrid from './grid/BankAccountsGrid'
 import BankAccountsBottomAppbar from './BankAccountsBottomAppBar'
 
 export default function BankAccountsPage() {
   return (
     <AdminLayout>
-      <BankAccountsModal>
-        <BankAccountsDetails />
-      </BankAccountsModal>
       <AdminContainer title={'Банкови сметки'}>
         <BankAccountsBottomAppbar />
         <BankAccountsGrid />
