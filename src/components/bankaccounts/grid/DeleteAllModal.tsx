48a5c3cae60d5d7e00fda0dcfbe3854409da1ea1
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-02-24 14:45:21 +0200

    bankAccounts: new delete all modal

---
+++ src/components/bankaccounts/grid/DeleteAllModal.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useMutation, useQueryClient } from 'react-query'
+import { observer } from 'mobx-react'
+import { AxiosError, AxiosResponse } from 'axios'
+import { Dialog, Card, CardContent, Box, Button, Typography } from '@mui/material'
+import { GridSelectionModel } from '@mui/x-data-grid'
+import { useTranslation } from 'next-i18next'
+
+import { BankAccountResponse } from 'gql/bankaccounts'
+import { ApiErrors } from 'service/apiErrors'
+import { endpoints } from 'service/apiEndpoints'
+import { useDeleteManyBankAccounts } from 'service/restRequests'
+import { ModalStore } from 'stores/dashboard/ModalStore'
+import { AlertStore } from 'stores/AlertStore'
+
+type Props = {
+  selectionModel: GridSelectionModel
+}
+
+export default observer(function DeleteAllModal({ selectionModel }: Props) {
+  const queryClient = useQueryClient()
+  const { isDeleteAllOpen, hideDeleteAll } = ModalStore
+  const { t } = useTranslation('bankaccounts')
+
+  const idsToDelete = selectionModel.map((x) => x.toString())
+  const mutationFn = useDeleteManyBankAccounts(idsToDelete)
+
+  const mutation = useMutation<
+    AxiosResponse<BankAccountResponse>,
+    AxiosError<ApiErrors>,
+    GridSelectionModel
+  >({
+    mutationFn,
+    onError: () => AlertStore.show(t('alerts.error'), 'error'),
+    onSuccess: () => {
+      hideDeleteAll()
+      AlertStore.show(t('alerts.deleteAll'), 'success')
+      queryClient.invalidateQueries(endpoints.bankAccounts.bankAccountList.url)
+    },
+  })
+
+  function deleteHandler() {
+    mutation.mutate(idsToDelete)
+  }
+
+  return (
+    <Dialog open={isDeleteAllOpen} onClose={hideDeleteAll}>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" sx={{ marginBottom: '16px', textAlign: 'center' }}>
+            {t('deleteTitle')}
+          </Typography>
+          <Typography variant="body1" sx={{ marginBottom: '16px', textAlign: 'center' }}>
+            {t('deleteAllContent')}
+          </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Button color="error" onClick={deleteHandler}>
+              {t('cta.delete')}
+            </Button>
+            <Button onClick={hideDeleteAll}>{t('cta.cancel')}</Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Dialog>
+  )
+})
