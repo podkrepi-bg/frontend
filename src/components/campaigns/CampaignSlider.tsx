886752d2623563db425f0bb186cc5a72c7734b59
ani <ani.kalpachka@scalefocus.com>
2022-03-04 20:18:47 +0200

    Fix slider options, add mobile breakpoint

--- src/components/campaigns/CampaignSlider.tsx
+++ src/components/campaigns/CampaignSlider.tsx
@@ -1,10 +1,23 @@
 import BearCarousel, { TBearSlideItemDataList, BearSlideItem } from 'bear-react-carousel'
 import 'bear-react-carousel/dist/index.css'
+import { Theme } from '@mui/material'
+import makeStyles from '@mui/styles/makeStyles'
+import createStyles from '@mui/styles/createStyles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    campaignSlider: {
+      margin: theme.spacing(3, 0),
+    },
+  }),
+)
+
+const slideSource = '/img/campaign-banner.png'
 
 const images = [
-  { id: 1, image: 'https://dummyimage.com/900x400/dee2e6/6c757d.jpg' },
-  { id: 2, image: 'https://dummyimage.com/900x400/dee2e6/6c757d.jpg' },
-  { id: 3, image: 'https://dummyimage.com/900x400/dee2e6/6c757d.jpg' },
+  { id: 1, image: slideSource },
+  { id: 2, image: slideSource },
+  { id: 3, image: slideSource },
 ]
 
 const bearSlideItemData: TBearSlideItemDataList = images.map((row) => {
@@ -15,13 +28,24 @@ const bearSlideItemData: TBearSlideItemDataList = images.map((row) => {
 })
 
 const CampaignSlider = () => {
+  const classes = useStyles()
+
   return (
     <BearCarousel
       data={bearSlideItemData}
-      slidesPerView={2}
-      isEnableLoop
+      slidesPerView={1}
+      isEnableLoop={true}
       isEnableNavButton
+      spaceBetween={15}
+      autoPlayTime={2000}
+      isEnableAutoPlay={true}
       aspectRatio={{ widthRatio: 16, heightRatio: 9 }}
+      className={classes.campaignSlider}
+      breakpoints={{
+        600: {
+          slidesPerView: 2,
+        },
+      }}
     />
   )
 }
