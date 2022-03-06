fdd89a6223063adee8e8c48c8b7285016256d575
dplamenov <dimitar.plamenov@gmail.com>
2022-03-06 20:51:09 +0200

    fix

--- src/components/coordinators/CoordinatorCreateSelect.tsx
+++ src/components/coordinators/CoordinatorCreateSelect.tsx
@@ -1,4 +1,5 @@
 import { usePersonList } from 'common/hooks/person'
+import { useCoordinatorsList } from 'common/hooks/coordinators'
 import { InputLabel, MenuItem, Select, FormControl } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import { useField } from 'formik'
@@ -7,18 +8,23 @@ function CoordinatorCreateSelect() {
   const { t } = useTranslation()
   const [field] = useField('personId')
   const { data: personList } = usePersonList()
+  const { data: coordinatorLsit } = useCoordinatorsList()
 
   return (
     <FormControl fullWidth>
       <InputLabel>Избери</InputLabel>
       <Select fullWidth defaultValue="" label={t('campaigns:campaign.type')} {...field}>
-        {personList?.map((person) => {
-          return (
-            <MenuItem key={person.id} value={person.id}>
-              {person.firstName} {person.lastName}
-            </MenuItem>
-          )
-        })}
+        {personList
+          ?.filter((person) => {
+            return !coordinatorLsit?.find((c) => c.personId === person.id)
+          })
+          ?.map((person) => {
+            return (
+              <MenuItem key={person.id} value={person.id}>
+                {person.firstName} {person.lastName}
+              </MenuItem>
+            )
+          })}
       </Select>
     </FormControl>
   )
