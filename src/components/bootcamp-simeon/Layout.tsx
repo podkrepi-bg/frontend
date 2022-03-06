c1735e7946aa374a6dc44837b79b94de49232c5c
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-03-06 12:58:28 +0200

    First files
    Created first files for the dummy layout.

---
+++ src/components/bootcamp-simeon/Layout.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Drawer } from '@mui/material'
+import Link from 'next/link'
+import { Grid } from '@mui/material'
+
+function Layout({ children }: { children: any }) {
+  return <></>
+}
+
+export default Layout
