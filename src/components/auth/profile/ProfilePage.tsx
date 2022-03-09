68360e7f16e1f3353ff88f795e2c41b57e737b1b
dplamenov <dimitar.plamenov@gmail.com>
2022-03-09 17:36:25 +0200

    profile page

--- src/components/auth/profile/ProfilePage.tsx
+++ src/components/auth/profile/ProfilePage.tsx
@@ -1,89 +1,147 @@
 import React from 'react'
 import { useTranslation } from 'next-i18next'
-import { Box, Container, Grid } from '@mui/material'
+import { Box, Container, Grid, Checkbox, Button } from '@mui/material'
 
 import { routes } from 'common/routes'
 import { isAdmin } from 'common/util/roles'
-import Layout from 'components/layout/Layout'
+import ProfileLayout from 'components/layout/ProfileLayout'
 import { useSession } from 'common/util/useSession'
 import LinkButton from 'components/common/LinkButton'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}>
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  )
+}
 
 export default function ProfilePage() {
   const { t } = useTranslation()
-
+  const [value, setValue] = React.useState(0)
   const { keycloak, session } = useSession()
+
+  const handleChange = (_: any, value: number) => {
+    setValue(value)
+  }
 
   if (!keycloak?.authenticated) {
     return (
-      <Layout
+      <ProfileLayout
         title={t('nav.profile')}
         githubUrl="https://github.com/podkrepi-bg/frontend/tree/master/src/components/auth/profile/ProfilePage.tsx"
         figmaUrl="https://www.figma.com/file/MmvFKzUv6yE5U2wrOpWtwS/Podkrepi.bg?node-id=5987%3A21094">
         Not authenticated
-      </Layout>
+      </ProfileLayout>
     )
   }
 
   return (
-    <Layout
-      title={t('nav.profile')}
+    <ProfileLayout
+      title={`Добре дошъл, ${session?.given_name}!`}
       githubUrl="https://github.com/podkrepi-bg/frontend/tree/master/src/components/auth/profile/ProfilePage.tsx"
       figmaUrl="https://www.figma.com/file/MmvFKzUv6yE5U2wrOpWtwS/Podkrepi.bg?node-id=5987%3A21094">
-      <Container maxWidth="xl">
-        <Grid container direction="row" justifyContent="space-between">
-          <Grid item>
-            <ul>
-              <li>
-                <span className="font-weight-bold mr-1">ID: </span>
-                <strong className="text-muted">{session?.sub ?? ''}</strong>
-              </li>
-              <li>
-                <span className="font-weight-bold mr-1">Email: </span>
-                <strong className="text-muted">{session?.email ?? ''}</strong>
-              </li>
-              <li>
-                <span className="font-weight-bold mr-1">First Name: </span>
-                <strong className="text-muted">{session?.given_name ?? ''}</strong>
-              </li>
-              <li>
-                <span className="font-weight-bold mr-1">Last Name: </span>
-                <strong className="text-muted">{session?.family_name ?? ''}</strong>
-              </li>
-              <li>
-                <span className="font-weight-bold mr-1">Administrator access: </span>
-                <strong className="text-muted">
-                  {keycloak?.authenticated && isAdmin(keycloak) ? 'Administrator' : 'User'}
-                </strong>
-              </li>
-              <li>
-                <span className="font-weight-bold mr-1">Allowed origins: </span>
-                <span className="text-muted">
-                  <pre>
-                    {JSON.stringify((session && session['allowed-origins']) ?? [], null, 2)}
-                  </pre>
-                </span>
-              </li>
-              <li>
-                <span className="font-weight-bold mr-1">Roles: </span>
-                <span className="text-muted">
-                  <pre>{JSON.stringify(keycloak.resourceAccess, null, 2)}</pre>
-                </span>
-              </li>
-            </ul>
-          </Grid>
-          {/* <Grid item>
-            <pre>JWT: {keycloak.token?.substring(0, 30)}...</pre>
-            <pre>{JSON.stringify(session, null, 2)}</pre>
-            <pre>{JSON.stringify(keycloak, null, 2)}</pre>
-          </Grid> */}
-        </Grid>
-
-        <Box textAlign="center">
-          <LinkButton variant="contained" href={routes.logout}>
-            {t('nav.logout')}
-          </LinkButton>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Дарения" />
+            <Tab label="Лична информация" />
+            <Tab label="Сертификати" />
+            <Tab label="Договор дарение" />
+          </Tabs>
         </Box>
-      </Container>
-    </Layout>
+        <TabPanel value={value} index={0}>
+          <h2>Абонамент месечни дарения</h2>
+          <Box sx={{ display: 'flex' }}>
+            <Box
+              sx={{
+                backgroundColor: '#EEEEEE',
+                flexGrow: 1,
+                marginRight: '10px',
+                padding: '10px',
+              }}>
+              <h3 style={{ fontSize: '16px', margin: 0 }}>Дарения</h3>
+
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <h4 style={{ fontSize: '24px', margin: 0 }}>Онлайн дарения</h4>
+                <p style={{ fontSize: '22px' }}>0.00 лв.</p>
+              </Box>
+              <p>Към момента няма направени дарения</p>
+              <hr></hr>
+
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <h4 style={{ fontSize: '24px', margin: 0 }}>Тотал онлайн дарения</h4>
+                <p style={{ fontSize: '22px' }}>0.00 лв.</p>
+              </Box>
+              <hr></hr>
+            </Box>
+            <Box sx={{ backgroundColor: '#C4C4C4', padding: '10px', position: 'relative' }}>
+              <h2>Бъди промяната</h2>
+              <h3>помогни на хора в нужда</h3>
+              <Button
+                variant="contained"
+                size="medium"
+                color="secondary"
+                sx={{ position: 'absolute', bottom: '35px' }}>
+                Дари сега ❤️
+              </Button>
+            </Box>
+          </Box>
+          <h2>История на даренията</h2>
+          <Box
+            sx={{
+              backgroundColor: '#EEEEEE',
+              flexGrow: 1,
+              marginRight: '10px',
+              padding: '10px',
+            }}>
+            <h3 style={{ fontSize: '16px', margin: 0 }}>Онлайн дарения</h3>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                width: '70%',
+              }}>
+              <span>Покажи:</span>
+              <Box>
+                <Checkbox defaultChecked />
+                <span>еднократни</span>
+              </Box>
+              <Box>
+                <Checkbox defaultChecked />
+                <span>месечни</span>
+              </Box>
+              <span>възможност за търсене по по дата</span>
+            </Box>
+            <h3 style={{ fontSize: '25px' }}>Към момента няма направени дарения</h3>
+          </Box>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Лична информация
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Сертификати
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Договор дарение
+        </TabPanel>
+      </Box>
+
+      <Box textAlign="center">
+        <LinkButton variant="contained" href={routes.logout}>
+          {t('nav.logout')}
+        </LinkButton>
+      </Box>
+    </ProfileLayout>
   )
 }
