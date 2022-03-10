f35f6478e142e110a1cbedc5740d79767be7c3bf
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-03-10 08:48:06 +0200

    documents: using the shared dialogs from admin + refactoring

--- src/components/documents/grid/DeleteModal.tsx
+++ src/components/documents/grid/DeleteModal.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { useMutation, useQueryClient } from 'react-query'
+import { useRouter } from 'next/router'
+import { useMutation } from 'react-query'
 import { observer } from 'mobx-react'
 import { AxiosError, AxiosResponse } from 'axios'
-import { Dialog, Card, CardContent, Box, Button, Typography } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 
 import { DocumentResponse } from 'gql/document'
 import { ApiErrors } from 'service/apiErrors'
-import { endpoints } from 'service/apiEndpoints'
 import { useDeleteDocument } from 'service/restRequests'
 import { ModalStore } from 'stores/dashboard/ModalStore'
 import { AlertStore } from 'stores/AlertStore'
+import { routes } from 'common/routes'
+import DeleteDialog from 'components/admin/DeleteDialog'
 
 export default observer(function DeleteModal() {
-  const queryClient = useQueryClient()
-  const { isDeleteOpen, hideDelete, selectedRecord } = ModalStore
+  const router = useRouter()
+  const { hideDelete, selectedRecord } = ModalStore
   const { t } = useTranslation()
 
   const mutationFn = useDeleteDocument()
@@ -29,7 +30,7 @@ export default observer(function DeleteModal() {
     onSuccess: () => {
       hideDelete()
       AlertStore.show(t('documents:alerts:delete'), 'success')
-      queryClient.invalidateQueries(endpoints.documents.documentsList.url)
+      router.push(routes.admin.documents.index)
     },
   })
 
@@ -37,24 +38,5 @@ export default observer(function DeleteModal() {
     deleteMutation.mutate(selectedRecord.id)
   }
 
-  return (
-    <Dialog open={isDeleteOpen} onClose={hideDelete} sx={{ top: '-35%' }}>
-      <Card>
-        <CardContent>
-          <Typography variant="h6" sx={{ marginBottom: '16px', textAlign: 'center' }}>
-            {t('documents:deleteTitle')}
-          </Typography>
-          <Typography variant="body1" sx={{ marginBottom: '16px', textAlign: 'center' }}>
-            {t('documents:deleteContent')} ({selectedRecord.name})
-          </Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Button color="error" onClick={deleteHandler}>
-              {t('documents:cta:delete')}
-            </Button>
-            <Button onClick={hideDelete}>{t('documents:cta:cancel')}</Button>
-          </Box>
-        </CardContent>
-      </Card>
-    </Dialog>
-  )
+  return <DeleteDialog deleteHandler={deleteHandler} />
 })
