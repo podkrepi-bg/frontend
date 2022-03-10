7e7bb8236d1f1af566d15fd14d1664235154002f
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-03-10 07:13:42 +0200

    coordinators: using the shared dialogs from admin

--- src/components/coordinators/grid/DeleteAllModal.tsx
+++ src/components/coordinators/grid/DeleteAllModal.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useMutation, useQueryClient } from 'react-query'
 import { observer } from 'mobx-react'
 import { AxiosError, AxiosResponse } from 'axios'
-import { Dialog, Button, DialogTitle, DialogActions } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 
 import { CoordinatorResponse } from 'gql/coordinators'
@@ -11,10 +10,11 @@ import { endpoints } from 'service/apiEndpoints'
 import { useDeleteCoordinator } from 'service/restRequests'
 import { ModalStore } from 'stores/dashboard/ModalStore'
 import { AlertStore } from 'stores/AlertStore'
+import DeleteAllDialog from 'components/admin/DeleteAllDialog'
 
 export default observer(function DeleteAllModal() {
   const queryClient = useQueryClient()
-  const { isDeleteAllOpen, hideDeleteAll, selectedIdsToDelete, setSelectedIdsToDelete } = ModalStore
+  const { hideDeleteAll, selectedIdsToDelete, setSelectedIdsToDelete } = ModalStore
   const { t } = useTranslation('')
 
   const mutation = useMutation<AxiosResponse<CoordinatorResponse>, AxiosError<ApiErrors>, string>({
@@ -28,23 +28,9 @@ export default observer(function DeleteAllModal() {
     },
   })
 
-  const deleteAllHandler = async () => {
+  const deleteHandler = async () => {
     await Promise.all(selectedIdsToDelete.map((id) => mutation.mutateAsync(id)))
   }
 
-  return (
-    <Dialog open={isDeleteAllOpen} onClose={hideDeleteAll} maxWidth="xs" disableScrollLock>
-      <DialogTitle>
-        {t('coordinator:alert.delete-row.question')} ({selectedIdsToDelete.length})?
-      </DialogTitle>
-      <DialogActions sx={{ m: '0 10px 10px 10px' }}>
-        <Button variant="contained" color="secondary" fullWidth onClick={deleteAllHandler}>
-          {t('coordinator:btns.confirm')}
-        </Button>
-        <Button variant="contained" color="primary" fullWidth onClick={hideDeleteAll}>
-          {t('coordinator:btns.cancel')}
-        </Button>
-      </DialogActions>
-    </Dialog>
-  )
+  return <DeleteAllDialog deleteHandler={deleteHandler} />
 })
