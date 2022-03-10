345631f319b3b19239dddb40ff2d89d5270fbc16
dplamenov <dimitar.plamenov@gmail.com>
2022-03-10 14:53:52 +0200

    update styles

--- src/components/auth/profile/PersonalInfoTab.tsx
+++ src/components/auth/profile/PersonalInfoTab.tsx
@@ -3,28 +3,46 @@ import { useSession } from 'common/util/useSession'
 import Tab from './Tab'
 import EditIcon from '@mui/icons-material/Edit'
 import { useState } from 'react'
+import { makeStyles, useTheme } from '@mui/styles'
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 600,
-  bgcolor: '#EEEEEE',
-  boxShadow: 24,
-  p: 4,
-}
+const useStyles = makeStyles({
+  modal: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    backgroundColor: '#EEEEEE',
+    padding: 20,
+  },
+  editSpan: {
+    color: '#294E85',
+  },
+  deleteAccountButton: { color: '#294E85', float: 'right' },
+  editIcon: { position: 'relative', top: '7px' },
+  heading: {
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '24px',
+    lineHeight: '123.5%',
+    /* identical to box height, or 30px */
+    letterSpacing: '0.25px',
+    color: '#000000',
+  },
+})
 
 function PersonalInfoTab(props: any) {
   const { value, index } = props
   const { session } = useSession()
   const [isDeleteAccountModalOpen, setIsDeleteAccountModalOpen] = useState(false)
+  const classes = useStyles()
 
   return (
     <>
       <Tab value={value} index={index}>
         <h1>Лична информация</h1>
-        <h2>Login информация</h2>
+        <h2 className={classes.heading}>Login информация</h2>
         <Box sx={{ display: 'flex' }}>
           <Box
             sx={{
@@ -47,14 +65,14 @@ function PersonalInfoTab(props: any) {
             <p>***********</p>
             <Box sx={{ position: 'absolute', right: '5px', top: '5px' }}>
               <Link href="#">
-                <EditIcon sx={{ position: 'relative', top: '7px' }}></EditIcon>
-                <span style={{ color: '#294E85' }}>Редактирай</span>
+                <EditIcon className="editIcon"></EditIcon>
+                <span className="editSpan">Редактирай</span>
               </Link>
             </Box>
           </Box>
         </Box>
         <hr></hr>
-        <h2>Лична информация</h2>
+        <h2 className={classes.heading}>Лична информация</h2>
         <Box sx={{ display: 'flex' }}>
           <Box
             sx={{
@@ -68,8 +86,8 @@ function PersonalInfoTab(props: any) {
             <p>{session?.name}</p>
             <Box sx={{ position: 'absolute', right: '5px', top: '5px' }}>
               <Link href="#">
-                <EditIcon sx={{ position: 'relative', top: '7px' }}></EditIcon>
-                <span style={{ color: '#294E85' }}>Редактирай</span>
+                <EditIcon className="editIcon"></EditIcon>
+                <span className="editSpan">Редактирай</span>
               </Link>
             </Box>
           </Box>
@@ -84,8 +102,8 @@ function PersonalInfoTab(props: any) {
             <p>не е наличен</p>
             <Box sx={{ position: 'absolute', right: '5px', top: '5px' }}>
               <Link href="#">
-                <EditIcon sx={{ position: 'relative', top: '7px' }}></EditIcon>
-                <span style={{ color: '#294E85' }}>Редактирай</span>
+                <EditIcon className="editIcon"></EditIcon>
+                <span className="editSpan">Редактирай</span>
               </Link>
             </Box>
           </Box>
@@ -93,7 +111,7 @@ function PersonalInfoTab(props: any) {
         <hr></hr>
         <Link
           href="#"
-          sx={{ color: '#294E85', float: 'right' }}
+          className="deleteAccountButton"
           onClick={() => setIsDeleteAccountModalOpen(true)}>
           изтриване на акаунт/ профил
         </Link>
@@ -103,7 +121,7 @@ function PersonalInfoTab(props: any) {
         onClose={() => setIsDeleteAccountModalOpen(false)}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description">
-        <Box sx={style}>
+        <Box className={classes.modal}>
           <Typography variant="h6" component="h2">
             Изтриване на профил
           </Typography>
