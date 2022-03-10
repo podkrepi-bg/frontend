7754c16b27ac8145e1fb8a5af2803c0004ceb1a0
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-03-10 09:43:27 +0200

    countries: using the shared details dialog

--- src/components/countries/grid/DetailsModal.tsx
+++ src/components/countries/grid/DetailsModal.tsx
@@ -1,50 +1,23 @@
 import { UseQueryResult } from 'react-query'
 import { observer } from 'mobx-react'
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
-import { makeStyles } from '@mui/styles'
 import { useTranslation } from 'next-i18next'
 
 import { ModalStore } from 'stores/dashboard/ModalStore'
 import { useCountry } from 'common/hooks/countries'
 
 import { CountryResponse } from 'gql/countries'
-
-const useStyles = makeStyles({
-  infoBtn: {
-    margin: '0 auto',
-  },
-})
+import DetailsDialog from 'components/admin/DetailsDialog'
 
 export default observer(function DetailsModal() {
-  const { isDetailsOpen, hideDetails, selectedRecord } = ModalStore
+  const { selectedRecord } = ModalStore
   const { data }: UseQueryResult<CountryResponse> = useCountry(selectedRecord.id)
   const { t } = useTranslation('countries')
 
-  const classes = useStyles()
+  const dataConverted = [
+    { name: 'ID', value: `${data?.id}` },
+    { name: t('fields.name'), value: `${data?.name}` },
+    { name: t('fields.country-code'), value: `${data?.countryCode}` },
+  ]
 
-  return (
-    <Dialog open={isDetailsOpen} onClose={hideDetails} maxWidth="xs" disableScrollLock>
-      <DialogTitle>{t('headings.info')}</DialogTitle>
-      <DialogContent>
-        <p>
-          <b>Id:</b> {data?.id}
-        </p>
-        <p>
-          <b>{t('fields.name')}:</b> {data?.name}
-        </p>
-        <p>
-          <b>{t('fields.country-code')}:</b> {data?.countryCode}
-        </p>
-      </DialogContent>
-      <DialogActions>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.infoBtn}
-          onClick={hideDetails}>
-          {t('btns.close')}
-        </Button>
-      </DialogActions>
-    </Dialog>
-  )
+  return <DetailsDialog data={dataConverted} />
 })
