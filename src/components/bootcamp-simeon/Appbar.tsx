2b2a8d5db5ef0ea48da3319d7afe907810a90e7d
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-03-14 16:46:09 +0200

    Component styles and appbar start up
    Started writing styles for my own components and also started writing the appbar component

--- src/components/bootcamp-simeon/Appbar.tsx
+++ src/components/bootcamp-simeon/Appbar.tsx
@@ -1,5 +1,48 @@
-import { AppBar } from '@mui/material'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import { styled } from '@mui/material/styles'
+import Image from 'next/image'
+import * as React from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useTranslation } from 'next-i18next'
+import { drawerWidth } from './styles'
 
-export default function BootcampAppbar() {
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean
+}
+
+interface Props {
+  open: boolean
+  handler: () => void
+}
+
+const AppBar = styled(MuiAppBar, { shouldForwardProp: (prop) => prop !== 'open' })<AppBarProps>(
+  ({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }),
+)
+
+export default function BootcampAppbar({ open, handler }: Props) {
+  const { t } = useTranslation()
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null)
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget)
+  }
+
+  const handleClosedUserMenu = () => {
+    setAnchorElUser(null)
+  }
+
   return <AppBar></AppBar>
 }
---
+++ src/components/bootcamp-simeon/styles.tsx
@@ -0,0 +1,9 @@
+import { makeStyles } from '@mui/styles'
+
+const drawerWidth = 194
+
+const useStyles = makeStyles({
+  active: {},
+})
+
+export { drawerWidth, useStyles as styles }
