f3a0820cfa413bdd714938a7a4d8676fafb750c4
dplamenov <dimitar.plamenov@gmail.com>
2022-03-16 18:02:13 +0200

    display name of upload files

--- src/components/campaigns/CampaignForm.tsx
+++ src/components/campaigns/CampaignForm.tsx
@@ -90,6 +90,7 @@ export default function CampaignForm({ initialValues = defaults }: CampaignFormP
   const router = useRouter()
   const [coordinator, setCoordinator] = useState<PersonFormData>()
   const [beneficiary, setBeneficiary] = useState<PersonFormData>()
+  const [files, setFiles] = useState<{ name: string }[]>([])
 
   const mutation = useMutation<
     AxiosResponse<CampaignResponse>,
@@ -226,11 +227,22 @@ export default function CampaignForm({ initialValues = defaults }: CampaignFormP
           </Grid>
           <Grid item xs={12}>
             <label htmlFor="contained-button-file">
-              <input id="contained-button-file" multiple type="file" style={{ display: 'none' }} />
+              <input
+                id="contained-button-file"
+                multiple
+                type="file"
+                style={{ display: 'none' }}
+                onChange={(e) => setFiles([...(e.target.files as FileList)])}
+              />
               <Button fullWidth variant="contained" color="info" component="span">
                 Добави снимки
               </Button>
             </label>
+            <ul>
+              {[...files].map((file) => {
+                return <li key={file.name}>{file.name}</li>
+              })}
+            </ul>
             {/* <FileUploadModal /> */}
           </Grid>
           <Grid item xs={12}>
