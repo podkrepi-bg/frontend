53994a3e7a80a58521e5dc9fafa855c17c0bb8db
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-03-17 15:50:47 +0200

    new data grid

--- src/components/bootcamp-simeon/BootcampSimeonList.tsx
+++ src/components/bootcamp-simeon/BootcampSimeonList.tsx
@@ -1,7 +1,15 @@
+import AdminContainer from 'components/admin/navigation/AdminContainer'
+import { useTranslation } from 'next-i18next'
 import Layout from './Layout'
 
 function BootcampSimeonList() {
-  return <Layout></Layout>
+  const { t } = useTranslation()
+
+  return (
+    <Layout>
+      <AdminContainer title={t('bootcampSimeon:nav.title')}></AdminContainer>
+    </Layout>
+  )
 }
 
 export default BootcampSimeonList
--- src/pages/bootcamp-simeon/index.tsx
+++ src/pages/bootcamp-simeon/index.tsx
@@ -1,18 +1,13 @@
-import { GetServerSideProps, GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import BootcampSimeonList from 'components/bootcamp-simeon/BootcampSimeonList'
 
-// export const getServerSideProps: GetServerSideProps = async () => ({
-//   props: {
-//     ...(await useBootcampSimeonList()),
-//   },
-// })
-
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale ?? 'bg', ['common'])),
-  },
-  revalidate: 10,
-})
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'bg', ['common'])),
+    },
+  }
+}
 
 export default BootcampSimeonList
