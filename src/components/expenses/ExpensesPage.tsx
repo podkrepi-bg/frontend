d74ed033efe30e8b8de89f2ea63aea54b9c033ad
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-03-17 14:12:13 +0200

    expenses: added grid appbar to main page

--- src/components/expenses/ExpensesPage.tsx
+++ src/components/expenses/ExpensesPage.tsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'next-i18next'
 import AdminLayout from 'components/admin/navigation/AdminLayout'
 import AdminContainer from 'components/admin/navigation/AdminContainer'
 
-import HeadingSeparator from './HeadingSeparator'
 import Grid from './grid/Grid'
+import GridAppbar from './grid/GridAppbar'
 
 export default function ExpensesPage() {
   const { t } = useTranslation('expenses')
@@ -12,7 +12,7 @@ export default function ExpensesPage() {
   return (
     <AdminLayout>
       <AdminContainer title={t('headings.expenses')}>
-        <HeadingSeparator />
+        <GridAppbar />
         <Grid />
       </AdminContainer>
     </AdminLayout>
