f05f530faadd4dcb6e8be9810afd9c6572e0ca47
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-03-18 22:36:42 +0200

    admin: new instance of modal store for every entity

--- src/components/coordinators/CoordinatorsPage.tsx
+++ src/components/coordinators/CoordinatorsPage.tsx
@@ -1,8 +1,11 @@
 import AdminLayout from 'components/admin/navigation/AdminLayout'
 import AdminContainer from 'components/admin/navigation/AdminContainer'
+import { ModalStoreImpl } from 'stores/dashboard/ModalStore'
 
 import CoordinatorsGrid from './grid/CoordinatorsGrid'
 import GridAppbar from './grid/GridAppbar'
+
+export const ModalStore = new ModalStoreImpl()
 
 export default function BankAccountsPage() {
   return (
