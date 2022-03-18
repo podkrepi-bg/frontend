4aa4ed75e4335931a47607ff49dce79411d40868
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-03-18 22:36:42 +0200

    admin: new instance of modal store for every entity

--- src/components/beneficiary/BeneficiaryPage.tsx
+++ src/components/beneficiary/BeneficiaryPage.tsx
@@ -2,9 +2,12 @@ import { useTranslation } from 'next-i18next'
 
 import AdminContainer from 'components/admin/navigation/AdminContainer'
 import AdminLayout from 'components/admin/navigation/AdminLayout'
+import { ModalStoreImpl } from 'stores/dashboard/ModalStore'
 
 import Grid from './grid/Grid'
 import GridAppbar from './grid/GridAppbar'
+
+export const ModalStore = new ModalStoreImpl()
 
 export default function BeneficiaryPage() {
   const { t } = useTranslation('beneficiary')
--- src/components/vaults/VaultsPage.tsx
+++ src/components/vaults/VaultsPage.tsx
@@ -2,8 +2,12 @@ import { useTranslation } from 'next-i18next'
 
 import AdminContainer from 'components/admin/navigation/AdminContainer'
 import AdminLayout from 'components/admin/navigation/AdminLayout'
+import { ModalStoreImpl } from 'stores/dashboard/ModalStore'
+
 import Grid from './grid/Grid'
 import GridAppbar from './grid/GridAppbar'
+
+export const ModalStore = new ModalStoreImpl()
 
 export default function VaultsPage() {
   const { t } = useTranslation('vaults')
