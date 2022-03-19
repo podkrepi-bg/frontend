7eec20deb15f6d63891a5ff273a53966dfd77d22
Kalin <kalink.wow@gmail.com>
2022-03-19 15:41:57 +0200

    remove text "Photos" from AppBar Typography

--- src/components/bootcamp/AppBar.tsx
+++ src/components/bootcamp/AppBar.tsx
@@ -30,9 +30,7 @@ export default function MenuAppBar() {
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
-          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
           {auth && (
             <div>
               <IconButton
