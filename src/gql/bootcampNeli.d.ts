b025bfd34da77cc7a6432b5506a2912c6b236252
nradkova <neliradkova@abv.bg>
2022-03-22 14:05:00 +0200

    gql types created

---
+++ src/gql/bootcampNeli.d.ts
@@ -0,0 +1,25 @@
+export type BootcampNeliType = {
+  id: string
+  firstName: string
+  lastName: string
+  email: string
+}
+
+export type BootcampNeliResponse = {
+  id: string
+  firstName: string
+  lastName: string
+  email: string
+}
+
+export type BootcampNeliInput = {
+  firstName: string
+  lastName: string
+  email: string
+}
+
+export type BootcampNeliFormData = {
+  firstName: string
+  lastName: string
+  email: string
+}
