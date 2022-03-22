34bba9a1954df71f4bb1229509d8612bdb01b54e
Kalin <kalink.wow@gmail.com>
2022-03-22 21:06:10 +0200

    add styles to CreatePageComponet Form

--- src/components/bootcamp/CreatePageComponet/Form.tsx
+++ src/components/bootcamp/CreatePageComponet/Form.tsx
@@ -7,19 +7,26 @@ const Form = () => {
   }
 
   return (
-    <form>
-      <Container sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
-        <TextField label="Име" variant="outlined" />
-        <TextField label="Фамилия" variant="outlined" />
-        <TextField label="Град" variant="outlined" />
-      </Container>
-      <Container>
-        <Button onClick={createHandler} variant="contained">Създай</Button>
-        <Link href="/bootcamp">
-          <Button >Затвори</Button>
-        </Link>
-      </Container>
-    </form>
+    <Container>
+      <form>
+        <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Container
+            sx={{ display: 'flex', flexDirection: 'column', gap: 3, marginBottom: 3, width: 400 }}>
+            <TextField label="Име" variant="outlined" />
+            <TextField label="Фамилия" variant="outlined" />
+            <TextField label="Град" variant="outlined" />
+          </Container>
+          <Container sx={{ width: 400, display: 'flex', justifyContent: 'center' }}>
+            <Button sx={{ marginRight: 3 }} onClick={createHandler} variant="contained">
+              Създай
+            </Button>
+            <Link href="/bootcamp">
+              <Button variant="contained">Затвори</Button>
+            </Link>
+          </Container>
+        </Container>
+      </form>
+    </Container>
   )
 }
 
