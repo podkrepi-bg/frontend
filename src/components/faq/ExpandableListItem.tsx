a63884400bd5390739bd9e153013a6c0a08475f0
Ilko Kacharov <kachar136@gmail.com>
2022-03-25 00:00:03 +0200

    Added mobile detection hook

--- src/components/faq/ExpandableListItem.tsx
+++ src/components/faq/ExpandableListItem.tsx
@@ -1,7 +1,9 @@
-import { Collapse, List, Box, ListItemText, useMediaQuery } from '@mui/material'
+import { Collapse, List, Box, ListItemText } from '@mui/material'
 import { ExpandMore, ExpandLess } from '@mui/icons-material'
 import React, { useState } from 'react'
-import theme from '../../common/theme'
+
+import theme from 'common/theme'
+import useMobile from 'common/hooks/useMobile'
 
 type Props = {
   header: string
@@ -12,14 +14,13 @@ const withAccentColor = (open: boolean) => (open ? theme.palette.primary.main : 
 
 const ExpandableListItem = ({ header, content }: Props) => {
   const [open, setOpen] = useState(false)
-  const isMobile = useMediaQuery('(max-width:900px)')
-  const isVerySmall = useMediaQuery('(max-width:600px)')
+  const { mobile, small } = useMobile()
 
   return (
     <List
       sx={{
-        margin: `0 1rem 1rem ${isVerySmall ? '1' : '2'}rem`,
-        width: `${isMobile ? '85vw' : '60vw'}`,
+        margin: `0 1rem 1rem ${small ? '1' : '2'}rem`,
+        width: `${mobile ? '85vw' : '60vw'}`,
         border: '0.2px',
         borderRadius: '6px',
         boxShadow: '0px 0.5px 1px #888888',
