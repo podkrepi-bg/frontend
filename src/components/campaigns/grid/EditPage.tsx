f6f1481942df525d8baf8ee962dd07b04064a7dd
Ilko Kacharov <kachar136@gmail.com>
2022-03-25 13:30:58 +0200

    Extract currency in own file and patch up campaign edit page

--- src/components/campaigns/grid/EditPage.tsx
+++ src/components/campaigns/grid/EditPage.tsx
@@ -1,14 +1,24 @@
-import EditForm from './EditForm'
 import { Container } from '@mui/material'
+import { useRouter } from 'next/router'
+import { UseQueryResult } from 'react-query'
+
+import { CampaignResponse } from 'gql/campaigns'
+import { useViewCampaignById } from 'common/hooks/campaigns'
 import AdminLayout from 'components/admin/navigation/AdminLayout'
 import AdminContainer from 'components/admin/navigation/AdminContainer'
+import NotFoundIllustration from 'components/errors/assets/NotFoundIllustration'
+
+import EditForm from './EditForm'
 
 export default function EditPage() {
+  const { query } = useRouter()
+  const { data: campaign }: UseQueryResult<CampaignResponse> = useViewCampaignById(String(query.id))
+  if (!campaign) return <NotFoundIllustration />
   return (
     <AdminLayout>
       <AdminContainer title={'Кампании'}>
         <Container maxWidth="md" sx={{ py: 5 }}>
-          <EditForm />
+          <EditForm campaign={campaign} />
         </Container>
       </AdminContainer>
     </AdminLayout>
