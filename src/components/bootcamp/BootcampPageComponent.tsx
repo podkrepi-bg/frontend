96a692eefcfea9e46da48d0961c03f9707e5d539
Kalin <kalink.wow@gmail.com>
2022-03-28 15:46:44 +0300

    add damin menu in bootcamp index page

--- src/components/bootcamp/BootcampPageComponent.tsx
+++ src/components/bootcamp/BootcampPageComponent.tsx
@@ -4,15 +4,20 @@ import TemporaryDrawer from './Drawer'
 import Box from '@mui/material/Box'
 import Footer from 'components/layout/Footer'
 import { boxStyles } from './BootcampPageComponentStyles'
+import AdminLayout from 'components/admin/navigation/AdminLayout'
+import AdminContainer from 'components/admin/navigation/AdminContainer'
 
 const BootcampComponent = () => {
   return (
     <>
       <MenuAppBar />
-      <Box sx={boxStyles}>
-        <TemporaryDrawer />
-        <MyDataGrid />
-      </Box>
+      <AdminLayout>
+        <AdminContainer title={'Bootcamp demo'}>
+          <Box sx={boxStyles}>
+            <MyDataGrid />
+          </Box>
+        </AdminContainer>
+      </AdminLayout>
       <Footer />
     </>
   )
