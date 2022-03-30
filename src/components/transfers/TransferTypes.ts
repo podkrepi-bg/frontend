8fcabbab253f073bebf32588fa6df842c74a711b
nradkova <neliradkova@abv.bg>
2022-03-30 14:25:18 +0300

    custom components

---
+++ src/components/transfers/TransferTypes.ts
@@ -0,0 +1,14 @@
+export const TransferStatus = {
+  initial: 'initial',
+  invalid: 'invalid',
+  incomplete: 'incomplete',
+  declined: 'declined',
+  cancelled: 'cancelled',
+  succeeded: 'succeeded',
+}
+
+export const Currency = {
+  BGN: 'BGN',
+  EUR: 'EUR',
+  USD: 'USD',
+}
