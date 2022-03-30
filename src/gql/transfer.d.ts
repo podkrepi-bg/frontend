212dcb53837827f69219f4c20579f2fc23fe2056
nradkova <neliradkova@abv.bg>
2022-03-30 11:27:30 +0300

    gql types created

---
+++ src/gql/transfer.d.ts
@@ -0,0 +1,48 @@
+import { UUID } from './types'
+import { VaultResponse } from './vault'
+import { PersonResponse } from './person'
+import { CampaignResponse } from './campaigns'
+import { Currency, TransferStatus } from 'components/transfers/TransferTypes'
+
+export type TransferInput = {
+  status: string
+  currency: string
+  amount: number
+  reason: string
+  documentId?: UUID | string | null
+  targetDate?: Date | string | null
+  approvedById?: UUID | string | null
+  sourceCampaignId: UUID | string
+  sourceVaultId: UUID | string
+  targetCampaignId: UUID | string
+  targetVaultId: UUID | string
+}
+
+export type TransferData = {
+  status: string | undefined
+  currency: string | undefined
+  amount: number | undefined
+  reason: string | undefined
+  documentId?: UUID | string | null
+  targetDate?: Date | string | null
+  approvedById?: UUID | string | null
+  sourceCampaignId: UUID | string | undefined
+  sourceVaultId: UUID | string | undefined
+  targetCampaignId: UUID | string | undefined
+  targetVaultId: UUID | string | undefined
+}
+
+export type TransferResponse = {
+  id: UUID
+  status: TransferStatus
+  currency: Currency
+  amount: number
+  reason: string
+  documentId: string | undefined
+  targetDate: Date | undefined
+  approvedBy: PersonResponse | undefined
+  sourceCampaign: CampaignResponse
+  sourceVault: VaultResponse
+  targetCampaign: CampaignResponse
+  targetVault: VaultResponse
+}
