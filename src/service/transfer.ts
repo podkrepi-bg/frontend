2cce033ffaf9183e87e8524dd5ef45e4d7caa688
nradkova <neliradkova@abv.bg>
2022-03-30 11:29:07 +0300

    hooks and rest services created

---
+++ src/service/transfer.ts
@@ -0,0 +1,52 @@
+import { AxiosResponse } from 'axios'
+import { KeycloakInstance } from 'keycloak-js'
+import { useKeycloak } from '@react-keycloak/ssr'
+
+import { apiClient } from 'service/apiClient'
+import { endpoints } from 'service/apiEndpoints'
+import { authConfig } from 'service/restRequests'
+
+import { TransferInput, TransferResponse } from 'gql/transfer'
+
+export const useCreateTransfer = () => {
+  const { keycloak } = useKeycloak<KeycloakInstance>()
+  return async (data: TransferInput) => {
+    return await apiClient.post<TransferInput, AxiosResponse<TransferResponse>>(
+      endpoints.transfer.createTransfer.url,
+      data,
+      authConfig(keycloak?.token),
+    )
+  }
+}
+
+export const useEditTransfer = (id: string) => {
+  const { keycloak } = useKeycloak<KeycloakInstance>()
+  return async (data: TransferInput) => {
+    return await apiClient.put<TransferInput, AxiosResponse<TransferResponse>>(
+      endpoints.transfer.editTransfer(id).url,
+      data,
+      authConfig(keycloak?.token),
+    )
+  }
+}
+
+export const useDeleteTransfer = (id: string) => {
+  const { keycloak } = useKeycloak<KeycloakInstance>()
+  return async () => {
+    return await apiClient.delete<TransferResponse, AxiosResponse<TransferResponse>>(
+      endpoints.transfer.removeTransfer(id).url,
+      authConfig(keycloak?.token),
+    )
+  }
+}
+
+export function useDeleteManyTransfer(idsToDelete: string[]) {
+  const { keycloak } = useKeycloak<KeycloakInstance>()
+  return async () => {
+    return await apiClient.post<TransferResponse, AxiosResponse<TransferResponse>>(
+      endpoints.transfer.removeManyTransfer.url,
+      idsToDelete,
+      authConfig(keycloak?.token),
+    )
+  }
+}
