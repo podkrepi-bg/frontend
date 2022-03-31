b6e7e504bfdae3e8ba8a1f0659fb2635e76a7d5a
borislavstoychev <stoy4ew@gmail.com>
2022-03-31 15:38:50 +0300

    :art: success page

--- src/components/one-time-donation/components/Steps.tsx
+++ src/components/one-time-donation/components/Steps.tsx
@@ -7,11 +7,23 @@ import StepContent from '@mui/material/StepContent'
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
-import { Grid, Stack } from '@mui/material'
+import {
+  Divider,
+  Grid,
+  Stack,
+  StepButton,
+  StepConnector,
+  stepConnectorClasses,
+} from '@mui/material'
 import FirstStep from './FirstStep'
 import SecondStep from './SecondStep'
 import ThirdStep from './ThirdStep'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import Success from './Success'
+import { SupervisorAccount } from '@mui/icons-material'
+import SuccesButtons from './SuccessButtons'
+import { styled } from '@mui/system'
+import { makeStyles } from '@mui/styles'
 const steps = [
   {
     label: 'First Step',
@@ -26,15 +38,35 @@ const steps = [
     description: <ThirdStep />,
   },
   {
-    label: 'Create an ad',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
+    label: 'Success',
+    description: <Success />,
   },
 ]
-
+const useStyles = makeStyles(() => ({
+  customLabelStyle: {
+    transform: 'scale(1.85)',
+  },
+  btnBack: {
+    background: 'rgba(255, 255, 255, 0.85)',
+    border: '1px solid #000000',
+    boxSizing: 'border-box',
+    borderRadius: '60px',
+    width: '284px',
+    height: '80px',
+    fontSize: '22px',
+  },
+  btn: {
+    background: '#62C4FB',
+    border: '1px solid #000000',
+    boxSizing: 'border-box',
+    borderRadius: '60px',
+    width: '284px',
+    height: '80px',
+    fontSize: '22px',
+  },
+}))
 export default function VerticalLinearStepper() {
+  const classes = useStyles()
   const [activeStep, setActiveStep] = React.useState(0)
 
   const handleNext = () => {
@@ -50,45 +82,30 @@ export default function VerticalLinearStepper() {
   }
 
   return (
-    <Box mx={25} sx={{ marginTop: 5 }}>
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map((step) => (
-          <Step key={step.label}>
-            <StepLabel>{null}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
+    <Box>
+      <Grid my={5}>
+        <Stepper nonLinear activeStep={activeStep} connector={<StepConnector />} alternativeLabel>
+          {steps.map((step) => (
+            <Step key={step.label}>
+              <StepLabel classes={{ iconContainer: classes.customLabelStyle }}>{null}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Grid>
       {steps[activeStep].description}
-      <Grid my={7} direction="row">
+      <Grid container justifyContent="space-between" mx={25} xs={6.5} my={7} direction="row">
         <Button
           disabled={activeStep === 0}
           onClick={handleBack}
           color="inherit"
-          sx={{
-            background: 'rgba(255, 255, 255, 0.85)',
-            border: '1px solid #000000',
-            boxSizing: 'border-box',
-            borderRadius: '60px',
-            width: '284px',
-            height: '80px',
-            marginRight: '51px',
-            fontSize: '22px',
-          }}>
+          className={classes.btnBack}>
           Назад
         </Button>
         <Button
           variant="contained"
           color="inherit"
           onClick={handleNext}
-          sx={{
-            background: '#62C4FB',
-            border: '1px solid #000000',
-            boxSizing: 'border-box',
-            borderRadius: '60px',
-            width: '284px',
-            height: '80px',
-            fontSize: '22px',
-          }}
+          className={classes.btn}
           endIcon={<ArrowForwardIosIcon />}>
           {activeStep === steps.length - 1 ? 'Finish' : 'Напред'}
         </Button>
