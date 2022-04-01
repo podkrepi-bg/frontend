992b799f2e3515c6d136a3fc9bc7a35e94ae7602
Kalin <kalink.wow@gmail.com>
2022-04-01 07:47:13 +0300

    clear BootcampPageComponent from unnecessary  imports

--- src/components/bootcamp/BootcampPageComponent.tsx
+++ src/components/bootcamp/BootcampPageComponent.tsx
@@ -1,8 +1,3 @@
-import MyDataGrid from './grid'
-import TemporaryDrawer from './Drawer'
-import Box from '@mui/material/Box'
-import Footer from 'components/layout/Footer'
-import { boxStyles } from './BootcampPageComponentStyles'
 import AdminLayout from 'components/admin/navigation/AdminLayout'
 import AdminContainer from 'components/admin/navigation/AdminContainer'
 import GridAppbar from './grid/GridAppbar'
