2125704843766a0d160cd3d8a5b104f940472449
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-04-02 00:37:19 +0300

    expenses: PersonSelect

---
+++ src/components/expenses/PersonSelect.tsx
@@ -0,0 +1,44 @@
+import { useTranslation } from 'react-i18next'
+import { FormControl, MenuItem } from '@mui/material'
+import { useField } from 'formik'
+
+import { usePersonList } from 'common/hooks/person'
+import FormTextField from 'components/common/form/FormTextField'
+
+type Props = {
+  name: string
+}
+
+export default function PersonSelect({ name }: Props) {
+  const { t } = useTranslation('expenses')
+  const { data: personList } = usePersonList()
+  const [field, meta] = useField(name)
+
+  return (
+    <FormControl
+      fullWidth
+      size="small"
+      variant="outlined"
+      error={Boolean(meta.error) && Boolean(meta.touched)}>
+      <FormTextField
+        select
+        type="text"
+        fullWidth
+        defaultValue=""
+        label={t('fields.' + name)}
+        {...field}>
+        <MenuItem value="" disabled>
+          {t('fields.' + name)}
+        </MenuItem>
+        <MenuItem key={'none'} value="">
+          <i>{t('fields.empty')}</i>
+        </MenuItem>
+        {personList?.map((person, index) => (
+          <MenuItem key={index} value={person.id}>
+            {person.firstName} {person.lastName} (<i>{person.id}</i>)
+          </MenuItem>
+        ))}
+      </FormTextField>
+    </FormControl>
+  )
+}
