f4c290ba10fec120b52abe19d547f0563e65bd37
Kalin <kalink.wow@gmail.com>
2022-04-01 16:05:45 +0300

    fix a bug in bootcamp index page

--- src/pages/bootcamp/index.tsx
+++ src/pages/bootcamp/index.tsx
@@ -1,20 +1,31 @@
 import BootcampComponent from 'components/bootcamp/BootcampPageComponent'
-import { securedProps } from 'middleware/auth/keycloak'
+import { keycloakInstance } from 'middleware/auth/keycloak'
 import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { dehydrate, QueryClient } from 'react-query'
+import { endpoints } from 'service/apiEndpoints'
+import { authQueryFnFactory } from 'service/restRequests'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale ?? 'bg', [
-      'common',
-      'auth',
-      'validation',
-      'bootcamp',
-      'admin',
-      'documents',
-    ])),
-  },
-})
+export const getServerSideProps: GetServerSideProps = async (params) => {
+  const client = new QueryClient()
+  const keycloak = keycloakInstance(params)
+  // await client.prefetchQuery(endpoints.benefactor.benefactorList.url, queryFn)
+  await client.prefetchQuery(endpoints.bootcamp.getAll.url, authQueryFnFactory(keycloak.token))
+
+  return {
+    props: {
+      ...(await serverSideTranslations(params.locale ?? 'bg', [
+        'common',
+        'auth',
+        'validation',
+        'bootcamp',
+        'admin',
+        'documents',
+      ])),
+      dehydratedState: dehydrate(client),
+    },
+  }
+}
 
 const BootcampPage = () => {
   return <BootcampComponent />
