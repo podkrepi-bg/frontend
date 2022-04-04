299794c3bc09910ba9b81cf2dab2f05ce79b0e36
Ilko Kacharov <kachar136@gmail.com>
2022-04-05 00:42:12 +0300

    Pass field props to the transfer status select instead of manual on change handler

--- src/components/transfers/custom/TransferStatusSelect.tsx
+++ src/components/transfers/custom/TransferStatusSelect.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import { useField } from 'formik'
 import { useTranslation } from 'next-i18next'
-import { useField, useFormikContext } from 'formik'
 
 import { MenuItem, TextField, TextFieldProps } from '@mui/material'
 
@@ -14,17 +14,11 @@ type selectProps = {
   name: string
 } & TextFieldProps
 
-export default function SelectStatus({ label, name, ...textFieldProps }: selectProps) {
+export default function TransferStatusSelect({ label, name, ...textFieldProps }: selectProps) {
   const { t } = useTranslation('transfer')
-
   const [field, meta] = useField(name)
-  const { setFieldValue } = useFormikContext()
-
   const helperText = meta.touched ? translateError(meta.error as TranslatableField, t) : ''
 
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setFieldValue(name, event.target.value)
-  }
   return (
     <TextField
       {...textFieldProps}
@@ -32,7 +26,6 @@ export default function SelectStatus({ label, name, ...textFieldProps }: selectP
       select
       variant="outlined"
       fullWidth
-      onChange={handleChange}
       label={t(label)}
       error={Boolean(meta.error) && Boolean(meta.touched)}
       helperText={helperText}>
