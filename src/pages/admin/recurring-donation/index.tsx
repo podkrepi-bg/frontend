226739e8619ea1d773ba08d77bfdf4ea35137f8f
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2022-04-07 16:04:45 +0100

    Added Recurring Donation Page

--- src/pages/admin/recurring-donation/index.tsx
+++ src/pages/admin/recurring-donation/index.tsx
@@ -3,14 +3,11 @@ import { dehydrate, QueryClient } from 'react-query'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { keycloakInstance } from 'middleware/auth/keycloak'
-// import { prefetchVaultsList } from 'common/hooks/vaults'
 import RecurringDonationPage from 'components/recurring-donation/RecurringDonationPage'
 
 export const getServerSideProps: GetServerSideProps = async (params) => {
   const client = new QueryClient()
   const keycloak = keycloakInstance(params)
-
-  //   await prefetchVaultsList(client, keycloak?.token)
 
   return {
     props: {
