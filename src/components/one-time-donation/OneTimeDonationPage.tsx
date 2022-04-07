516fe58d010e9cc27165469e9c64ddfa56369c9f
borislavstoychev <stoy4ew@gmail.com>
2022-04-07 16:17:49 +0300

    :art:

--- src/components/one-time-donation/OneTimeDonationPage.tsx
+++ src/components/one-time-donation/OneTimeDonationPage.tsx
@@ -8,61 +8,25 @@ import {} from 'path'
 import { UseQueryResult } from 'react-query'
 import { CampaignResponse } from 'gql/campaigns'
 import { useViewCampaign } from 'common/hooks/campaigns'
-import HorizontalLabelPositionBelowStepper from './components/Steps'
+import DonationStepper from './components/Steps'
 import { useRouter } from 'next/router'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    banner: {
-      position: 'absolute',
-      background: 'rgba(0, 0, 0, 0.4)',
-    },
     title: {
-      // position: 'absolute',
-      // width: '827px',
-      // height: '100px',
-      // right: '207px',
-      // top: '492px',
       fontFamily: 'Montserrat',
       fontStyle: 'normal',
-      // fontWeight: '500',
       fontSize: '45px',
       lineHeight: '50px',
-      /* or 111% */
       textAlign: 'center',
       letterSpacing: '-1.5px',
       color: '#000000',
-    },
-    stepper: {
-      position: 'absolute',
-      left: '30.96%',
-      right: '68.28%',
-      top: '23.06%',
-      bottom: '76.06%',
-
-      fontFamily: 'Open Sans',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      fontSize: '18px',
-      lineHeight: '25px',
-
-      /* identical to box height */
-
-      color: '#000000',
-    },
-    avatarWrapper: {
-      position: 'absolute',
-      left: '750px',
-      top: '275px',
     },
     avatar: {
       borderRadius: '50%',
       border: `10px solid ${theme.palette.common.white} !important`,
       textAlign: 'center',
-    },
-    infoWrapper: {
-      gap: theme.spacing(2),
-      display: 'grid',
+      maxWidth: '307',
     },
   }),
 )
@@ -76,17 +40,18 @@ export default function OneTimeDonation() {
 
   return (
     <>
-      <Grid sx={{ paddingTop: '112px' }}>
-        <Image
-          sizes="100%"
-          objectFit="cover"
-          width="1440"
-          height="272"
-          className={classes.banner}
-          alt="The house from the offer."
-          src="/img/campaign-banner.png"
-        />
-        <Grid className={classes.avatarWrapper}>
+      <Grid sx={{ pt: 10 }}>
+        <Grid sx={{ minHeight: '182px' }}>
+          <Image
+            sizes="100%"
+            objectFit="cover"
+            width="1440px"
+            height="320px"
+            alt="The house from the offer."
+            src="/img/campaign-banner.png"
+          />
+        </Grid>
+        <Grid sx={{ maxWidth: '307px', ml: 'auto', mr: 'auto', mt: '-180px' }}>
           <Image
             width="307"
             height="307"
@@ -95,10 +60,10 @@ export default function OneTimeDonation() {
             src="/img/campaign-banner.png"
           />
         </Grid>
+        <Typography className={classes.title}>{data?.campaign.title}</Typography>
       </Grid>
       <Layout>
-        <Typography className={classes.title}>{data?.campaign.title}</Typography>
-        <HorizontalLabelPositionBelowStepper />
+        <DonationStepper />
       </Layout>
     </>
   )
