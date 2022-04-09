a4f05826e28ca3cc265546f5fab7ba827d008d52
dplamenov <dimitar.plamenov@gmail.com>
2022-04-09 09:26:22 +0300

    use latest version of api

--- src/components/auth/profile/PersonalInfoTab.tsx
+++ src/components/auth/profile/PersonalInfoTab.tsx
@@ -68,7 +68,7 @@ const useStyles = makeStyles({
 function PersonalInfoTab(props: { value: number; index: number }) {
   const { value, index } = props
   const { session } = useSession()
-  const { data: person, refetch } = useCurrentPerson()
+  const { data: { user: person } = { user: null }, refetch } = useCurrentPerson()
   const [isDeleteAccountModalOpen, setIsDeleteAccountModalOpen] = useState(false)
   const [isUpdateNameModalOpen, setIsUpdateNameModalOpen] = useState(false)
   const [isUpdateBirthdayModalOpen, setIsUpdateBirthdayModalOpen] = useState(false)
