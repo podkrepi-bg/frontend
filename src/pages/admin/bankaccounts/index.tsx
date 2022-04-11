9fa91f3c5d2d3a86372dc8cac1e624e813e04b82
borislavstoychev <stoy4ew@gmail.com>
2022-04-11 18:45:44 +0300

    :lock: bank accounts

--- src/pages/admin/bankaccounts/index.tsx
+++ src/pages/admin/bankaccounts/index.tsx
@@ -1,31 +1,11 @@
 import BankAccountsPage from 'components/bankaccounts/BankAccountsPage'
+import { securedPropsWithTranslation } from 'middleware/auth/keycloak'
 
-import { dehydrate, QueryClient } from 'react-query'
-import { GetServerSideProps } from 'next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-import { endpoints } from 'service/apiEndpoints'
-import { authQueryFnFactory } from 'service/restRequests'
-import { keycloakInstance } from 'middleware/auth/keycloak'
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const keycloak = keycloakInstance(ctx)
-  const client = new QueryClient()
-  await client.prefetchQuery(
-    endpoints.bankAccounts.bankAccountList.url,
-    authQueryFnFactory(keycloak.token),
-  )
-  return {
-    props: {
-      ...(await serverSideTranslations(ctx.locale ?? 'bg', [
-        'common',
-        'auth',
-        'validation',
-        'admin',
-        'bankaccounts',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
+export const getServerSideProps = securedPropsWithTranslation([
+  'common',
+  'auth',
+  'validation',
+  'admin',
+  'bankaccounts',
+])
 export default BankAccountsPage
