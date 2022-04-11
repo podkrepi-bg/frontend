9f9ef0fa3e0f98051b535fa6830bdb47e407b769
borislavstoychev <stoy4ew@gmail.com>
2022-04-11 18:35:21 +0300

    :lock: benefactor

--- src/pages/admin/benefactor/[id]/index.tsx
+++ src/pages/admin/benefactor/[id]/index.tsx
@@ -1,30 +1,11 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { keycloakInstance } from 'middleware/auth/keycloak'
-import { authQueryFnFactory } from 'service/restRequests'
+import { securedPropsWithTranslation } from 'middleware/auth/keycloak'
 import BenefactorEditPage from 'components/benefactor/BenefactorEditPage'
-import { endpoints } from 'service/apiEndpoints'
 
-export const getServerSideProps: GetServerSideProps = async (params) => {
-  const keycloak = keycloakInstance(params)
-  const client = new QueryClient()
-  await client.prefetchQuery(
-    `${endpoints.benefactor.getBenefactor}/${params.query.id}`,
-    authQueryFnFactory(keycloak.token),
-  )
-
-  return {
-    props: {
-      ...(await serverSideTranslations(params.locale ?? 'bg', [
-        'common',
-        'auth',
-        'benefactor',
-        'validation',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
+export const getServerSideProps = securedPropsWithTranslation([
+  'common',
+  'auth',
+  'benefactor',
+  'validation',
+])
 
 export default BenefactorEditPage
