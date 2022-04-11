0175b2dbd505f5707174bf58f31b7b4728d7b6bb
borislavstoychev <stoy4ew@gmail.com>
2022-04-11 18:52:41 +0300

    :lock: beneficiary

--- src/pages/admin/beneficiary/index.tsx
+++ src/pages/admin/beneficiary/index.tsx
@@ -1,30 +1,12 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
 import DocumentsPage from 'components/beneficiary/BeneficiaryPage'
-import { prefetchBeneficiariesList } from 'service/beneficiary'
-import { keycloakInstance } from 'middleware/auth/keycloak'
+import { securedPropsWithTranslation } from 'middleware/auth/keycloak'
 
-export const getServerSideProps: GetServerSideProps = async (params) => {
-  const client = new QueryClient()
-  const keycloak = keycloakInstance(params)
-
-  await prefetchBeneficiariesList(client, keycloak?.token)
-
-  return {
-    props: {
-      ...(await serverSideTranslations(params.locale ?? 'bg', [
-        'common',
-        'auth',
-        'beneficiary',
-        'validation',
-        'admin',
-        'documents',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
-
+export const getServerSideProps = securedPropsWithTranslation([
+  'common',
+  'auth',
+  'beneficiary',
+  'validation',
+  'admin',
+  'documents',
+])
 export default DocumentsPage
