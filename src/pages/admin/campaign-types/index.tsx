fba9fff60f9343d5606eaa3e6bab475bb340e215
borislavstoychev <stoy4ew@gmail.com>
2022-04-11 18:58:51 +0300

    :lock: campaign types

--- src/pages/admin/campaign-types/index.tsx
+++ src/pages/admin/campaign-types/index.tsx
@@ -1,30 +1,13 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
 import CampaignTypesPage from 'components/campaign-types/CampaignTypesPage'
-import { prefetchCampaignTypesList } from 'service/campaignTypes'
-import { keycloakInstance } from 'middleware/auth/keycloak'
+import { securedPropsWithTranslation } from 'middleware/auth/keycloak'
 
-export const getServerSideProps: GetServerSideProps = async (params) => {
-  const client = new QueryClient()
-  const keycloak = keycloakInstance(params)
-
-  await prefetchCampaignTypesList(client, keycloak?.token)
-
-  return {
-    props: {
-      ...(await serverSideTranslations(params.locale ?? 'bg', [
-        'common',
-        'auth',
-        'campaign-types',
-        'validation',
-        'admin',
-        'documents',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
+export const getServerSideProps = securedPropsWithTranslation([
+  'common',
+  'auth',
+  'campaign-types',
+  'validation',
+  'admin',
+  'documents',
+])
 
 export default CampaignTypesPage
