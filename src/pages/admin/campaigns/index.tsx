0e89bb5245e8462ae96183f900d88946059114e7
borislavstoychev <stoy4ew@gmail.com>
2022-04-11 19:04:18 +0300

    :lock: campaigns

--- src/pages/admin/campaigns/index.tsx
+++ src/pages/admin/campaigns/index.tsx
@@ -1,25 +1,10 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-import { queryFn } from 'service/restRequests'
-import { endpoints } from 'service/apiEndpoints'
 import CampaignsPage from 'components/campaigns/grid/CampaignPage'
+import { securedPropsWithTranslation } from 'middleware/auth/keycloak'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const client = new QueryClient()
-  await client.prefetchQuery(endpoints.campaign.listCampaigns.url, queryFn)
-  return {
-    props: {
-      ...(await serverSideTranslations(locale ?? 'bg', [
-        'common',
-        'auth',
-        'validation',
-        'campaigns',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
-
+export const getServerSideProps = securedPropsWithTranslation([
+  'common',
+  'auth',
+  'validation',
+  'campaigns',
+])
 export default CampaignsPage
