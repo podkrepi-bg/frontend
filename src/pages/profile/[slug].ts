e9719873c7347a7e23da92d403762ca7884e9340
dimitar.nizamov <dimitur2204@gmail.com>
2022-04-12 08:33:50 +0200

    Add translations to profile tabs page

--- src/pages/profile/[slug].ts
+++ src/pages/profile/[slug].ts
@@ -1,13 +1,15 @@
 import { GetServerSideProps } from 'next'
 import { securedProps } from 'middleware/auth/keycloak'
 import ProfilePage from 'components/auth/profile/ProfilePage'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { slug } = ctx.query
   return {
     props: {
       slug,
-      ...securedProps(ctx),
+      ...(await serverSideTranslations(ctx.locale ?? 'bg', ['common', 'index'])),
+      ...(await securedProps(ctx)),
     },
   }
 }
