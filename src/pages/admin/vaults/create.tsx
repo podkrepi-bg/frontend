c3822ea4585713a74e257a1e1dcdb322702a96c5
borislavstoychev <stoy4ew@gmail.com>
2022-04-13 12:01:06 +0300

    :lock: vaults

--- src/pages/admin/vaults/create.tsx
+++ src/pages/admin/vaults/create.tsx
@@ -1,18 +1,11 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
 import CreatePage from 'components/vaults/CreatePage'
+import { securedPropsWithTranslation } from 'middleware/auth/keycloak'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const client = new QueryClient()
-
-  return {
-    props: {
-      ...(await serverSideTranslations(locale ?? 'bg', ['common', 'auth', 'vaults', 'validation'])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
+export const getServerSideProps = securedPropsWithTranslation([
+  'common',
+  'auth',
+  'vaults',
+  'validation',
+])
 
 export default CreatePage
