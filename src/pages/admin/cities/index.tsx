85de1745c8f5be7473bec5c41de575cd29e6590c
borislavstoychev <stoy4ew@gmail.com>
2022-04-13 10:13:49 +0300

    :lock: cities

--- src/pages/admin/cities/index.tsx
+++ src/pages/admin/cities/index.tsx
@@ -1,28 +1,10 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-import { endpoints } from 'service/apiEndpoints'
 import CityPage from 'components/cities/CityPage'
-import { authQueryFnFactory } from 'service/restRequests'
-import { keycloakInstance } from 'middleware/auth/keycloak'
+import { securedAdminProps } from 'middleware/auth/keycloak'
+import { endpoints } from 'service/apiEndpoints'
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const keycloak = keycloakInstance(ctx)
-  const client = new QueryClient()
-  await client.prefetchQuery(endpoints.city.citiesList.url, authQueryFnFactory(keycloak.token))
-  return {
-    props: {
-      ...(await serverSideTranslations(ctx.locale ?? 'bg', [
-        'common',
-        'auth',
-        'validation',
-        'admin',
-        'cities',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
+export const getServerSideProps = securedAdminProps(
+  ['common', 'auth', 'validation', 'admin', 'cities'],
+  () => endpoints.city.citiesList.url,
+)
 
 export default CityPage
