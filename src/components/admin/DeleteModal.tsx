c011252eed8b4f30e23fd6bd457e7b4e960d5529
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2022-04-13 09:18:32 +0300

    admin: DeleteModal, pass ModalStore instance from props

--- src/components/admin/DeleteModal.tsx
+++ src/components/admin/DeleteModal.tsx
@@ -4,18 +4,24 @@ import { observer } from 'mobx-react'
 import { AxiosError, AxiosResponse } from 'axios'
 import { useTranslation } from 'next-i18next'
 import { ApiErrors } from 'service/apiErrors'
-import { ModalStore } from 'stores/dashboard/ModalStore'
+import { ModalStoreImpl } from 'stores/dashboard/ModalStore'
 import { AlertStore } from 'stores/AlertStore'
 import DeleteDialog from 'components/admin/DeleteDialog'
 
 type Props = {
+  modalStore: ModalStoreImpl
   mutationFn: (id: string) => Promise<AxiosResponse>
   translate: string //translate exemple: 'file_name:alerts.error', 'file_name:'alerts.delete
   endpoint: string
 }
 
-export default observer(function DeleteModal({ mutationFn, translate, endpoint }: Props) {
-  const { hideDelete, selectedRecord } = ModalStore
+export default observer(function DeleteModal({
+  modalStore,
+  mutationFn,
+  translate,
+  endpoint,
+}: Props) {
+  const { hideDelete, selectedRecord } = modalStore
   const { t } = useTranslation(translate)
   const queryClient = useQueryClient()
 
@@ -33,5 +39,5 @@ export default observer(function DeleteModal({ mutationFn, translate, endpoint }
     deleteMutation.mutate(selectedRecord.id)
   }
 
-  return <DeleteDialog modalStore={ModalStore} deleteHandler={deleteHandler} />
+  return <DeleteDialog modalStore={modalStore} deleteHandler={deleteHandler} />
 })
