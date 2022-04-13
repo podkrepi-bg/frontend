8d083dcc3f334b9766a7e92397aed754fbaf2927
borislavstoychev <stoy4ew@gmail.com>
2022-04-13 11:38:49 +0300

    :lock: expenses

--- src/pages/admin/expenses/index.tsx
+++ src/pages/admin/expenses/index.tsx
@@ -1,33 +1,10 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-import { keycloakInstance } from 'middleware/auth/keycloak'
+import { securedAdminProps } from 'middleware/auth/keycloak'
 import { endpoints } from 'service/apiEndpoints'
-import { authQueryFnFactory } from 'service/restRequests'
 import ExpensesPage from 'components/expenses/ExpensesPage'
 
-export const getServerSideProps: GetServerSideProps = async (params) => {
-  const client = new QueryClient()
-  const keycloak = keycloakInstance(params)
-
-  await client.prefetchQuery(
-    endpoints.expenses.listExpenses.url,
-    authQueryFnFactory(keycloak.token),
-  )
-
-  return {
-    props: {
-      ...(await serverSideTranslations(params.locale ?? 'bg', [
-        'common',
-        'auth',
-        'validation',
-        'admin',
-        'expenses',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
+export const getServerSideProps = securedAdminProps(
+  ['common', 'auth', 'validation', 'admin', 'expenses'],
+  () => endpoints.expenses.listExpenses.url,
+)
 
 export default ExpensesPage
