b782aad2e0618982ce10a3a69f39d14113bc1014
borislavstoychev <stoy4ew@gmail.com>
2022-04-13 11:34:46 +0300

    :lock: donations

--- src/pages/admin/donations/create.tsx
+++ src/pages/admin/donations/create.tsx
@@ -1,23 +1,10 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
 import CreatePage from 'components/donations/CreatePage'
+import { securedPropsWithTranslation } from 'middleware/auth/keycloak'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const client = new QueryClient()
-
-  return {
-    props: {
-      ...(await serverSideTranslations(locale ?? 'bg', [
-        'common',
-        'auth',
-        'donations',
-        'validation',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
-
+export const getServerSideProps = securedPropsWithTranslation([
+  'common',
+  'auth',
+  'donations',
+  'validation',
+])
 export default CreatePage
