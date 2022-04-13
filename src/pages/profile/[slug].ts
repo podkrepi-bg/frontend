665012c8201e08f8867018195c9278f7ac1c64b1
dimitar.nizamov <dimitur2204@gmail.com>
2022-04-13 15:23:52 +0300

    Remove passing of slug prop to ProfilePage

--- src/pages/profile/[slug].ts
+++ src/pages/profile/[slug].ts
@@ -4,10 +4,8 @@ import ProfilePage from 'components/auth/profile/ProfilePage'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { slug } = ctx.query
   return {
     props: {
-      slug,
       ...(await serverSideTranslations(ctx.locale ?? 'bg', ['common', 'index'])),
       ...(await securedProps(ctx)),
     },
