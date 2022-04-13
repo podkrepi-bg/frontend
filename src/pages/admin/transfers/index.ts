4e771a5e17176a615c588f6bd42fc9e21201a1bd
borislavstoychev <stoy4ew@gmail.com>
2022-04-13 11:43:44 +0300

    :lock: transfer

--- src/pages/admin/transfers/index.ts
+++ src/pages/admin/transfers/index.ts
@@ -1,29 +1,10 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-import { keycloakInstance } from 'middleware/auth/keycloak'
-import { prefetchTransferList } from 'common/hooks/transfers'
-
+import { securedAdminProps } from 'middleware/auth/keycloak'
 import TransferPage from 'components/transfers/TransferPage'
+import { endpoints } from 'service/apiEndpoints'
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const keycloak = keycloakInstance(ctx)
-  const client = new QueryClient()
-  await prefetchTransferList(client, keycloak?.token)
-
-  return {
-    props: {
-      ...(await serverSideTranslations(ctx.locale ?? 'bg', [
-        'common',
-        'auth',
-        'validation',
-        'transfer',
-        'admin',
-      ])),
-      dehydratedState: dehydrate(client),
-    },
-  }
-}
+export const getServerSideProps = securedAdminProps(
+  ['common', 'auth', 'validation', 'transfer', 'admin'],
+  () => endpoints.transfer.listTransfer.url,
+)
 
 export default TransferPage
