4eca962cb8ee0d0cf88a45e45f0cb8f2a32e76f6
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2022-04-14 10:52:00 +0100

    added  useTranslation

--- src/components/recurring-donation/grid/GridAppbar.tsx
+++ src/components/recurring-donation/grid/GridAppbar.tsx
@@ -27,7 +27,7 @@ export default function GridAppbar() {
         height: '72px',
       }}>
       <Box sx={{ height: '64px', display: 'flex', alignItems: 'start', pt: 1 }}>
-        <Typography>{t('recurring-donation:recurring-donation')}</Typography>
+        <Typography>{t('recurring-donation:recurring-donations')}</Typography>
       </Box>
       <Box sx={{ height: '64px', display: 'flex', alignItems: 'flex-end', pb: 1 }}>
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
