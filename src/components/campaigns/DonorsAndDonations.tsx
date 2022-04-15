345635cf68f02458e7d472f36615023f5217903d
dimitar.nizamov <dimitur2204@gmail.com>
2022-04-15 03:12:58 +0300

    Make see all button a toggle

--- src/components/campaigns/DonorsAndDonations.tsx
+++ src/components/campaigns/DonorsAndDonations.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Grid, Theme, Typography } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
@@ -14,6 +14,8 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     donationsWrapper: {
       marginTop: theme.spacing(5),
+      maxHeight: 400,
+      overflowY: 'scroll',
     },
     donationItemWrapper: {
       display: 'flex',
@@ -43,40 +45,48 @@ export default function DonorsAndDonations({
 }) {
   const classes = useStyles()
   const { t } = useTranslation()
-  const [donationsToShow, setDonationsToShow] = useState<CampaignDonation[] | undefined>(
-    donations?.slice(0, 1),
-  )
+  const [donationsToShow, setDonationsToShow] = useState<CampaignDonation[] | undefined>(undefined)
+  const [all, setAll] = useState<boolean>(false)
+  useEffect(() => {
+    all ? setDonationsToShow(donations) : setDonationsToShow(donations?.slice(0, 1))
+  }, [donations, all])
   return (
-    <Grid item className={classes.donationsWrapper}>
-      {donationsToShow && donationsToShow.length !== 0 ? (
-        donationsToShow.map(({ person, amount, createdAt }, key) => (
-          <Grid key={key} className={classes.donationItemWrapper}>
-            <AccountCircleIcon fontSize="large" color="disabled" />
-            <Grid>
-              <Typography>
-                {t('campaigns:cta.donor')} {key + 1}.{' '}
-                {person
-                  ? person.firstName + ' ' + person.lastName
-                  : t('campaigns:donations.anonymous')}
-              </Typography>
-              <Grid className={classes.donationQuantityAndTimeWrapper}>
+    <>
+      <Grid item className={classes.donationsWrapper}>
+        {donationsToShow && donationsToShow.length !== 0 ? (
+          donationsToShow.map(({ person, amount, createdAt }, key) => (
+            <Grid key={key} className={classes.donationItemWrapper}>
+              <AccountCircleIcon fontSize="large" color="disabled" />
+              <Grid>
                 <Typography>
-                  {Math.round(amount / 100) + ' ' + t('campaigns:donations.lv')}
+                  {t('campaigns:cta.donor')} {key + 1}.{' '}
+                  {person
+                    ? person.firstName + ' ' + person.lastName
+                    : t('campaigns:donations.anonymous')}
                 </Typography>
-                <FiberManualRecordIcon className={classes.separatorIcon} />
-                <Typography>{formatDuration(getDurationUntilNow(parseISO(createdAt)))}</Typography>
+                <Grid className={classes.donationQuantityAndTimeWrapper}>
+                  <Typography>
+                    {(amount / 100).toFixed(2) + ' ' + t('campaigns:donations.lv')}
+                  </Typography>
+                  <FiberManualRecordIcon className={classes.separatorIcon} />
+                  <Typography>
+                    {formatDuration(getDurationUntilNow(parseISO(createdAt)))}
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        ))
-      ) : (
-        <Typography sx={{ textAlign: 'center', marginBottom: theme.spacing(4) }}>
-          {t('campaigns:donations.none')}
-        </Typography>
-      )}
-      <Button onClick={() => setDonationsToShow(donations)} className={classes.seeAllButton}>
-        {t('campaigns:cta.see-all')}
-      </Button>
-    </Grid>
+          ))
+        ) : (
+          <Typography sx={{ textAlign: 'center', marginBottom: theme.spacing(4) }}>
+            {t('campaigns:donations.none')}
+          </Typography>
+        )}
+      </Grid>
+      <Grid>
+        <Button onClick={() => setAll((prev) => !prev)} className={classes.seeAllButton}>
+          {all ? t('campaigns:cta.see-less') : t('campaigns:cta.see-all')}
+        </Button>
+      </Grid>
+    </>
   )
 }
