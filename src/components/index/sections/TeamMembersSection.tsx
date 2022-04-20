b8ee32db1c3f9a75b22f86ee52104f51020d3e30
Angelina <62725741+angelina-7@users.noreply.github.com>
2022-04-20 16:55:41 +0300

    added team member cards

--- src/components/index/sections/TeamMembersSection.tsx
+++ src/components/index/sections/TeamMembersSection.tsx
@@ -1,10 +1,13 @@
 import { Box, Grid, Typography } from '@mui/material'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import { useTranslation } from 'next-i18next'
 
 import theme from 'common/theme'
 import Heading from 'components/common/Heading'
 import MemberCard from '../helpers/teamMembers/MemberCard'
 import { data } from '../helpers/teamMembers/memberData'
+import { staticUrls } from 'common/routes'
+import LinkButton from 'components/common/LinkButton'
 
 export default function TeamMembersSection() {
   const { t } = useTranslation()
@@ -27,12 +30,26 @@ export default function TeamMembersSection() {
         от целта да създадем устойчива и прозрачна платформа за дарения, която подкрепя каузи и хора
         в нужда, като заедно с това популяризира и връща доверието към дарителството в България.
       </Typography>
-      <Grid container flexDirection="column">
+      <Grid
+        container
+        justifyContent="center"
+        spacing={6}
+        paddingTop={theme.spacing(7)}
+        paddingBottom={theme.spacing(12)}>
         {data.map((x, index) => (
-          <Box display="flex" alignItems="center" flexDirection="column" key={index}>
-            <MemberCard info={x} />
-          </Box>
+          <Grid key={index} item xs={12} sm={6} lg={4} paddingBottom={theme.spacing(7)}>
+            <Box textAlign="center">
+              <MemberCard info={x} />
+            </Box>
+          </Grid>
         ))}
+        <LinkButton
+          href={staticUrls.blog}
+          variant="outlined"
+          endIcon={<ChevronRightIcon />}
+          sx={{ marginTop: '2rem' }}>
+          {'Запознай се с екипа ни'}
+        </LinkButton>
       </Grid>
     </>
   )
