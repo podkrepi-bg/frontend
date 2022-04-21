b279de79accf63e4384dfbc0b5e5d7bbd04d479b
quantum-grit <quantum-grit@gmail.com>
2022-04-21 12:04:16 +0300

    added sitemap using next-sitemap package for seo purposes

--- src/pages/admin/documents/[id]/edit.tsx
+++ src/pages/admin/documents/[id]/edit.tsx
@@ -1,7 +1,3 @@
-import { GetServerSideProps } from 'next'
-import { dehydrate, QueryClient } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
 import EditPage from 'components/documents/EditPage'
 import { securedAdminProps } from 'middleware/auth/keycloak'
 import { endpoints } from 'service/apiEndpoints'
