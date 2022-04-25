cc8fbe868236975d8eec89cdd4f09593480c6523
dimitar.nizamov <dimitur2204@gmail.com>
2022-04-25 22:40:33 +0200

    Styled person info

--- src/components/person/PersonInfo.tsx
+++ src/components/person/PersonInfo.tsx
@@ -1,4 +1,6 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Theme, Typography } from '@mui/material'
+import createStyles from '@mui/styles/createStyles'
+import makeStyles from '@mui/styles/makeStyles'
 import theme from 'common/theme'
 import { format, parseISO } from 'date-fns'
 import { PersonResponse } from 'gql/person'
@@ -8,24 +10,39 @@ type Props = {
   person: PersonResponse
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    infoHeading: {
+      marginBottom: theme.spacing(2),
+      marginTop: theme.spacing(2),
+    },
+    infoWrapper: {
+      '&>*': {
+        marginBottom: theme.spacing(1),
+      },
+    },
+  }),
+)
+
 function PersonInfo({ person }: Props) {
+  const classes = useStyles()
   return (
     <Grid container>
       <Grid item xs={12} md={6}>
-        <Typography variant="h6" color={theme.palette.primary.dark}>
+        <Typography className={classes.infoHeading} variant="h6" color={theme.palette.primary.dark}>
           Contact information:
         </Typography>
-        <Box>
+        <Box className={classes.infoWrapper}>
           <Typography>Email: {person.email}</Typography>
           <Typography>Tel: {person.phone}</Typography>
-          <Typography>Tel: {person.address}</Typography>
+          <Typography>Adress: {person.address}</Typography>
         </Box>
       </Grid>
       <Grid item xs={12} md={6}>
-        <Typography variant="h6" color={theme.palette.primary.dark}>
+        <Typography className={classes.infoHeading} variant="h6" color={theme.palette.primary.dark}>
           General information:
         </Typography>
-        <Box>
+        <Box className={classes.infoWrapper}>
           <Typography>Created at: {format(parseISO(person.createdAt), 'PPpp')}</Typography>
           <Typography>Company: {person.company}</Typography>
           <Typography>Confirmed email: {person.emailConfirmed ? 'Yes' : 'No'}</Typography>
