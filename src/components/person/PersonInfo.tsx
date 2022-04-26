943204c3ff80572ee0fc8bd0fca8901ad153790d
dimitar.nizamov <dimitur2204@gmail.com>
2022-04-26 22:04:16 +0200

    Add translations for person info component

--- src/components/person/PersonInfo.tsx
+++ src/components/person/PersonInfo.tsx
@@ -4,6 +4,7 @@ import makeStyles from '@mui/styles/makeStyles'
 import theme from 'common/theme'
 import { format, parseISO } from 'date-fns'
 import { PersonResponse } from 'gql/person'
+import { useTranslation } from 'next-i18next'
 import React from 'react'
 
 type Props = {
@@ -26,26 +27,39 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function PersonInfo({ person }: Props) {
   const classes = useStyles()
+  const { t } = useTranslation()
   return (
     <Grid container>
       <Grid item xs={12} md={6}>
         <Typography className={classes.infoHeading} variant="h6" color={theme.palette.primary.dark}>
-          Contact information:
+          {t('person:info.contact')}
         </Typography>
         <Box className={classes.infoWrapper}>
-          <Typography>Email: {person.email}</Typography>
-          <Typography>Tel: {person.phone}</Typography>
-          <Typography>Adress: {person.address}</Typography>
+          <Typography>
+            {t('person:info.email')}: {person.email}
+          </Typography>
+          <Typography>
+            {t('person:info.tel')}: {person.phone}
+          </Typography>
+          <Typography>
+            {t('person:info.address')}: {person.address}
+          </Typography>
         </Box>
       </Grid>
       <Grid item xs={12} md={6}>
         <Typography className={classes.infoHeading} variant="h6" color={theme.palette.primary.dark}>
-          General information:
+          {t('person:info.general')}
         </Typography>
         <Box className={classes.infoWrapper}>
-          <Typography>Created at: {format(parseISO(person.createdAt), 'PPpp')}</Typography>
-          <Typography>Company: {person.company}</Typography>
-          <Typography>Confirmed email: {person.emailConfirmed ? 'Yes' : 'No'}</Typography>
+          <Typography>
+            {t('person:info.createdAt')}: {format(parseISO(person.createdAt), 'PPpp')}
+          </Typography>
+          <Typography>
+            {t('person:info.company')}: {person.company}
+          </Typography>
+          <Typography>
+            {t('person:info.confirmedEmail')}: {person.emailConfirmed ? 'Yes' : 'No'}
+          </Typography>
         </Box>
       </Grid>
     </Grid>
