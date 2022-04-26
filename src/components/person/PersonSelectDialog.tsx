cb1faf6700bf68c0f8d8299ecc4f710fa094c98b
dimitar.nizamov <dimitur2204@gmail.com>
2022-04-26 20:28:14 +0200

    Add select person styled box

--- src/components/person/PersonSelectDialog.tsx
+++ src/components/person/PersonSelectDialog.tsx
@@ -6,12 +6,16 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  InputAdornment,
+  TextField,
   Typography,
 } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 import useConfirm from 'common/hooks/confirm'
 import theme from 'common/theme'
 import CloseModalButton from 'components/common/CloseModalButton'
 import { PersonResponse } from 'gql/person'
+import { useTranslation } from 'next-i18next'
 import React, { useState } from 'react'
 import PersonAutocomplete from './PersonAutocomplete'
 import PersonInfo from './PersonInfo'
@@ -20,8 +24,21 @@ type Props = {
   onConfirm: (person: PersonResponse) => void
 }
 
+const useStyles = makeStyles({
+  imitateInputBox: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    border: `1px solid rgba(0, 0, 0, 0.23)`,
+    borderRadius: '3px',
+    padding: '8.5px 14px',
+    cursor: 'pointer',
+  },
+})
 function PersonSelectDialog({ onConfirm: confirmCallback }: Props) {
   const [person, setPerson] = useState<PersonResponse | null>(null)
+  const { t } = useTranslation('person')
+  const classes = useStyles()
   const { open, confirmHandler, closeHandler, openHandler, loading } = useConfirm({
     onConfirm: async () => {
       confirmCallback(person as PersonResponse)
@@ -32,33 +49,36 @@ function PersonSelectDialog({ onConfirm: confirmCallback }: Props) {
   })
   return (
     <>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Typography>{person ? person.firstName : 'You need to select a person'}</Typography>
-        <Button
-          sx={{ marginLeft: theme.spacing(3) }}
-          variant="outlined"
-          color="primary"
-          onClick={openHandler}>
-          Select Person
+      <Box onClick={openHandler} className={classes.imitateInputBox}>
+        <Typography>
+          {person
+            ? `${person.firstName} ${person.lastName} (${person.id})`
+            : t('person:selectDialog.notSelected')}
+        </Typography>
+        <Button sx={{ padding: 0 }} onClick={openHandler}>
+          {t('person:selectDialog.select')}
         </Button>
       </Box>
       <Dialog fullWidth open={open} onClose={closeHandler}>
-        <DialogTitle>Person Select</DialogTitle>
+        <DialogTitle>{t('person:selectDialog.personSelect')}</DialogTitle>
         <DialogContent>
-          <PersonAutocomplete
-            onSelect={(person) => {
-              setPerson(person)
-            }}
-            showId
-          />
-          <Box sx={{ marginTop: theme.spacing(3) }}>
-            {person ? <PersonInfo person={person} /> : "You haven't selected a person"}
+          <Box sx={{ marginTop: theme.spacing(2) }}>
+            <PersonAutocomplete
+              onSelect={(person) => {
+                setPerson(person)
+              }}
+              showId
+            />
+          </Box>
+
+          <Box sx={{ marginTop: theme.spacing(2) }}>
+            {person ? <PersonInfo person={person} /> : t('person:selectDialog.notSelected')}
           </Box>
         </DialogContent>
         <DialogActions>
           <CloseModalButton onClose={closeHandler} />
           <LoadingButton onClick={confirmHandler} loading={loading}>
-            Confirm
+            {t('person:selectDialog.confirm')}
           </LoadingButton>
         </DialogActions>
       </Dialog>
