63042d779257a30ff6e3afd20f115aaf080c4e74
PepiPetrov <pepi_petrov@outlook.com>
2022-04-28 11:27:26 +0300

    added formatted donation date and place for signatures

--- src/components/pdf/Certificate.tsx
+++ src/components/pdf/Certificate.tsx
@@ -3,6 +3,7 @@ import { Document, Page, StyleSheet, Text, View, Font } from '@react-pdf/rendere
 import Logo from './Logo'
 import { DonationResponse } from 'gql/donations'
 import { PersonResponse } from 'gql/person'
+import { format } from 'date-fns'
 
 Font.register({
   family: 'Roboto',
@@ -52,14 +53,22 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto',
     textDecoration: 'underline',
   },
+  dateText: {
+    fontFamily: 'Roboto',
+  },
+  members: {
+    fontFamily: 'Roboto',
+    fontSize: '10',
+  },
 })
+
 type Props = {
   donation: DonationResponse
   person?: PersonResponse
 }
 export default function Certificate({ donation, person }: Props) {
   const name = `${person?.firstName} ${person?.lastName}`
-
+  const formattedDate = format(Date.parse(donation.createdAt), 'dd.MM.yyyy')
   return (
     <Document title="Дарение">
       <Page size="A4" style={styles.page}>
@@ -77,18 +86,20 @@ export default function Certificate({ donation, person }: Props) {
         </View>
         <View>
           <Text style={styles.donationText}>
-            дари сума в размер на <Text style={{ color: '#29599D' }}>{donation?.amount}</Text>
+            дари сума в размер на <Text style={{ color: '#29599D' }}>{donation?.amount} </Text>
             лева за дейността на сдружението.
           </Text>
         </View>
         <View style={styles.dateAndSignView}>
           <View>
-            <Text style={styles.date}>{donation?.createdAt}</Text>
-            <Text style={styles.date}>Дата</Text>
+            <Text style={styles.date}>{formattedDate}</Text>
+            <Text style={styles.dateText}>Дата</Text>
           </View>
           <View>
-            <Text style={styles.date}>{new Date(donation?.createdAt).toLocaleDateString()}</Text>
-            <Text style={styles.date}>Дата</Text>
+            <Text>_______________</Text>
+            <Text style={styles.members}>Членове на УС</Text>
+            <Text style={styles.members}>Станка Черкезова-Калайджиева</Text>
+            <Text style={styles.members}>Ана Николова</Text>
           </View>
         </View>
       </Page>
