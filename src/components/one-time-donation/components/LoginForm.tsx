7f13e6c56117a3bf073daaeedeeccd3d7f8f5919
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-02 23:15:58 +0200

    Remove unnecessary import

--- src/components/one-time-donation/components/LoginForm.tsx
+++ src/components/one-time-donation/components/LoginForm.tsx
@@ -1,6 +1,5 @@
 import { Box, Button, Checkbox, FormControlLabel, Grid, Typography } from '@mui/material'
 import theme from 'common/theme'
-import CheckboxField from 'components/common/form/CheckboxField'
 import FormTextField from 'components/common/form/FormTextField'
 import Link from 'components/common/Link'
 import { useTranslation } from 'next-i18next'
