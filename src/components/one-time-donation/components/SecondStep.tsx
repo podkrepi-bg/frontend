829429432c1fdeda5d4a22fa586608515f6220d5
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-02 23:34:27 +0200

    Add collapse to the login form when anonymous is true

--- src/components/one-time-donation/components/SecondStep.tsx
+++ src/components/one-time-donation/components/SecondStep.tsx
@@ -1,13 +1,12 @@
-import { Grid } from '@mui/material'
 import React from 'react'
 import AnonymousMenu from './AnonymousForm'
 import LoginForm from './LoginForm'
 
 export default function SecondStep() {
   return (
-    <Grid>
+    <>
       <LoginForm />
       <AnonymousMenu />
-    </Grid>
+    </>
   )
 }
