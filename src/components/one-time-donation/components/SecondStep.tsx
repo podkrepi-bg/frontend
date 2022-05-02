c752c1f0c81899462cd52c78679e83c2010d9fad
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-02 23:03:36 +0200

    Change checkbox field with normal checkbox for remember checkbox

--- src/components/one-time-donation/components/SecondStep.tsx
+++ src/components/one-time-donation/components/SecondStep.tsx
@@ -1,11 +1,9 @@
 import { Grid } from '@mui/material'
 import React from 'react'
-import AnonymousMenu from './AnonymousMenu'
-import { useTranslation } from 'next-i18next'
+import AnonymousMenu from './AnonymousForm'
 import LoginForm from './LoginForm'
 
 export default function SecondStep() {
-  const { t } = useTranslation('one-time-donation')
   return (
     <Grid>
       <LoginForm />
