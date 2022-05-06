59c51dfe46df6a670eaa791b52d1824ba642ec74
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-07 00:34:20 +0200

    Add fail and success pages functionality

--- src/components/one-time-donation/components/Fail.tsx
+++ src/components/one-time-donation/components/Fail.tsx
@@ -1,59 +1,56 @@
 import { Grid, Typography, Button } from '@mui/material'
-import { createStyles, makeStyles } from '@mui/styles'
-import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined'
+import FailIcon from 'common/icons/Fail'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import { useTranslation } from 'next-i18next'
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    h2: {
-      fontFamily: 'Montserrat',
-      fontStyle: 'normal',
-      fontWeight: '500',
-      fontSize: '32px',
-      lineHeight: '120%',
-      marginTop: '47.25px',
-      marginBottom: '59px',
-      letterSpacing: '-0.5px',
-      color: '#343434',
-      textAlign: 'center',
-    },
-    buttons: {
-      width: '309.55px',
-      height: '70.69px',
-      background: '#32A9FE',
-      border: '0.7441px solid #000000',
-      boxSizing: 'border-box',
-      borderRadius: '35.3275px',
-      margin: 10,
-    },
-    body: {
-      maxWidth: '702px',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  }),
-)
 export default function Fail() {
-  const classes = useStyles()
   const { t } = useTranslation('one-time-donation')
 
   return (
-    <Grid className={classes.body}>
+    <Grid>
       <Grid
         container
         justifyContent="center"
-        sx={{ '& .MuiSvgIcon-root': { fontSize: 132.5, color: '#F44336', marginTop: '58px' } }}>
-        <ErrorOutlineOutlinedIcon />
+        sx={{
+          width: 450,
+          height: 450,
+          margin: '0 auto',
+        }}>
+        <FailIcon />
       </Grid>
-      <Grid>
-        <Typography className={classes.h2}>{t('fail.title')}</Typography>
+      <Grid container rowSpacing={2} justifyContent={'center'} textAlign="center">
+        <Grid item xs={12}>
+          <Typography variant="h4" fontSize={14}>
+            {t('fail.title')}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            '& .MuiSvgIcon-root': {
+              fontSize: '38px',
+              color: '#909090',
+              margin: 2,
+            },
+          }}>
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <LinkedInIcon />
+        </Grid>
       </Grid>
-      <Grid container justifyContent="center">
-        <Typography>{t('fail.subtitle')}</Typography>
-      </Grid>
-      <Grid container display="flex" my={10}>
-        <Button className={classes.buttons}>{t('fail.btn-again')}</Button>
-        <Button className={classes.buttons}>{t('fail.btn-connect')}</Button>
+      <Grid container rowSpacing={3} justifyContent="center">
+        <Grid textAlign={'center'} item xs={12} md={6}>
+          <Button variant="contained" color="primary">
+            {t('fail.btn-again')}
+          </Button>
+        </Grid>
+        <Grid textAlign={'center'} item xs={12} md={6}>
+          <Button variant="contained" color="primary">
+            {t('fail.connect')}
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   )
