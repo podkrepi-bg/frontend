5eb7e1acd80823ce210e14c8517bf4a81c36aa97
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-12 20:34:33 +0200

    Add team members section carousel

--- src/components/index/sections/TeamMembersSection.tsx
+++ src/components/index/sections/TeamMembersSection.tsx
@@ -1,5 +1,9 @@
-import { Box, Container, Grid, Typography } from '@mui/material'
+import React from 'react'
+import { Container, Grid, IconButton, Typography } from '@mui/material'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import { Navigation, A11y, Autoplay } from 'swiper'
+import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react'
 
 import theme from 'common/theme'
 import Heading from 'components/common/Heading'
@@ -8,7 +12,29 @@ import { data } from '../helpers/teamMembers/memberData'
 import { staticUrls } from 'common/routes'
 import LinkButton from 'components/common/LinkButton'
 
+import 'swiper/css'
+
+const swiperOptions: SwiperProps = {
+  loop: true,
+  loopAdditionalSlides: 1,
+  modules: [Navigation, A11y, Autoplay],
+  autoplay: { delay: 5000 },
+  breakpoints: {
+    [theme.breakpoints.values.xs]: {
+      slidesPerView: 1,
+    },
+    [theme.breakpoints.values.md]: {
+      slidesPerView: 2,
+    },
+    [theme.breakpoints.values.lg]: {
+      slidesPerView: 3,
+    },
+  },
+  spaceBetween: 30,
+}
 export default function TeamMembersSection() {
+  const navigationPrevRef = React.useRef(null)
+  const navigationNextRef = React.useRef(null)
   return (
     <Container maxWidth="md">
       <Heading
@@ -33,20 +59,43 @@ export default function TeamMembersSection() {
         spacing={6}
         paddingTop={theme.spacing(7)}
         paddingBottom={theme.spacing(12)}>
-        {data.map((x, index) => (
-          <Grid key={index} item xs={12} sm={6} lg={4} paddingBottom={theme.spacing(7)}>
-            <Box textAlign="center">
-              <MemberCard info={x} />
-            </Box>
-          </Grid>
-        ))}
-        <LinkButton
-          href={staticUrls.blog}
-          variant="outlined"
-          endIcon={<ChevronRightIcon />}
-          sx={{ marginTop: '2rem' }}>
-          {'Запознай се с екипа ни'}
-        </LinkButton>
+        <Grid item xs={12} display="flex" alignItems="center">
+          <IconButton ref={navigationPrevRef} aria-label="Previouos slide">
+            <ChevronLeftIcon />
+          </IconButton>
+          <Swiper
+            {...swiperOptions}
+            style={{ marginLeft: theme.spacing(2), marginRight: theme.spacing(2) }}
+            navigation={{
+              prevEl: navigationPrevRef.current,
+              nextEl: navigationNextRef.current,
+            }}
+            onBeforeInit={(swiper) => {
+              if (!swiper.params.navigation || swiper.params.navigation === true) {
+                return
+              }
+              swiper.params.navigation.prevEl = navigationPrevRef.current
+              swiper.params.navigation.nextEl = navigationNextRef.current
+            }}>
+            {data.map((x) => (
+              <SwiperSlide key={x.title}>
+                <MemberCard info={x} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <IconButton ref={navigationNextRef} aria-label="Next slide">
+            <ChevronRightIcon />
+          </IconButton>
+        </Grid>
+        <Grid item xs={12} textAlign="center">
+          <LinkButton
+            href={staticUrls.blog}
+            variant="outlined"
+            endIcon={<ChevronRightIcon />}
+            sx={{ marginTop: '2rem' }}>
+            {'Запознай се с екипа ни'}
+          </LinkButton>
+        </Grid>
       </Grid>
     </Container>
   )
