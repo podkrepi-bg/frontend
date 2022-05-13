2eb686d7f9e5676d3c30b3aba62bade021d5e617
Ilko Kacharov <kachar136@gmail.com>
2022-05-13 10:50:11 +0300

    Remove unused code

--- src/pages/campaigns/donation/[slug].tsx
+++ src/pages/campaigns/donation/[slug].tsx
@@ -1,13 +1,13 @@
-import OneTimeDonation from 'components/one-time-donation/OneTimeDonationPage'
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import { dehydrate } from 'react-query'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { QueryClient } from 'react-query'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 import { queryFn } from 'service/restRequests'
 import { securedProps } from 'middleware/auth/securedProps'
+import OneTimeDonation from 'components/one-time-donation/OneTimeDonationPage'
 
 export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  // const keycloak = keycloakInstance(params)
   const { slug } = ctx.query
   const response = await securedProps(ctx, `/campaigns/donation/${slug}`)
   const client = new QueryClient()
