7dfc9a4da776f1ac0627ed4bbc5fcefe1665cca9
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-13 11:49:19 +0200

    Made the WhatUnitesUsCards a carousel

--- src/components/index/sections/WhatUnitesUsSection.tsx
+++ src/components/index/sections/WhatUnitesUsSection.tsx
@@ -1,14 +1,51 @@
-import { Container, Grid } from '@mui/material'
+import React from 'react'
+import { Container, Grid, IconButton } from '@mui/material'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import { useTranslation } from 'next-i18next'
-
+import { Navigation, A11y, Autoplay } from 'swiper'
+import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react'
 import theme from 'common/theme'
 import Heading from 'components/common/Heading'
 import WhatUnitesUsCard from '../helpers/whatUnitesUs/WhatUnitesUsCard'
 import { data } from '../helpers/whatUnitesUs/whatUnitesUsData'
+import { createStyles, makeStyles } from '@mui/styles'
+
+const swiperOptions: SwiperProps = {
+  loop: true,
+  loopAdditionalSlides: 1,
+  modules: [Navigation, A11y, Autoplay],
+  autoplay: { delay: 5000 },
+  breakpoints: {
+    [theme.breakpoints.values.xs]: {
+      slidesPerView: 1,
+    },
+    [theme.breakpoints.values.md]: {
+      slidesPerView: 2,
+    },
+    [theme.breakpoints.values.lg]: {
+      slidesPerView: 3,
+    },
+  },
+  spaceBetween: 20,
+}
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    sliderNavNext: {
+      order: 3,
+    },
+    sliderNavPrevious: {
+      order: 1,
+    },
+  }),
+)
 
 export default function WhatUnitesUsSection() {
   const { t } = useTranslation()
-
+  const navigationPrevRef = React.useRef(null)
+  const navigationNextRef = React.useRef(null)
+  const classes = useStyles()
   return (
     <Container maxWidth="md">
       <Heading
@@ -24,11 +61,40 @@ export default function WhatUnitesUsSection() {
         flexDirection="row"
         alignItems="baseline"
         paddingBottom={theme.spacing(12)}>
-        {data.map((x, index) => (
-          <Grid key={index} item xs={12} sm={6} lg={4}>
-            <WhatUnitesUsCard info={x} />
-          </Grid>
-        ))}
+        <Grid item xs={12} display="flex" alignItems="center" position="relative">
+          <IconButton
+            className={classes.sliderNavPrevious}
+            ref={navigationPrevRef}
+            aria-label="Previouos slide">
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton
+            className={classes.sliderNavNext}
+            ref={navigationNextRef}
+            aria-label="Next slide">
+            <ChevronRightIcon />
+          </IconButton>
+          <Swiper
+            {...swiperOptions}
+            style={{ marginLeft: theme.spacing(2), marginRight: theme.spacing(2), order: 2 }}
+            navigation={{
+              prevEl: navigationPrevRef.current,
+              nextEl: navigationNextRef.current,
+            }}
+            onBeforeInit={(swiper) => {
+              if (!swiper.params.navigation || swiper.params.navigation === true) {
+                return
+              }
+              swiper.params.navigation.prevEl = navigationPrevRef.current
+              swiper.params.navigation.nextEl = navigationNextRef.current
+            }}>
+            {data.map((x) => (
+              <SwiperSlide key={x.title}>
+                <WhatUnitesUsCard info={x} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Grid>
       </Grid>
     </Container>
   )
