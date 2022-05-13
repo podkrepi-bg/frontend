743add1fd363d12d58089b1d21b5ecddc01d6c77
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-13 20:36:15 +0200

    Fix WantToHelp section styles

--- src/components/index/sections/WantToHelpPodkrepiBg.tsx
+++ src/components/index/sections/WantToHelpPodkrepiBg.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography, useMediaQuery } from '@mui/material'
 import { createStyles, makeStyles } from '@mui/styles'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import { useTranslation } from 'next-i18next'
@@ -12,13 +12,8 @@ import Heading from 'components/common/Heading'
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      display: 'flex',
-      paddingTop: '36px',
-      paddingBottom: '36px',
-      paddingLeft: '25vw',
-      paddingRight: '25vw',
       marginBottom: theme.spacing(12),
-      flexDirection: 'column',
+      padding: theme.spacing(6, 5),
       alignItems: 'center',
       backgroundColor: theme.palette.secondary.light,
     },
@@ -36,10 +31,12 @@ const useStyles = makeStyles(() =>
 export default function WantToHelpPodkrepiBgSection() {
   const classes = useStyles()
   const { t } = useTranslation()
-
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'))
   return (
-    <Grid className={classes.root}>
+    <Box className={classes.root}>
       <Heading
+        maxWidth="lg"
+        margin="0 auto"
         textAlign="center"
         variant="h4"
         fontFamily="Montserrat"
@@ -47,18 +44,18 @@ export default function WantToHelpPodkrepiBgSection() {
         {t('index:help-podkrepi-bg-section.want-to-help')}
       </Heading>
       <Grid
+        maxWidth="lg"
+        margin="0 auto"
         container
-        display="flex"
-        direction="row"
-        justifyContent="space-around"
         alignItems="center"
-        paddingTop="36px">
-        <Grid item style={{ width: '60%' }}>
+        marginTop={theme.spacing(1)}
+        rowSpacing={4}>
+        <Grid item xs={12} md={6} textAlign={mdDown ? 'center' : 'left'}>
           <Typography variant="subtitle1" className={classes.text}>
             {t('index:help-podkrepi-bg-section.text')}
           </Typography>
         </Grid>
-        <Grid item>
+        <Grid item xs={12} md={6} textAlign={mdDown ? 'center' : 'right'}>
           <LinkButton
             variant="contained"
             href={routes.support}
@@ -68,6 +65,6 @@ export default function WantToHelpPodkrepiBgSection() {
           </LinkButton>
         </Grid>
       </Grid>
-    </Grid>
+    </Box>
   )
 }
