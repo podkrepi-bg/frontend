552ecf8ca3302c4c42c82aa180cf541b9da911bf
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-13 20:18:31 +0200

    Remove unnecessary files

--- src/components/index/IndexPage.tsx
+++ src/components/index/IndexPage.tsx
@@ -1,21 +1,15 @@
-import { useRef } from 'react'
-import { Container } from '@mui/material'
 import { useTranslation } from 'next-i18next'
-
 import Layout from 'components/layout/Layout'
-
 import Jumbotron from './sections/Jumbotron'
 import CampaignsSection from './sections/CampaignsSection'
 import HowWeWorkSection from './sections/HowWeWorkSection'
 import ReadyToStartCampaignSection from './sections/ReadyToStartCampaignSection'
 import WhatUnitesUsSection from './sections/WhatUnitesUsSection'
-import { RefObject } from 'react'
 import WantToHelpPodkrepiBgSection from './sections/WantToHelpPodkrepiBg'
 import FaqSection from './sections/FaqSection'
 import TeamMembersSection from './sections/TeamMembersSection'
 
 export default function IndexPage() {
-  const scrollElement = useRef<HTMLAnchorElement>(null)
   const { t } = useTranslation()
 
   return (
