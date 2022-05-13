a2f1c1bc19af5396f23543b8985c86feb492b5ef
Ilko Kacharov <kachar136@gmail.com>
2022-05-13 15:30:43 +0300

    Redirect to profile if auth user goes to /login or /register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,14 +1,24 @@
 import { GetServerSideProps } from 'next'
+import { getProviders, getSession } from 'next-auth/react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+import { baseUrl, routes } from 'common/routes'
 import RegisterPage from 'components/auth/register/RegisterPage'
-import { getProviders } from 'next-auth/react'
 
 export type RegisterPageProps = {
   providers: Awaited<ReturnType<typeof getProviders>>
 }
 
 export const getServerSideProps: GetServerSideProps<RegisterPageProps> = async (ctx) => {
+  const session = await getSession(ctx)
+  if (session) {
+    return {
+      redirect: {
+        destination: `${baseUrl}/${routes.profile.index}`,
+        permanent: false,
+      },
+    }
+  }
   return {
     props: {
       ...(await serverSideTranslations(ctx.locale ?? 'bg', ['common', 'auth', 'validation'])),
