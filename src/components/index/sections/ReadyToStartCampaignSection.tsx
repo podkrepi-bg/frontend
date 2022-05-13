8be9f9b5a0661baa34126df8aaa4efabfa8c2275
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-13 20:18:31 +0200

    Remove unnecessary files

--- src/components/index/sections/ReadyToStartCampaignSection.tsx
+++ src/components/index/sections/ReadyToStartCampaignSection.tsx
@@ -1,5 +1,4 @@
 import { Box, Grid, Typography, useMediaQuery } from '@mui/material'
-import { createStyles, makeStyles } from '@mui/styles'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import { useTranslation } from 'next-i18next'
 
@@ -8,34 +7,7 @@ import LinkButton from 'components/common/LinkButton'
 import { routes } from 'common/routes'
 import theme from 'common/theme'
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    text: {
-      color: theme.palette.primary.dark,
-      fontWeight: 500,
-      textAlign: 'center',
-    },
-    button: {
-      margin: 'auto',
-      color: 'black',
-    },
-    content: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      flexDirection: 'row',
-      marginBottom: '1rem',
-      paddingTop: '60px',
-      paddingBottom: '60px',
-      paddingLeft: '20vw',
-      paddingRight: '20vw',
-      backgroundColor: theme.palette.secondary.light,
-    },
-  }),
-)
-
 export default function ReadyToStartCampaignSection() {
-  const classes = useStyles()
   const { t } = useTranslation()
   const downMd = useMediaQuery(theme.breakpoints.down('md'))
   return (
