5ecd930a1ddcef81b26580b8fa5a394c0385492e
Bunny 303 <303bunnies@gmail.com>
2022-05-16 16:06:29 +0300

    Added caling of pdf generation, updated data fetch in the handler

--- src/pages/api/pdf/certificate/[donationId].tsx
+++ src/pages/api/pdf/certificate/[donationId].tsx
@@ -1,28 +1,29 @@
 import { renderToStream } from '@react-pdf/renderer'
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
-import { PersonResponse } from 'gql/person'
 import { apiClient } from 'service/apiClient'
-import { DonationResponse } from 'gql/donations'
+import { UserDonationResponse } from 'gql/donations'
 import { endpoints } from 'service/apiEndpoints'
 import Certificate from 'components/pdf/Certificate'
+import { authConfig } from 'service/restRequests'
+import { getToken } from 'next-auth/jwt'
 
 const Handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const id = Array.isArray(req.query.donationId) ? req.query.donationId[0] : req.query.donationId
 
-  const { data: donation } = await apiClient.get<DonationResponse>(
-    endpoints.donation.getDonation(id).url,
+  const jwt = await getToken({ req })
+  const { data: donation } = await apiClient.get<UserDonationResponse>(
+    endpoints.donation.getUserDonation(id).url,
+    authConfig(jwt?.accessToken),
   )
 
-  let person: PersonResponse | undefined = undefined
-  if (donation?.personId) {
-    const { data } = await apiClient.get<PersonResponse>(
-      endpoints.person.viewPerson(donation.personId).url,
-    )
-    person = data
+  if (!donation) {
+    res.send('What should happen?')
   }
 
-  const pdfStream = await renderToStream(<Certificate donation={donation} person={person} />)
+  const pdfStream = await renderToStream(
+    <Certificate donation={donation} person={donation.person} />,
+  )
   res.setHeader('Content-Type', 'application/pdf')
   pdfStream.pipe(res)
 }
