8e20f287ddf28e296d01faeb38c7fd3ec0dea197
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-18 23:41:33 +0200

    Small homepage container fixes

--- src/components/index/sections/ReadyToStartCampaignSection.tsx
+++ src/components/index/sections/ReadyToStartCampaignSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Container, Grid, Typography } from '@mui/material'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import { useTranslation } from 'next-i18next'
 
@@ -14,22 +14,24 @@ export default function ReadyToStartCampaignSection() {
         backgroundColor: '#F4F4F4',
         py: 6,
       }}>
-      <Grid container maxWidth="lg" margin="0 auto">
-        <Grid item xs={12} md={6} alignItems="center" display="flex">
-          <Typography sx={{ xs: { textAlign: 'center' }, textAlign: 'left', m: 0 }} variant="h5">
-            {t('index:ready-to-start-campaign-section.text')}
-          </Typography>
+      <Container maxWidth="lg">
+        <Grid container margin="0 auto">
+          <Grid item xs={12} md={6} alignItems="center" display="flex">
+            <Typography sx={{ xs: { textAlign: 'center' }, textAlign: 'left', m: 0 }} variant="h5">
+              {t('index:ready-to-start-campaign-section.text')}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6} sx={{ xs: { textAlign: 'center' }, textAlign: 'right' }}>
+            <LinkButton
+              href={routes.campaigns.create}
+              variant="contained"
+              color="primary"
+              endIcon={<ChevronRightIcon />}>
+              {t('index:ready-to-start-campaign-section.button')}
+            </LinkButton>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6} sx={{ xs: { textAlign: 'center' }, textAlign: 'right' }}>
-          <LinkButton
-            href={routes.campaigns.create}
-            variant="contained"
-            color="primary"
-            endIcon={<ChevronRightIcon />}>
-            {t('index:ready-to-start-campaign-section.button')}
-          </LinkButton>
-        </Grid>
-      </Grid>
+      </Container>
     </Box>
   )
 }
