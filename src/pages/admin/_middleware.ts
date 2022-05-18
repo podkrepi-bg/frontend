d709ea69960cf96260ebf72635e4e257be6ed302
dimitar.nizamov <dimitur2204@gmail.com>
2022-05-18 19:47:42 +0200

    Fix middleware typings

--- src/pages/admin/_middleware.ts
+++ src/pages/admin/_middleware.ts
@@ -1,13 +1,13 @@
 // https://next-auth.js.org/configuration/nextjs#middleware
 import { isAdmin } from 'common/util/roles'
-import { getToken } from 'next-auth/jwt'
 import withAuth from 'next-auth/middleware'
+import { useSession } from 'next-auth/react'
 
 export default withAuth({
   callbacks: {
-    authorized: async ({ req }) => {
-      const jwt = await getToken({ req })
-      return isAdmin(jwt)
+    authorized: async () => {
+      const { data: session } = useSession()
+      return isAdmin(session)
     },
   },
 })
