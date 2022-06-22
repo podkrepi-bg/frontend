fa4ea086de30fbf8fa6478c62e08df5904b22798
ani-kalpachka <ani.kalpachka@merklecxm.com>
2022-06-22 23:39:54 +0300

    Extract CampaignInfo component, fix spacings

--- src/components/campaigns/CampaignDetails.tsx
+++ src/components/campaigns/CampaignDetails.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { styled } from '@mui/material/styles'
-import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
+import { useTranslation } from 'next-i18next'
 import { CampaignResponse } from 'gql/campaigns'
 import CampaignMessages from './CampaignMessages'
 import CampaignSlider from './CampaignSlider'
-import { Grid, Typography } from '@mui/material'
 import {
   backgroundCampaignPictureUrl,
   beneficiaryCampaignPictureUrl,
-  coordinatorCampaignPictureUrl,
 } from 'common/util/campaignImageUrls'
+import CampaignInfo from './CampaignInfo'
+import { styled } from '@mui/material/styles'
+import { Grid, Typography } from '@mui/material'
 
 const PREFIX = 'CampaignDetails'
 
@@ -18,12 +18,8 @@ const classes = {
   bannerWrapper: `${PREFIX}-bannerWrapper`,
   banner: `${PREFIX}-banner`,
   campaignTitle: `${PREFIX}-campaignTitle`,
-  campaignSubtitle: `${PREFIX}-campaignSubtitle`,
   beneficiaryAvatarWrapper: `${PREFIX}-beneficiaryAvatarWrapper`,
   beneficiaryAvatar: `${PREFIX}-beneficiaryAvatar`,
-  campaignInfoWrapper: `${PREFIX}-campaignInfoWrapper`,
-  coordinatorAvatar: `${PREFIX}-coordinatorAvatar`,
-  campaignDate: `${PREFIX}-campaignDate`,
   beneficiaryName: `${PREFIX}-beneficiaryName`,
 }
 
@@ -42,24 +38,23 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
 
   [`& .${classes.campaignTitle}`]: {
     fontWeight: 500,
-    margin: 0,
-  },
-
-  [`& .${classes.campaignSubtitle}`]: {
-    color: theme.palette.common.white,
-    padding: theme.spacing(0, 4.7),
-    fontSize: theme.spacing(4),
-    height: theme.spacing(16),
+    textAlign: 'center',
+    [theme.breakpoints.up('md')]: {
+      textAlign: 'initial',
+    },
   },
 
   [`& .${classes.beneficiaryAvatarWrapper}`]: {
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'column',
-    marginTop: theme.spacing(41.125),
+    marginTop: theme.spacing(47),
     [theme.breakpoints.up('md')]: {
       textAlign: 'left',
       flexDirection: 'initial',
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginTop: theme.spacing(41),
     },
   },
 
@@ -68,24 +63,11 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
     textAlign: 'center',
   },
 
-  [`& .${classes.campaignInfoWrapper}`]: {
-    gap: theme.spacing(2),
-    display: 'grid',
-  },
-
-  [`& .${classes.coordinatorAvatar}`]: {
-    borderRadius: '50%',
-  },
-
-  [`& .${classes.campaignDate}`]: {
-    fontSize: theme.spacing(2),
-  },
-
   [`& .${classes.beneficiaryName}`]: {
-    marginTop: theme.spacing(2),
     fontSize: theme.spacing(4),
+    marginTop: theme.spacing(3),
     [theme.breakpoints.up('md')]: {
-      marginTop: theme.spacing(4),
+      marginTop: theme.spacing(8),
     },
   },
 }))
@@ -99,21 +81,6 @@ export default function CampaignDetails({ campaign }: Props) {
 
   const bannerSource = backgroundCampaignPictureUrl(campaign)
   const beneficiaryAvatarSource = beneficiaryCampaignPictureUrl(campaign)
-  const coordinatorAvatarSource = coordinatorCampaignPictureUrl(campaign)
-
-  const startDate = new Date(campaign.startDate)
-  const formattedStartDate = startDate.toLocaleDateString('bg-BG', {
-    day: 'numeric',
-    month: 'long',
-    year: '2-digit',
-  })
-
-  const endDate = new Date(campaign.endDate)
-  const formattedEndDate = endDate.toLocaleDateString('bg-BG', {
-    day: 'numeric',
-    month: 'long',
-    year: '2-digit',
-  })
 
   return (
     <StyledGrid item xs={12} md={8}>
@@ -126,7 +93,7 @@ export default function CampaignDetails({ campaign }: Props) {
           className={classes.banner}
         />
       </Grid>
-      <Grid item py={4} className={classes.beneficiaryAvatarWrapper}>
+      <Grid item className={classes.beneficiaryAvatarWrapper}>
         <Image
           src={beneficiaryAvatarSource}
           alt={campaign.title}
@@ -138,37 +105,15 @@ export default function CampaignDetails({ campaign }: Props) {
           {campaign.beneficiary.person.firstName}
         </Typography>
       </Grid>
-      <Typography paragraph variant="h2" component="h2" py={4} className={classes.campaignTitle}>
+      <Typography paragraph variant="h2" component="h2" my={8} className={classes.campaignTitle}>
         {campaign.title}
       </Typography>
-      <Grid container justifyContent="space-between" mb={4}>
-        <Typography variant="subtitle2" component="p" className={classes.campaignDate}>
-          {t('campaigns:campaign.start-date')} {formattedStartDate}
-        </Typography>
-        <Typography variant="subtitle2" component="p" className={classes.campaignDate}>
-          {t('campaigns:campaign.end-date')} {formattedEndDate}
-        </Typography>
-      </Grid>
-      <Grid className={classes.campaignInfoWrapper}>
-        <Typography variant="subtitle2" component="p">
+      <CampaignInfo campaign={campaign} />
+      <Grid>
+        <Typography variant="subtitle2" component="p" mb={5}>
           {campaign.description}
         </Typography>
         <CampaignSlider />
-        <Typography variant="h6" component="h6">
-          {t('campaigns:campaign.coordinator.name')}
-        </Typography>
-        <Grid container alignItems="center" gap={4}>
-          <Image
-            src={coordinatorAvatarSource}
-            alt={campaign.title}
-            width={100}
-            height={100}
-            className={classes.coordinatorAvatar}
-          />
-          <Typography variant="h6" component="h6">
-            {campaign.coordinator.person.firstName} {campaign.coordinator.person.lastName}
-          </Typography>
-        </Grid>
         <CampaignMessages />
       </Grid>
     </StyledGrid>
