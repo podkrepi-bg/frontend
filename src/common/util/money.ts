8b4eb200f9d1404f978b691c364a9e718eae5b0c
quantum-grit <quantum-grit@gmail.com>
2022-06-23 10:52:15 +0300

    first attempt to fix the targetAmount conversion

--- src/common/util/money.ts
+++ src/common/util/money.ts
@@ -13,3 +13,15 @@ export const money = (number: number, currency = 'BGN', divisionFactor = 100) =>
     number / divisionFactor,
   )
 }
+
+/**
+ * Used for formatting a number into internal Money value
+ *
+ * @param number number
+ * @param currency string
+ * @param divisionFactor number @default 100
+ * @returns number
+ */
+export const toMoney = (number: number, currency = 'BGN', divisionFactor = 100) => {
+  return number * divisionFactor
+}
