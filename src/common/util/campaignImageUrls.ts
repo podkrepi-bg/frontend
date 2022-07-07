cba36f4ace969583a6924b1aa1c08d7a511af863
nradkova <neliradkova@abv.bg>
2022-07-07 12:43:26 +0300

    created: organizerCampaignPictureUrl

--- src/common/util/campaignImageUrls.ts
+++ src/common/util/campaignImageUrls.ts
@@ -30,6 +30,11 @@ export function coordinatorCampaignPictureUrl(campaign: CampaignResponse): strin
   return file ? fileUrl(file) : '/img/support-us-image.png'
 }
 
+export function organizerCampaignPictureUrl(campaign: CampaignResponse): string {
+  const file = findFileWithRole(campaign, CampaignFileRole.organizerPhoto)
+  return file ? fileUrl(file) : '/img/support-us-image.png'
+}
+
 export function beneficiaryCampaignPictureUrl(campaign: CampaignResponse): string {
   const file = findFileWithRole(campaign, CampaignFileRole.beneficiaryPhoto)
   return file ? fileUrl(file) : '/img/support-us-image.png'
