17ca3ebd45205848a1275d0f0df4d8086b08412a
nradkova <neliradkova@abv.bg>
2022-07-07 11:44:13 +0300

    created: OrganizerSelect component

---
+++ src/components/campaigns/grid/OrganizerSelect.tsx
@@ -0,0 +1,33 @@
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from '@mui/material'
+import { TranslatableField, translateError } from 'common/form/validation'
+import { useOrganizersList } from 'common/hooks/organizer'
+import { useField } from 'formik'
+import { useTranslation } from 'react-i18next'
+
+export default function OrganizerSelect({ name = 'organizerId' }) {
+  const { t } = useTranslation()
+  const { data } = useOrganizersList()
+  const [field, meta] = useField(name)
+
+  const helperText = meta.touched ? translateError(meta.error as TranslatableField, t) : ''
+  return (
+    <FormControl
+      fullWidth
+      size="small"
+      variant="outlined"
+      error={Boolean(meta.error) && Boolean(meta.touched)}>
+      <InputLabel>{t('Организатор')}</InputLabel>
+      <Select fullWidth defaultValue="" label={t('Организатор')} {...field}>
+        <MenuItem value="" disabled>
+          {t('Организатор')}
+        </MenuItem>
+        {data?.map((organizer, index) => (
+          <MenuItem key={index} value={organizer.id}>
+            {organizer.person.firstName} {organizer.person.lastName}
+          </MenuItem>
+        ))}
+      </Select>
+      {helperText && <FormHelperText error>{helperText}</FormHelperText>}
+    </FormControl>
+  )
+}
