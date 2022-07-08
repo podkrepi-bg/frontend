be7b4c128ede8c2ea1ba63c8df05f1c46766dc97
borislavstoychev <stoy4ew@gmail.com>
2022-07-08 12:36:44 +0300

    :hammer:

--- src/components/campaigns/InlineDonation.tsx
+++ src/components/campaigns/InlineDonation.tsx
@@ -1,24 +1,12 @@
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import { styled } from '@mui/material/styles'
 import { useTranslation } from 'next-i18next'
 import { CampaignResponse } from 'gql/campaigns'
 import { baseUrl, routes } from 'common/routes'
 import { money } from 'common/util/money'
-import { useSinglePriceList } from 'common/hooks/donation'
-import LinkButton from 'components/common/LinkButton'
 import CampaignProgress from './CampaignProgress'
 import DonorsAndDonations from './DonorsAndDonations'
-import {
-  Button,
-  CircularProgress,
-  Grid,
-  lighten,
-  List,
-  ListItem,
-  ListItemText,
-  Menu,
-  Typography,
-} from '@mui/material'
+import { Button, CircularProgress, Grid, lighten, Menu, Typography } from '@mui/material'
 import { AddLinkOutlined, Favorite } from '@mui/icons-material'
 import ShareIcon from '@mui/icons-material/Share'
 import { useCampaignDonationHistory } from 'common/hooks/campaigns'
@@ -90,15 +78,12 @@ export default function InlineDonation({ campaign }: Props) {
   const { t } = useTranslation()
   const router = useRouter()
   const { asPath } = useRouter()
-  const [showDonationPriceList, setDonationPriceList] = useState(false)
-  const onClick = () => setDonationPriceList(true)
   const [status, copyUrl] = useCopyToClipboard(baseUrl + asPath, 1000)
   const active = status === 'copied' ? 'inherit' : 'primary'
   const target = campaign.targetAmount
   const summary = campaign.summary.find(() => true)
   const reached = summary?.reachedAmount ?? 0
   const donors = summary?.donors ?? 0
-  const { data: prices } = useSinglePriceList()
   const {
     data: donations,
     error: donationHistoryError,
@@ -109,14 +94,6 @@ export default function InlineDonation({ campaign }: Props) {
 
   const handleMenu = (event: React.MouseEvent) => setAnchorEl(event.currentTarget)
   const handleClose = () => setAnchorEl(null)
-
-  const sortedPrices = useMemo(() => {
-    if (!prices) return []
-    return prices?.sort((a, b) => {
-      if (a.unit_amount === null || b.unit_amount === null) return 0
-      return a.unit_amount - b.unit_amount
-    })
-  }, [prices])
 
   return (
     <StyledGrid item xs={12} mt={5} p={3} className={classes.inlineDonationWrapper}>
@@ -178,38 +155,18 @@ export default function InlineDonation({ campaign }: Props) {
             color={active}
           />
         </Menu>
-        <LinkButton
+        <Button
           fullWidth
-          href="#"
-          onClick={onClick}
+          onClick={() =>
+            router.push({
+              pathname: routes.campaigns.oneTimeDonation(campaign.slug),
+            })
+          }
           variant="contained"
           color="secondary"
           startIcon={<Favorite color="action" />}>
           {t('common:support')}
-        </LinkButton>
-        {showDonationPriceList && (
-          <List className={classes.donationPriceList}>
-            {sortedPrices.map((price, index) => {
-              if (!price) return null
-              return (
-                <ListItem button key={index}>
-                  <ListItemText
-                    onClick={() =>
-                      router.push({
-                        pathname: routes.campaigns.oneTimeDonation(campaign.slug),
-                        query: {
-                          price: price.id,
-                        },
-                      })
-                    }
-                    primary={`${(price.unit_amount ?? 100) / 100} лв.`}
-                    secondary={price.metadata.title}
-                  />
-                </ListItem>
-              )
-            })}
-          </List>
-        )}
+        </Button>
       </Grid>
       {donationHistoryError ? (
         'Error fetching donation history'
