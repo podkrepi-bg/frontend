399682f2a2951521fdd5198fbfe62db5256b0944
nradkova <neliradkova@abv.bg>
2022-07-13 12:23:49 +0300

    created: mobile view of InlineDonation component

--- src/components/campaigns/InlineDonation.tsx
+++ src/components/campaigns/InlineDonation.tsx
@@ -16,7 +16,9 @@ import CustomListItem from 'components/admin/navigation/CustomListItem'
 import { socialMedia } from './helpers/socialMedia'
 import { useCopyToClipboard } from 'common/util/useCopyToClipboard'
 import { AlertStore } from 'stores/AlertStore'
-
+import useMobile from 'common/hooks/useMobile'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 const PREFIX = 'InlineDonation'
 
 const classes = {
@@ -27,14 +29,21 @@ const classes = {
   donationPriceList: `${PREFIX}-donationPriceList`,
   dropdownLinkButton: `${PREFIX}-dropdownLinkButton`,
   dropdownLinkText: `${PREFIX}-dropdownLinkText`,
+  buttonContainer: `${PREFIX}-buttonContainer`,
+  sharesContainer: `${PREFIX}-sharesContainer`,
+  openButton: `${PREFIX}-openButton`,
 }
 
 const StyledGrid = styled(Grid)(({ theme }) => ({
   [`&.${classes.inlineDonationWrapper}`]: {
-    backgroundColor: theme.palette.common.white,
+    backgroundColor: '#EEEEEE',
     borderRadius: theme.spacing(1),
     height: 'fit-content',
-    boxShadow: '1px 2px 8px #8888888c',
+    boxShadow: '2px 4px 5px rgba(0, 0, 0, 0.25)',
+    [theme.breakpoints.down('md')]: {
+      margin: theme.spacing(0),
+      borderRadius: theme.spacing(0),
+    },
   },
 
   [`& .${classes.reachedMoney}`]: {
@@ -68,6 +77,27 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
       color: theme.palette.primary.main,
     },
   },
+  [`& .${classes.buttonContainer}`]: {
+    [theme.breakpoints.down('md')]: {
+      display: 'flex',
+      flexWrap: 'nowrap',
+      marginTop: theme.spacing(2),
+    },
+  },
+  [`& .${classes.sharesContainer}`]: {
+    [theme.breakpoints.down('md')]: {
+      display: 'none',
+    },
+  },
+  [`& .${classes.openButton}`]: {
+    textAlign: 'center',
+    marginBottom: `-${theme.spacing(7)}`,
+    borderRadius: '50%',
+    border: 'none',
+    backgroundColor: '#EEEEEE',
+    minWidth: theme.spacing(5),
+    paddingBottom: 'unset',
+  },
 }))
 
 type Props = {
@@ -89,6 +119,11 @@ export default function InlineDonation({ campaign }: Props) {
     error: donationHistoryError,
     isLoading: isDonationHistoryLoading,
   } = useCampaignDonationHistory(campaign.id)
+  const { mobile } = useMobile()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const detailsShown = isOpen || !mobile
+  console.log('detailsShown', detailsShown)
 
   const [anchorEl, setAnchorEl] = useState<Element | null>(null)
 
@@ -107,15 +142,19 @@ export default function InlineDonation({ campaign }: Props) {
         </Typography>
       </Grid>
       <CampaignProgress raised={reached} target={target} />
-      <Grid display="inline-block" m={3} ml={0}>
-        <Typography className={classes.donorsSharesCount}>{donors}</Typography>
-        <Typography>{t('campaigns:campaign.donors')}</Typography>
-      </Grid>
-      <Grid display="inline-block" m={3} ml={0}>
-        <Typography className={classes.donorsSharesCount}>{0}</Typography>
-        <Typography>{t('campaigns:campaign.shares')}</Typography>
-      </Grid>
-      <Grid container gap={2}>
+      {detailsShown && (
+        <>
+          <Grid display="inline-block" m={3} ml={0}>
+            <Typography className={classes.donorsSharesCount}>{donors}</Typography>
+            <Typography>{t('campaigns:campaign.donors')}</Typography>
+          </Grid>
+          <Grid display="inline-block" m={3} ml={0}>
+            <Typography className={classes.donorsSharesCount}>{0}</Typography>
+            <Typography>{t('campaigns:campaign.shares')}</Typography>
+          </Grid>
+        </>
+      )}
+      <Grid container gap={2} className={classes.buttonContainer}>
         <Button
           fullWidth
           variant="outlined"
@@ -168,14 +207,22 @@ export default function InlineDonation({ campaign }: Props) {
           {t('common:support')}
         </Button>
       </Grid>
-      {donationHistoryError ? (
-        'Error fetching donation history'
-      ) : isDonationHistoryLoading ? (
-        <CircularProgress sx={{ display: 'block', margin: `${theme.spacing(3)} auto` }} />
-      ) : (
-        <DonorsAndDonations donations={donations} />
-      )}
+      {detailsShown &&
+        (donationHistoryError ? (
+          'Error fetching donation history'
+        ) : isDonationHistoryLoading ? (
+          <CircularProgress sx={{ display: 'block', margin: `${theme.spacing(3)} auto` }} />
+        ) : (
+          <DonorsAndDonations donations={donations} />
+        ))}
       {/* <pre>{JSON.stringify(prices, null, 2)}</pre> */}
+      {mobile && (
+        <Grid textAlign="center">
+          <Button variant="text" onClick={() => setIsOpen(!isOpen)} className={classes.openButton}>
+            {isOpen ? <ExpandLessIcon fontSize="large" /> : <ExpandMoreIcon fontSize="large" />}
+          </Button>
+        </Grid>
+      )}
     </StyledGrid>
   )
 }
