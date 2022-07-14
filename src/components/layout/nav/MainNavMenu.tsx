2864665ba65ccdef7402c220a691041655c48551
nradkova <neliradkova@abv.bg>
2022-07-14 16:22:55 +0300

    removed: AdminMenu from MainNavMenu

--- src/components/layout/nav/MainNavMenu.tsx
+++ src/components/layout/nav/MainNavMenu.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { useSession } from 'next-auth/react'
 import { Button, Grid } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 
 import { staticUrls } from 'common/routes'
-import { isAdmin } from 'common/util/roles'
 
 import DonationMenu from './DonationMenu'
 import ProjectMenu from './ProjectMenu'
-import AdminMenu from './AdminMenu'
 import DevelopmentMenu from './DevelopmentMenu'
 
 export default function MainNavMenu({ children }: { children?: React.ReactNode }) {
   const { t } = useTranslation()
-  const { data: session, status } = useSession()
 
   return (
     <Grid container direction="row" wrap="nowrap" alignItems="baseline" spacing={1}>
@@ -26,11 +22,6 @@ export default function MainNavMenu({ children }: { children?: React.ReactNode }
       <Grid item>
         <DevelopmentMenu />
       </Grid>
-      {status === 'authenticated' && isAdmin(session) && (
-        <Grid item>
-          <AdminMenu />
-        </Grid>
-      )}
       <Grid item>
         <Button
           variant="text"
