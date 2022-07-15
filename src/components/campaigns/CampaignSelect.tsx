998d1bb1c3fe57b8f34c4eef0494d60969596a15
borislavstoychev <stoy4ew@gmail.com>
2022-07-15 18:59:00 +0300

    used FormTextField in the CampaignSelect

--- src/components/campaigns/CampaignSelect.tsx
+++ src/components/campaigns/CampaignSelect.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { useField, useFormikContext } from 'formik'
+import { useField } from 'formik'
 import { useTranslation } from 'next-i18next'
 
-import { MenuItem, TextField, TextFieldProps } from '@mui/material'
+import { FormControl, MenuItem, TextFieldProps } from '@mui/material'
 
 import { CampaignResponse } from 'gql/campaigns'
 
-import { translateError } from 'common/form/useForm'
-import { TranslatableField } from 'common/form/validation'
+import FormTextField from 'components/common/form/FormTextField'
 
 type Props = {
   label: string
@@ -16,35 +15,33 @@ type Props = {
 } & TextFieldProps
 
 export default function CampaignSelect({ label, name, campaigns, ...textFieldProps }: Props) {
-  const { t } = useTranslation('transfer')
+  const { t } = useTranslation()
 
   const [field, meta] = useField(name)
-  const { setFieldValue } = useFormikContext()
-
-  const helperText = meta.touched ? translateError(meta.error as TranslatableField, t) : ''
-
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setFieldValue(name, event.target.value)
-  }
 
   return (
-    <TextField
-      {...textFieldProps}
-      {...field}
-      select
-      variant="outlined"
+    <FormControl
       fullWidth
-      onChange={handleChange}
-      label={t(label)}
-      error={Boolean(meta.error) && Boolean(meta.touched)}
-      helperText={helperText}>
-      {[{ id: '', title: '' }, ...(campaigns || [])].map((p) => {
-        return (
-          <MenuItem key={p.id} value={p.id}>
-            {p.title}
+      size="small"
+      variant="outlined"
+      error={Boolean(meta.error) && Boolean(meta.touched)}>
+      <FormTextField
+        select
+        type="text"
+        fullWidth
+        defaultValue=""
+        label={t(label)}
+        {...field}
+        {...textFieldProps}>
+        <MenuItem value="" disabled>
+          {t(label)}
+        </MenuItem>
+        {campaigns?.map((value, index) => (
+          <MenuItem key={index} value={value.id}>
+            {value.title}
           </MenuItem>
-        )
-      })}
-    </TextField>
+        ))}
+      </FormTextField>
+    </FormControl>
   )
 }
