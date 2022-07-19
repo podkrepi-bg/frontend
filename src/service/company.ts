13c6e7a3f210e713b5cb1312e85ce5825808d18c
nradkova <neliradkova@abv.bg>
2022-07-20 00:31:13 +0300

    added: useCreateCompany

--- src/service/company.ts
+++ src/service/company.ts
@@ -2,8 +2,11 @@ import { useSession } from 'next-auth/react'
 
 import { endpoints } from './apiEndpoints'
 import { CompanyResponse } from 'gql/company'
-import { authQueryFnFactory } from './restRequests'
+import { authConfig, authQueryFnFactory } from './restRequests'
 import { useQuery } from 'react-query'
+import { AdminCompanyFormData, AdminCompanyResponse } from 'gql/person'
+import { apiClient } from './apiClient'
+import { AxiosResponse } from 'axios'
 
 export const useCompaniesList = () => {
   const { data: session } = useSession()
@@ -21,4 +24,15 @@ export const useViewCompany = (id: string) => {
     endpoints.company.viewCompany(id).url,
     authQueryFnFactory<CompanyResponse>(session?.accessToken),
   )
+}
+
+export const useCreateCompany = () => {
+  const { data: session } = useSession()
+  return async (data: AdminCompanyFormData) => {
+    return await apiClient.post<AdminCompanyFormData, AxiosResponse<AdminCompanyResponse>>(
+      endpoints.company.createCompany.url,
+      data,
+      authConfig(session?.accessToken),
+    )
+  }
 }
