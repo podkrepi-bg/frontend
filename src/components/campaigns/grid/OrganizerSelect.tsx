40146cf5ee910251341a8e2b2dd553ad322091b2
nradkova <neliradkova@abv.bg>
2022-07-20 00:57:28 +0300

    refactored: OrganizerSelect

--- src/components/campaigns/grid/OrganizerSelect.tsx
+++ src/components/campaigns/grid/OrganizerSelect.tsx
@@ -4,22 +4,26 @@ import { useOrganizersList } from 'common/hooks/organizer'
 import { useField } from 'formik'
 import { useTranslation } from 'react-i18next'
 
-export default function OrganizerSelect({ name = 'organizerId' }) {
+export default function OrganizerSelect({ name = 'organizerId', label = 'campaigns:organizer' }) {
   const { t } = useTranslation()
   const { data } = useOrganizersList()
   const [field, meta] = useField(name)
 
   const helperText = meta.touched ? translateError(meta.error as TranslatableField, t) : ''
+  if (!data) {
+    return null
+  }
+
   return (
     <FormControl
       fullWidth
       size="small"
       variant="outlined"
       error={Boolean(meta.error) && Boolean(meta.touched)}>
-      <InputLabel>{t('campaigns:organizer')}</InputLabel>
-      <Select fullWidth defaultValue="" label={t('campaigns:organizer')} {...field}>
+      <InputLabel>{t(label)}</InputLabel>
+      <Select fullWidth defaultValue="" label={t(label)} {...field}>
         <MenuItem value="" disabled>
-          {t('campaigns:organizer')}
+          {t(label)}
         </MenuItem>
         {data?.map((organizer, index) => (
           <MenuItem key={index} value={organizer.id}>
