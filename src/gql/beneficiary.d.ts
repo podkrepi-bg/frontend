e8f5118b1391d8f48b95d6ea5a3d29d7c14c0e80
nradkova <neliradkova@abv.bg>
2022-07-21 19:41:38 +0300

    removed: organizerId and coordinatorId from types

--- src/gql/beneficiary.d.ts
+++ src/gql/beneficiary.d.ts
@@ -8,24 +8,17 @@ export type BeneficiaryListResponse = {
   person?: Person
   company?: { companyName: string }
   companyId?: string
-  /// Coordinator for this beneficiary
-  coordinatorId?: string
-  /// Organizer for this beneficiary
-  organizerId?: string
   countryCode: string
   cityId: string
   description: string
   publicData?: string
   privateData?: string
   campaigns: []
-  coordinatorRelation?: PersonRelation
   organizerRelation?: PersonRelation
 }
 
 export type ViewBeneficiaryResponse = BeneficiaryListResponse & {
   city: { name: string }
-  coordinator?: { person: Person }
-  organizer?: { person: Person }
   company?: { companyName: string }
 }
 
@@ -33,16 +26,11 @@ export type BeneficiaryFormData = {
   type: BeneficiaryType
   personId?: string
   companyId?: string
-  /// Coordinator for this beneficiary
-  coordinatorId?: string
-  /// Organizer for this beneficiary
-  organizerId?: string
   countryCode: string
   cityId: string
-  description: string
+  description?: string
   publicData?: string
   privateData?: string
   campaigns: []
-  coordinatorRelation?: PersonRelation
   organizerRelation?: PersonRelation
 }
