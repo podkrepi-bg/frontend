a5e9e88af6f10e4fee530a220eac0219cedaec07
borislavstoychev <stoy4ew@gmail.com>
2022-07-27 15:22:11 +0300

    unused import

--- src/components/campaigns/InlineDonation.tsx
+++ src/components/campaigns/InlineDonation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { styled } from '@mui/material/styles'
 import { useTranslation } from 'next-i18next'
 import { CampaignResponse } from 'gql/campaigns'
