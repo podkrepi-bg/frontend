fb65d301289cbaacbcbd7d8b67c6944625d9a991
quantum-grit <quantum-grit@gmail.com>
2022-08-31 15:02:33 +0300

    updated getSession with getServerSession for the remaining getServerSideProps

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,17 @@
 import { GetServerSideProps } from 'next'
-import { getSession } from 'next-auth/react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { routes } from 'common/routes'
 import LoginPage from 'components/auth/login/LoginPage'
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await getSession(ctx)
+  //For getting session on server side the docs recommend using unstable_getServerSession as per
+  //here: https://next-auth.js.org/getting-started/introduction#server-side
+  //the docs say there is noting unstable, it just may change in next versions
+  const session = await unstable_getServerSession(ctx.req, ctx.res, authOptions)
+
   if (session) {
     return {
       redirect: {
