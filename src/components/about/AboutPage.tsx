b330731587bf126564f5c910a705de6c0e5a7b34
ani-kalpachka <ani_kalpachka888@abv.bg>
2022-09-14 22:43:59 +0300

    Replace sections in About us page

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -5,16 +5,15 @@ import ActiveMembersSection from './sections/ActiveMembersSection'
 import SupervisoryBoardSection from './sections/SupervisoryBoardSection'
 import AssociationMembersSection from './sections/AssociationMembersSection'
 import AboutTheTeamSection from './sections/AboutTheTeamSection'
-import PrinciplesThatUniteUs from './sections/PrinciplesThatUniteUs'
 
 export default function AboutPage() {
   const { t } = useTranslation()
   return (
     <Layout title={t('about:about.title')} maxWidth="xl">
       <ManagementBoardSection />
-      <SupervisoryBoardSection />
-      <ActiveMembersSection />
       <AssociationMembersSection />
+      <ActiveMembersSection />
+      <SupervisoryBoardSection />
       <AboutTheTeamSection />
       {/* <PrinciplesThatUniteUs /> */}
     </Layout>
