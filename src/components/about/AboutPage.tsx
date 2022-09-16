34b069b1e7be43ea0d453f15b3ab70659441ef5b
Ani <ani_kalpachka888@abv.bg>
2022-09-16 17:26:46 +0300

    Refactor Pricniples section, minor fixes on About us page (#1023)
    * Refactor Pricniples section, minor fixes on About us page
    
    * Replace sections in About us page
    
    * Add Zhana Borisova to the active members

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -1,9 +1,9 @@
 import Layout from 'components/layout/Layout'
 import { useTranslation } from 'next-i18next'
 import ManagementBoardSection from './sections/ManagementBoardSection'
-import ActiveMembers from './sections/ActiveMembersSection'
+import ActiveMembersSection from './sections/ActiveMembersSection'
 import SupervisoryBoardSection from './sections/SupervisoryBoardSection'
-import AssociationMEmbersSection from './sections/AssociationMembersSection'
+import AssociationMembersSection from './sections/AssociationMembersSection'
 import AboutTheTeamSection from './sections/AboutTheTeamSection'
 
 export default function AboutPage() {
@@ -11,9 +11,9 @@ export default function AboutPage() {
   return (
     <Layout title={t('about:about.title')} maxWidth="xl">
       <ManagementBoardSection />
+      <AssociationMembersSection />
+      <ActiveMembersSection />
       <SupervisoryBoardSection />
-      <ActiveMembers />
-      <AssociationMEmbersSection />
       <AboutTheTeamSection />
       {/* <PrinciplesThatUniteUs /> */}
     </Layout>
--- src/components/about/sections/PrinciplesThatUniteUs.tsx
+++ src/components/about/sections/PrinciplesThatUniteUs.tsx
@@ -1,94 +1,29 @@
 import React from 'react'
 import { useTranslation } from 'next-i18next'
 import { Grid } from '@mui/material'
-
-import Heading from 'components/common/Heading'
-
 import PrincipleCard from './PrincipleCard'
-import AwarenessIcon from '../icons/AwarenessIcon'
-import ExpertiseIcon from '../icons/ExpertiseIcon'
-import PrivacyIcon from '../icons/PrivacyIcon'
-import ProactiveIcon from '../icons/ProactiveIcon'
-import RespectIcon from '../icons/RespectIcon'
-import TransparencyIcon from '../icons/TransparencyIcon'
-import VoluntaryIcon from '../icons/VoluntaryIcon'
+import Heading from 'components/common/Heading'
+import { principlesData } from '../helpers/principlesData'
 
 export default function PrinciplesThatUniteUs() {
-  const { t } = useTranslation()
+  const { t } = useTranslation('about')
 
   return (
     <Grid container spacing={1}>
       <Grid xs={12} item>
-        <Heading
-          id="principles-that-unite-us"
-          variant="h4"
-          component="h1"
-          align="center"
-          sx={(theme) => ({
-            color: theme.palette.primary.dark,
-            pt: theme.spacing(5),
-            pb: theme.spacing(3),
-          })}
-          linkable>
-          {t('about:principlesThatUniteUs.title')}
+        <Heading variant="h4" component="h2" textAlign="center" fontWeight="500" mt={15} mb={8}>
+          {t('principlesThatUniteUs.title')}
         </Heading>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={VoluntaryIcon}
-          heading={t('about:principlesThatUniteUs.voluntary.heading')}
-          content={t('about:principlesThatUniteUs.voluntary.content')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={ProactiveIcon}
-          heading={t('about:principlesThatUniteUs.proactivity.heading')}
-          content={t('about:principlesThatUniteUs.proactivity.content')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={ExpertiseIcon}
-          heading={t('about:principlesThatUniteUs.expertise.heading')}
-          content={t('about:principlesThatUniteUs.expertise.content')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={RespectIcon}
-          heading={t('about:principlesThatUniteUs.respect.heading')}
-          content={t('about:principlesThatUniteUs.respect.content')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={TransparencyIcon}
-          heading={t('about:principlesThatUniteUs.transparency.heading')}
-          content={t('about:principlesThatUniteUs.transparency.content')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={PrivacyIcon}
-          heading={t('about:principlesThatUniteUs.privacy.heading')}
-          content={t('about:principlesThatUniteUs.privacy.content')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={AwarenessIcon}
-          heading={t('about:principlesThatUniteUs.awareness.heading')}
-          content={t('about:principlesThatUniteUs.awareness.content')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PrincipleCard
-          Icon={AwarenessIcon}
-          heading={t('about:principlesThatUniteUs.sharing.heading')}
-          content={t('about:principlesThatUniteUs.sharing.content')}
-        />
-      </Grid>
+      {principlesData.map((principle) => (
+        <Grid key={principle.heading} item xs={12} sm={6}>
+          <PrincipleCard
+            Icon={principle.icon}
+            heading={t(principle.heading)}
+            content={t(principle.content)}
+          />
+        </Grid>
+      ))}
     </Grid>
   )
 }
