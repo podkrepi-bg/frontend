bc1d6bfa4baa66fa3ef7b63f6fece1735d2ff766
quantum-grit <91589884+quantum-grit@users.noreply.github.com>
2022-09-16 17:27:01 +0300

    added token expiration check in admin and profile page (#1025)
    Co-authored-by: quantum-grit <quantum-grit@gmail.com>

--- src/pages/profile/_middleware.ts
+++ src/pages/profile/_middleware.ts
@@ -1,12 +1,10 @@
 // https://next-auth.js.org/configuration/nextjs#middleware
-import { getToken } from 'next-auth/jwt'
 import withAuth from 'next-auth/middleware'
 
 export default withAuth({
   callbacks: {
-    authorized: async ({ req }) => {
-      const session = await getToken({ req })
-      return !!session
+    authorized: async ({ token }) => {
+      return token ? token.accessTokenExpires > Date.now() : false
     },
   },
 })
