aa7e03c8e1a3013a6ab4f1f85b94b44fbf69e4f8
marinovl7 <lachezar.marinov@tum.de>
2022-10-07 11:47:17 +0200

    Fixed hidden scroll bar when dropdown menu from the navbar is opened(desktop)

--- src/components/layout/nav/GenericMenu.tsx
+++ src/components/layout/nav/GenericMenu.tsx
@@ -28,8 +28,8 @@ export default function GenericMenu({ label, children }: Props) {
         {label}
       </Button>
       <Menu
-        disableScrollLock={true}
         keepMounted
+        disableScrollLock={true}
         id="menu-donation"
         anchorEl={anchorEl}
         elevation={6}
