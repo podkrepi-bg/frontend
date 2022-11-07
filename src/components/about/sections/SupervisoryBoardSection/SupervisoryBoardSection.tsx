cc84fad33de6d725bad2401fe9156ec132ca0a05
ani-kalpachka <ani_kalpachka888@abv.bg>
2022-11-07 08:59:41 +0200

    Migrate styles for SupervisoryBoardSection

--- src/components/about/sections/SupervisoryBoardSection/SupervisoryBoardSection.tsx
+++ src/components/about/sections/SupervisoryBoardSection/SupervisoryBoardSection.tsx
@@ -1,111 +1,51 @@
 import { useTranslation } from 'next-i18next'
 
-import { Grid, Theme, Typography, Link } from '@mui/material'
-import { createStyles, makeStyles } from '@mui/styles'
-import { LinkedIn } from '@mui/icons-material'
+import { Grid } from '@mui/material'
 
 import { data } from '../../helpers/supervisoryBoardData'
-import { AboutHeading, Avatar, Description, LinkedInText } from 'components/about/AboutPage.styled'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    supervisoryBoardMembersWrapper: {
-      flexGrow: 1,
-      width: '100%',
-      textAlign: 'center',
-      flexWrap: 'wrap',
-
-      [theme.breakpoints.up('sm')]: {
-        display: 'flex',
-        gap: theme.spacing(3),
-      },
-    },
-    teamMemberWrapper: {
-      width: '100%',
-      backgroundColor: theme.palette.secondary.light,
-      padding: theme.spacing(4),
-      marginBottom: theme.spacing(3),
-      [theme.breakpoints.up('sm')]: {
-        width: '10%',
-        flex: '1 0 40%',
-        marginBottom: theme.spacing(0),
-      },
-      [theme.breakpoints.up('md')]: {
-        marginBottom: theme.spacing(0),
-        flex: '1 0 30%',
-      },
-    },
-    infoWrapper: {
-      dispay: 'block',
-      marginBottom: theme.spacing(4),
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
-        justifyContent: 'space-around',
-        gap: theme.spacing(2),
-        alignItems: 'center',
-      },
-    },
-    name: {
-      fontWeight: 700,
-      marginTop: theme.spacing(2),
-      [theme.breakpoints.up('md')]: {
-        marginTop: 0,
-      },
-    },
-    imageLinkedInWrapper: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    linkedInButton: {
-      display: 'flex',
-      alignItems: 'center',
-      marginTop: theme.spacing(3),
-      alignSelf: 'center',
-      [theme.breakpoints.up('md')]: {
-        alignSelf: 'start',
-      },
-      '&:hover': {
-        '&>svg, &>h6': {
-          textDecoration: 'underline',
-          color: theme.palette.primary.main,
-        },
-      },
-    },
-    LinkedInIcon: {
-      marginLeft: '-4px',
-    },
-  }),
-)
+import {
+  AboutHeading,
+  Avatar,
+  Description,
+  LinkedInButton,
+  LinkedInIcon,
+  LinkedInText,
+} from 'components/about/AboutPage.styled'
+import {
+  SupervisoryBoardMembersWrapper,
+  TeamMemberWrapper,
+  Wrapper,
+  TeamMemberName,
+  NameLinkedInWrapper,
+} from './SupervisoryBoardSection.styled'
 
 export default function SupervisoryBoardSection() {
   const { t } = useTranslation('about')
-  const classes = useStyles()
 
   return (
     <Grid component="section">
       <AboutHeading variant="h4">{t('about.supervisory-board-members')}</AboutHeading>
-      <Grid className={classes.supervisoryBoardMembersWrapper}>
+      <SupervisoryBoardMembersWrapper>
         {data.map((teamMember) => (
-          <Grid key={teamMember.name} className={classes.teamMemberWrapper}>
-            <Grid className={classes.infoWrapper}>
+          <TeamMemberWrapper key={teamMember.name}>
+            <Wrapper>
               <Avatar alt="Team member avatar" src={teamMember.img} width="200px" height="200px" />
-              <Grid className={classes.imageLinkedInWrapper}>
-                <Typography variant="subtitle1" className={classes.name}>
-                  {teamMember.name}
-                </Typography>
-                <Link
+              <NameLinkedInWrapper>
+                <TeamMemberName variant="subtitle1">{teamMember.name}</TeamMemberName>
+                <LinkedInButton
                   href={teamMember.linkedInProfile}
                   target="_blank"
-                  className={classes.linkedInButton}>
-                  <LinkedIn color="action" fontSize="large" className={classes.LinkedInIcon} />
+                  style={{ float: 'left' }}>
+                  <LinkedInIcon color="action" fontSize="large" />
                   <LinkedInText variant="subtitle1">{t('about.linkedIn')}</LinkedInText>
-                </Link>
-              </Grid>
-            </Grid>
+                </LinkedInButton>
+              </NameLinkedInWrapper>
+            </Wrapper>
             <Description variant="body2">{teamMember.description}</Description>
-          </Grid>
+          </TeamMemberWrapper>
         ))}
-      </Grid>
+      </SupervisoryBoardMembersWrapper>
     </Grid>
   )
 }
