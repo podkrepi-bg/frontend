af7f81c15b6058fad2000017f6a29aa0308e52bf
ani-kalpachka <ani_kalpachka888@abv.bg>
2022-11-13 22:03:13 +0200

    Show all campaigns on Campaigns listing page

--- src/components/campaigns/CampaignsList.tsx
+++ src/components/campaigns/CampaignsList.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
-import { Box, Button, Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 
 import { CampaignResponse } from 'gql/campaigns'
 import useMobile from 'common/hooks/useMobile'
 
 import CampaignCard from './CampaignCard'
-import { useMemo, useState } from 'react'
-import { useTranslation } from 'next-i18next'
+import { useMemo } from 'react'
 
 const cardAlignment = (index: number, array: CampaignResponse[]) => {
   if (index === array.length - 1 && array.length % 2 === 1) {
@@ -22,16 +21,10 @@ const cardAlignment = (index: number, array: CampaignResponse[]) => {
 type Props = { campaignToShow: CampaignResponse[] }
 
 export default function CampaignsList({ campaignToShow }: Props) {
-  const { t } = useTranslation()
   const { mobile } = useMobile()
-  const numberOfMinimalShownCampaigns = 6
-  const [all, setAll] = useState<boolean>(false)
   const campaigns = useMemo<CampaignResponse[]>(() => {
-    if (all) {
-      return campaignToShow ?? []
-    }
-    return campaignToShow?.slice(0, numberOfMinimalShownCampaigns) ?? []
-  }, [campaignToShow, all])
+    return campaignToShow ?? []
+  }, [campaignToShow])
 
   return (
     <Grid container justifyContent="center" spacing={2}>
@@ -50,13 +43,6 @@ export default function CampaignsList({ campaignToShow }: Props) {
           </Grid>
         ))}
       </Grid>
-      {campaignToShow && campaignToShow?.length > numberOfMinimalShownCampaigns && (
-        <Grid container justifyContent="center">
-          <Button onClick={() => setAll((prev) => !prev)} variant="outlined" sx={{ mt: 1 }}>
-            {all ? t('campaigns:cta.see-less') : t('campaigns:cta.see-all')}
-          </Button>
-        </Grid>
-      )}
       <Grid>
         <Box sx={{ my: 10 }}>
           {mobile ? (
