56d939246e084905e51246b865a9f0340dc771d1
Boyan Vushkov <bvushkov@gmail.com>
2022-11-18 19:07:22 +0200

    Consistent date format in update birthday modal (#1115)
    * Consistent date format in update birthday modal
    
    * Generic formatting of date in birthday modal

---
+++ src/components/common/form/FormDatePicker.tsx
@@ -0,0 +1,47 @@
+import { TextField } from '@mui/material'
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { format } from 'date-fns'
+import { useField, useFormikContext } from 'formik'
+import { useTranslation } from 'next-i18next'
+
+import { DATE_VALUE_FORMAT, getDateFormat } from 'common/util/date'
+
+/**
+ * MUI date picker to be connected with Formik. Propagates updates to the passed Formik field name
+ * @param name - name of the Formik field to bind
+ * @param label - prompt text
+ * @returns
+ */
+export default function FormDatePicker({ name, label }: { name: string; label: string }) {
+  const [field] = useField(name)
+  const { setFieldValue } = useFormikContext()
+  const { i18n } = useTranslation()
+
+  const dateViewFormat = getDateFormat(i18n.language)
+  const mask = dateViewFormat.replace(new RegExp(/[^./]/g), '_')
+
+  const updateValue = (newValue: Date) => {
+    let formattedValue
+    try {
+      formattedValue = format(newValue, DATE_VALUE_FORMAT)
+    } catch {
+      formattedValue = field.value
+    } finally {
+      setFieldValue(name, formattedValue, true)
+    }
+  }
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        mask={mask}
+        inputFormat={dateViewFormat}
+        label={label}
+        value={field.value}
+        onChange={(newValue) => updateValue(newValue)}
+        renderInput={(params) => <TextField size="small" {...params} />}
+      />
+    </LocalizationProvider>
+  )
+}
