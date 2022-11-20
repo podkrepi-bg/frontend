8d540b6a2909adbc08a34191f746fe301adcb532
ivo <ivailo96g@gmail.com>
2022-11-20 11:32:16 -0500

    [Feature] added See more/less button in the about page

--- src/components/about/sections/ManagementBoardSection/ManagementBoardDescription.tsx
+++ src/components/about/sections/ManagementBoardSection/ManagementBoardDescription.tsx
@@ -1,21 +1,30 @@
 import { Button } from '@mui/material'
 import { Description } from 'components/about/AboutPage.styled'
 import { useTranslation } from 'next-i18next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export function ManagementBoardDescritpion({ description }) {
-  const { t } = useTranslation()
+export function ManagementBoardDescription({ description }) {
+  const { t } = useTranslation('about')
 
   const [show, setShow] = useState(false)
+  const [text, setText] = useState('See more')
+
+  useEffect(() => setText(t('about.see-more')), [])
+
+  function handleClick() {
+    setShow(!show)
+    const buttonText = show ? t('about.see-more') : t('about.see-less')
+    setText(buttonText)
+  }
 
   return (
     <>
       <Description variant="body2" height={show ? 'auto' : '200px'}>
         {description}
       </Description>
-      <div margin="0 auto" width="100%">
-        <Button variant="outlined" onClick={() => setShow(!show)}>
-          {show ? t('campaigns:cta.see-less') : t('campaigns:cta.see-more')}
+      <div>
+        <Button variant="outlined" onClick={handleClick}>
+          {text}
         </Button>
       </div>
     </>
