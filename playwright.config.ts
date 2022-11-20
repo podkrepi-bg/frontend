4d7fa7be866536d2947a80adcb9bab317e609869
Ivan Milchev <ivan@mondoo.com>
2022-11-20 20:32:02 +0100

    record playwright tests
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,6 +40,8 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    /* Record videos for every test but keep only the ones for the failed tests */
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
