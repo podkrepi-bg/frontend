ecf57cc93abd70677d3ebdf23a11d002da6e6d82
ambo <agoranovreff@gmail.com>
2022-11-21 17:13:57 +0200

    remove unused import

--- src/components/common/ExpandableText.tsx
+++ src/components/common/ExpandableText.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'next-i18next'
 import { Box } from '@mui/material'
 
