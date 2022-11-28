7ff4f5224e6567e93aa16494c9f5847c117f8df6
dimitar.nizamov <dimitur2204@gmail.com>
2022-11-28 17:19:18 +0100

    1198 - add popover to the report button

--- src/components/layout/ReportButton.tsx
+++ src/components/layout/ReportButton.tsx
@@ -1,12 +1,42 @@
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { IconButton } from '@mui/material'
+import { IconButton, Popover, Typography } from '@mui/material'
 import { ErrorOutline } from '@mui/icons-material'
 
 export default function ReportButton() {
+  const [anchorEl, setAnchorEl] = useState<Element | null>(null)
   const { t } = useTranslation()
+
+  const handleClick = (event: React.MouseEvent) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'report-a-problem-form' : undefined
   return (
-    <IconButton aria-label={t('common:report')} size="small">
-      <ErrorOutline />
-    </IconButton>
+    <>
+      <IconButton
+        aria-label={t('common:report')}
+        aria-describedby={id}
+        size="small"
+        onClick={handleClick}>
+        <ErrorOutline />
+      </IconButton>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}>
+        <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+      </Popover>
+    </>
   )
 }
