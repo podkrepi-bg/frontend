897b5a48d5905d5116126b0776962ae697bea7c9
kabaivanov <118026471+kabaivanov@users.noreply.github.com>
2022-12-07 14:57:46 +0200

    remove now obsolete Container import
    As directed by Ani, we're getting rid of the Container import since we no longer use a Container component.

--- src/components/layout/Footer/Footer.styled.tsx
+++ src/components/layout/Footer/Footer.styled.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import { lighten } from '@mui/material/styles'
 import { styled } from '@mui/material/styles'
 
