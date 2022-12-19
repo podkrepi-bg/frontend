9026d9e917294a2a724f7b2605fea676a6e386f5
Lyoubomir Katzarov <lyoubomir.katzarov@gmail.com>
2022-12-19 17:21:41 +0100

    1247-Fix main menu donate button routing. (#1263)
    * 1247-Fix main menu donate button routing.
    
    * Update src/components/layout/nav/MainNavMenu.tsx
    
    Co-authored-by: Ilko Kacharov <kachar136@gmail.com>

--- src/components/layout/nav/MainNavMenu.tsx
+++ src/components/layout/nav/MainNavMenu.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Button, Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 
 import { routes } from 'common/routes'
 
 import DonationMenu from './DonationMenu'
 import ProjectMenu from './ProjectMenu'
+import LinkButton from 'components/common/LinkButton'
 
 export default function MainNavMenu({ children }: { children?: React.ReactNode }) {
   const { t } = useTranslation()
@@ -13,13 +14,16 @@ export default function MainNavMenu({ children }: { children?: React.ReactNode }
   return (
     <Grid container direction="row" wrap="nowrap" alignItems="baseline" spacing={4}>
       <Grid item>
-        <Button
+        <LinkButton
           variant="outlined"
+          size="large"
           color="inherit"
-          href={routes.campaigns.index}
-          style={{ borderColor: '#32A9FE' }}>
-          {t('nav.donatе')}
-        </Button>
+          sx={{ borderColor: '#32A9FE' }}
+          href={routes.campaigns.index}>
+          <Typography variant="button" color="#000000DE">
+            {t('nav.donatе')}
+          </Typography>
+        </LinkButton>
       </Grid>
       <Grid item>
         <DonationMenu />
