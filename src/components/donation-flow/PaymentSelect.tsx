060ffb30d64921f59cd8f949e10cca6673911960
dimitar.nizamov <dimitur2204@gmail.com>
2023-01-03 16:02:46 +0100

    1273 - add styles to the radio group

--- src/components/donation-flow/PaymentSelect.tsx
+++ src/components/donation-flow/PaymentSelect.tsx
@@ -1,6 +1,15 @@
 import { FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material'
+import { styled } from '@mui/material/styles'
 import theme from 'common/theme'
 import React from 'react'
+
+export const StyledFormControlLabel = styled(FormControlLabel)(() => ({
+  '&:not(:last-child)': {
+    borderBottom: `1px solid ${theme.borders.dark}`,
+  },
+  padding: theme.spacing(2),
+  margin: 0,
+}))
 
 function PaymentSelect() {
   const [value, setValue] = React.useState('female')
@@ -8,6 +17,7 @@ function PaymentSelect() {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>, value: string) => {
     setValue(event.target.value)
   }
+
   return (
     <FormControl>
       <RadioGroup
@@ -18,10 +28,14 @@ function PaymentSelect() {
         sx={{
           border: `1px solid ${theme.borders.dark}`,
           borderRadius: theme.borders.semiRound,
-          padding: theme.spacing(2),
         }}>
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <StyledFormControlLabel
+          sx={{ borderBottom: `1px solid ${theme.borders.dark}`, padding: theme.spacing(2) }}
+          value="female"
+          control={<Radio />}
+          label="Female"
+        />
+        <StyledFormControlLabel value="male" control={<Radio />} label="Male" />
       </RadioGroup>
     </FormControl>
   )
