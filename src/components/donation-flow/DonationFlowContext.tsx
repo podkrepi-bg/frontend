0db5c83b8e5b0d6d6a7de15b4b1a42183ea4bc2b
dimitar.nizamov <dimitur2204@gmail.com>
2023-01-05 11:24:44 +0100

    payment-process-stripe - add the choose amount from the old donation flow

--- src/components/donation-flow/DonationFlowContext.tsx
+++ src/components/donation-flow/DonationFlowContext.tsx
@@ -1,11 +1,25 @@
 import React, { PropsWithChildren } from 'react'
+import { useRouter } from 'next/router'
+import { useViewCampaign } from 'common/hooks/campaigns'
+import CenteredSpinner from 'components/common/CenteredSpinner'
+import { CampaignResponse } from 'gql/campaigns'
 
-const DonationFlowContext = React.createContext({
-  stripePaymentIntentId: null,
-})
+type DonationContext = {
+  stripePaymentIntentId: string
+  setStripePaymentIntentId: React.Dispatch<React.SetStateAction<string>>
+  campaign: CampaignResponse
+}
+
+export const DonationFlowContext = React.createContext({} as DonationContext)
 
 export const DonationFlowProvider = ({ children }: PropsWithChildren) => {
-  const [stripePaymentIntentId, setStripePaymentIntentId] = React.useState(null)
-  const value = { stripePaymentIntentId, setStripePaymentIntentId }
+  //get the campaign with react-query and pass it to the context
+  const router = useRouter()
+  const slug = String(router.query.slug)
+  const { data, isLoading } = useViewCampaign(slug)
+  const [stripePaymentIntentId, setStripePaymentIntentId] = React.useState('')
+  if (isLoading || !data) return <CenteredSpinner size="2rem" />
+  const { campaign } = data
+  const value = { stripePaymentIntentId, setStripePaymentIntentId, campaign }
   return <DonationFlowContext.Provider value={value}>{children}</DonationFlowContext.Provider>
 }
