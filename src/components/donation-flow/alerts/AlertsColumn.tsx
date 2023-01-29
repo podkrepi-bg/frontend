bec9d21e9963f9181ac5e73ccbdac4d455eb01cd
dimitar.nizamov <dimitur2204@gmail.com>
2023-01-29 22:45:28 +0100

    1286 - add authentication alert info

--- src/components/donation-flow/alerts/AlertsColumn.tsx
+++ src/components/donation-flow/alerts/AlertsColumn.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { useSession } from 'next-auth/react'
+import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material'
 import { useFormikContext } from 'formik'
 import { AnchoredAlert, AnchoredAlertProps } from './AnchoredAlert'
-import { DonationFormDataPaymentOption, DonationFormDataV2 } from '../helpers/types'
+import {
+  DonationFormDataAuthState,
+  DonationFormDataPaymentOption,
+  DonationFormDataV2,
+} from '../helpers/types'
 
 const cardAlertDescription = `Таксата на Stripe се изчислява според района на картодържателя: 1.2% + 0.5лв. за Европейската икономическа зона`
 const bankAlertDescription = `Таксата за транзакция при банков превод зависи от индивидуалните условия на Вашата банка. от (0-4лв)`
@@ -18,7 +23,7 @@ function AlertsColumn({
   sectionsRefArray: React.MutableRefObject<HTMLDivElement | null>[]
 }) {
   const {
-    values: { payment },
+    values: { payment, authentication },
   } = useFormikContext<DonationFormDataV2>()
 
   const alerts: Omit<AnchoredAlertProps, 'sectionRef'>[] = [
@@ -34,7 +39,34 @@ function AlertsColumn({
     },
     {
       color: 'info',
-      children: <Typography>Amount selected is wrong</Typography>,
+      children: (
+        <Box>
+          <Typography>Избирайки да се впишете. ще можете да:</Typography>
+          <List
+            sx={{
+              listStyleType: 'disc',
+              pl: 2,
+              '& .MuiListItem-root': {
+                display: 'list-item',
+              },
+            }}>
+            <ListItem>
+              <ListItemText primary="създадете акаунт като физическо или юридическо лице" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="получите сертификат за дарение" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="правите месечни дарения по избрана кампания" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="получавате и известия за статуса на подкрепени вече кампании" />
+            </ListItem>
+          </List>
+        </Box>
+      ),
+      icon: false,
+      hidden: authentication === DonationFormDataAuthState.AUTHENTICATED,
     },
   ]
 
