d261be51ed9ddf7e4c4a0a0a676a0ce3c4e7bcb8
dimitar.nizamov <dimitur2204@gmail.com>
2023-02-05 19:39:51 +0100

    1297 - update social share list button to use proper links and serialized url

--- src/components/common/SocialShareListButton.tsx
+++ src/components/common/SocialShareListButton.tsx
@@ -3,11 +3,13 @@ import Popover from '@mui/material/Popover'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import { List, ListItemButton } from '@mui/material'
-import { Facebook, LinkedIn, Share } from '@mui/icons-material'
+import { ContentCopy, Facebook, LinkedIn, Share, Twitter } from '@mui/icons-material'
+import { AlertStore } from 'stores/AlertStore'
+import theme from 'common/theme'
 
 export default function SocialShareListButton({ url }: { url: string }) {
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
-
+  const serializedUrl = new URLSearchParams(url).toString()
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget)
   }
@@ -34,20 +36,28 @@ export default function SocialShareListButton({ url }: { url: string }) {
           horizontal: 'left',
         }}>
         <List>
-          <ListItemButton>
+          <ListItemButton
+            onClick={() => {
+              navigator.clipboard.writeText(url)
+              AlertStore.show('Campaign link copied to clipboard', 'success')
+            }}>
+            <Typography>Copy campaign link</Typography>
+            <ContentCopy sx={{ ml: 1, fill: theme.palette.grey[400] }} />
+          </ListItemButton>
+          <ListItemButton href={`https://www.facebook.com/sharer/sharer.php?u=${serializedUrl}`}>
             <Typography>Share on Facebook</Typography>
             <Facebook sx={{ ml: 1, fill: '#4267B2' }} />
           </ListItemButton>
           <ListItemButton
             href={`
-            https://www.linkedin.com/shareArticle?u=https%253A//${url}
+            http://www.linkedin.com/shareArticle?mini=true&url=${serializedUrl}
           `}>
             <Typography>Share on LinkedIn</Typography>
             <LinkedIn sx={{ ml: 1, fill: '#0077b5' }} />
           </ListItemButton>
-          <ListItemButton href={``}>
+          <ListItemButton href={`https://twitter.com/intent/tweet?url=${serializedUrl}`}>
             <Typography>Share on Twitter</Typography>
-            <LinkedIn sx={{ ml: 1, fill: '#0077b5' }} />
+            <Twitter sx={{ ml: 1, fill: '#1DA1F2' }} />
           </ListItemButton>
         </List>
       </Popover>
