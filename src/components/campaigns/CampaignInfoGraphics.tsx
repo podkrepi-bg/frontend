d5edfc6a6957c271a8db8302742883d99553bf3a
ani-kalpachka <ani_kalpachka888@abv.bg>
2023-02-11 13:45:17 +0200

    Update campaignInfoGraphics component

--- src/components/campaigns/CampaignInfoGraphics.tsx
+++ src/components/campaigns/CampaignInfoGraphics.tsx
@@ -14,6 +14,7 @@ const PREFIX = 'CampaignInfoGraphics'
 const classes = {
   infoBlock: `${PREFIX}-infoBlock`,
   title: `${PREFIX}-title`,
+  subtext: `${PREFIX}-subtext`,
   volunteerButton: `${PREFIX}-volunteerButton`,
 }
 
@@ -21,13 +22,30 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
   [`& .${classes.infoBlock}`]: {
     display: 'inline-flex',
     alignItems: 'center',
-    padding: theme.spacing(0, 3, 3, 0),
+    padding: theme.spacing(0, 3, 5, 0),
     alignSelf: 'start',
+
+    '&:last-of-type': {
+      display: 'block',
+      textAlign: 'center',
+    },
   },
 
   [`& .${classes.title}`]: {
     fontWeight: 700,
     fontSize: theme.typography.pxToRem(16),
+    fontFamily: "Montserrat, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+    marginBottom: theme.spacing(1),
+    width: theme.spacing(36),
+
+    '&:last-of-type': {
+      width: '100%',
+    },
+  },
+
+  [`& .${classes.subtext}`]: {
+    fontFamily: 'Lato',
+    width: theme.spacing(38),
   },
 
   [`& .${classes.volunteerButton}`]: {
@@ -36,6 +54,7 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
     letterSpacing: theme.spacing(0.05),
     color: theme.palette.common.black,
     border: `2px solid ${theme.palette.secondary.main}`,
+    fontFamily: 'Lato',
   },
 }))
 
@@ -48,34 +67,40 @@ export default function CampaignInfoGraphics() {
 
   return (
     <StyledGrid container>
-      <Grid item xs={12} md={6} className={classes.infoBlock}>
-        <Image alt="Donation icon" src={donationIconSource} width={189} height={127} />
-        <Grid>
+      <Grid item xs={12} lg={6} className={classes.infoBlock}>
+        <Image alt="Donation icon" src={donationIconSource} width={95} height={100} />
+        <Grid ml={1}>
           <Typography className={classes.title}>
             {t('campaigns:info-graphics.donation-title')}
           </Typography>
-          <Typography> {t('campaigns:info-graphics.donation-text')}</Typography>
+          <Typography className={classes.subtext}>
+            {t('campaigns:info-graphics.donation-text')}
+          </Typography>
         </Grid>
       </Grid>
-      <Grid item xs={12} md={6} className={classes.infoBlock}>
+      <Grid item xs={12} lg={6} className={classes.infoBlock}>
         <Image alt="Transparency icon" src={transparencyIconSource} width={202} height={100} />
-        <Grid>
+        <Grid ml={1}>
           <Typography className={classes.title}>
             {t('campaigns:info-graphics.transparency-title')}
           </Typography>
-          <Typography> {t('campaigns:info-graphics.transparency-text')}</Typography>
+          <Typography className={classes.subtext}>
+            {t('campaigns:info-graphics.transparency-text')}
+          </Typography>
         </Grid>
       </Grid>
-      <Grid item xs={12} md={6} className={classes.infoBlock}>
-        <Image alt="Approval icon" src={approvalIconSource} width={202} height={102} />
-        <Grid>
+      <Grid item xs={12} lg={6} className={classes.infoBlock}>
+        <Image alt="Approval icon" src={approvalIconSource} width={115} height={100} />
+        <Grid ml={1}>
           <Typography className={classes.title}>
             {t('campaigns:info-graphics.approval-title')}
           </Typography>
-          <Typography>{t('campaigns:info-graphics.approval-title')}</Typography>
+          <Typography className={classes.subtext}>
+            {t('campaigns:info-graphics.approval-text')}
+          </Typography>
         </Grid>
       </Grid>
-      <Grid item xs={12} md={6} className={classes.infoBlock}>
+      <Grid item xs={12} lg={6} className={classes.infoBlock}>
         <Grid>
           <Typography className={classes.title}>
             {t('campaigns:info-graphics.become-volunteer-title')}
