6dd45e4fda5f433ea8a6ccd3d3554a9a5a16b836
Ani <ani_kalpachka888@abv.bg>
2023-02-13 11:03:32 +0200

    Update FE of Campaign Details page according to the new design (#1326)
    * Update banner and title styles
    
    * Update campaign's info
    
    * Update operator component, minor fe fixes
    
    * Update feedback and report campaign
    
    * Add CampaignInfoGraphics component
    
    * Update volunteer button
    
    * Update EN translations
    
    * Update campaignInfoGraphics component
    
    * Add responsive styles for CampaignInfoGraphics component
    
    * Change fonts

--- src/components/campaigns/CampaignInfoOperator.tsx
+++ src/components/campaigns/CampaignInfoOperator.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { useTranslation } from 'next-i18next'
 
+import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
+
 import { CampaignResponse } from 'gql/campaigns'
-import { coordinatorCampaignPictureUrl } from 'common/util/campaignImageUrls'
+
 import { Button, Grid, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import EmailIcon from '@mui/icons-material/Email'
 
-const PREFIX = 'CampaignInfoCoordinator'
+import { coordinatorCampaignPictureUrl } from 'common/util/campaignImageUrls'
+
+const PREFIX = 'CampaignInfoOperator'
 
 const classes = {
   infoButtonWrapper: `${PREFIX}-infoButtonWrapper`,
@@ -21,6 +24,7 @@ const classes = {
 const StyledGrid = styled(Grid)(({ theme }) => ({
   [`& .${classes.avatarWrapper}`]: {
     paddingLeft: theme.spacing(2.5),
+
     [theme.breakpoints.up('lg')]: {
       paddingLeft: theme.spacing(0),
     },
@@ -33,10 +37,11 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
   },
 
   [`& .${classes.trustedButton}`]: {
-    color: theme.palette.primary.main,
+    color: theme.palette.common.black,
     textDecoration: 'underline',
-    fontSize: theme.spacing(1.75),
-    padding: 0,
+    fontSize: theme.typography.pxToRem(16),
+    padding: theme.spacing(0, 0.37),
+
     '&:hover': {
       backgroundColor: 'unset',
       textDecoration: 'underline',
@@ -45,6 +50,7 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
 
   [`& .${classes.infoButtonWrapper}`]: {
     justifyContent: 'center',
+
     [theme.breakpoints.up('lg')]: {
       justifyContent: 'start',
     },
@@ -55,26 +61,26 @@ type Props = {
   campaign: CampaignResponse
 }
 
-export default function CampaignInfoCoordinator({ campaign }: Props) {
+export default function CampaignInfoOperator({ campaign }: Props) {
   const { t } = useTranslation()
 
   const coordinatorAvatarSource = coordinatorCampaignPictureUrl(campaign)
 
   return (
-    <StyledGrid container gap={3}>
+    <StyledGrid container gap={3} mb={8}>
       <Grid item className={classes.avatarWrapper} minWidth="max-content" flex={1}>
         <Image
           src={coordinatorAvatarSource}
           //  A11Y TODO: Translate alt text
           alt={`Image of ${campaign.coordinator.person.firstName} ${campaign.coordinator.person.lastName}`}
-          width={100}
-          height={100}
+          width={84}
+          height={84}
           className={classes.personAvatar}
         />
       </Grid>
       <Grid item flex={6}>
         <Typography variant="subtitle2" component="p">
-          <strong>{t('campaigns:campaign.podkrepi-bg-coordinator')}</strong>
+          <strong>{t('campaigns:campaign.podkrepi-bg-operator')}</strong>
         </Typography>
         <Typography variant="subtitle2" component="p">
           {campaign.coordinator.person.firstName} {campaign.coordinator.person.lastName}
@@ -91,7 +97,7 @@ export default function CampaignInfoCoordinator({ campaign }: Props) {
             campaign.title
           }
           className={classes.trustedButton}>
-          {campaign.coordinator.person.email}
+          {t('campaigns:campaign.btn-connect')}
         </Button>
       </Grid>
     </StyledGrid>
