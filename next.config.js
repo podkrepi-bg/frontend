ea33edd2deef979a599ae37d0fd5bb2ced5ed3fe
quantum-grit <91589884+quantum-grit@users.noreply.github.com>
2023-02-16 11:49:47 +0200

    fixing warn modularizeImports (#1349)
    * modularizeImports is official feature after nextjs v13
    
    * upgraded: nextjs from 13.0.5 to 13.1.6
    
    * upgrade next_auth to latest and renaming unstable_getServerSession to getServerSession as per the new version
    
    * updated getServerSession reference docs
    
    ---------
    
    Co-authored-by: quantum-grit <quantum-grit@gmail.com>

--- next.config.js
+++ next.config.js
@@ -61,14 +61,12 @@ const moduleExports = {
       },
     ]
   },
-  experimental: {
-    modularizeImports: {
-      '@mui/material': {
-        transform: '@mui/material/{{member}}',
-      },
-      '@mui/icons-material/?(((\\w*)?/?)*)': {
-        transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
-      },
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/icons-material/?(((\\w*)?/?)*)': {
+      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
     },
   },
 }
