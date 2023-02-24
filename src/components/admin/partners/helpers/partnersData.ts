2c3055a55ca1adaf8ac6db755eb3c268ef8fba23
dzhani ivanov <dzhani.ivanov@gmail.com>
2023-02-24 17:22:50 +0200

    logo images now redirect to partners pages

--- src/components/admin/partners/helpers/partnersData.ts
+++ src/components/admin/partners/helpers/partnersData.ts
@@ -2,11 +2,18 @@ type Partners = {
   title: string
   name: string
   image: string
+  website: string
 }
 
 type TechAndMarketingPartners = {
   title: string
   items: string[]
+  websites: string[]
+}
+
+type Organizations = {
+  name: string
+  website: string
 }
 
 export const partners: Partners[] = [
@@ -14,28 +21,66 @@ export const partners: Partners[] = [
     title: 'partners:sections.hosting',
     name: 'superhosting',
     image: '/img/partners/superhosting.svg',
+    website: 'https://www.superhosting.bg/',
   },
-  { title: 'partners:sections.video', name: 'rinkoff', image: '/img/partners/rinkoff.svg' },
-  { title: 'partners:sections.media', name: 'darik', image: '/img/partners/darik.svg' },
+  {
+    title: 'partners:sections.video',
+    name: 'rinkoff',
+    image: '/img/partners/rinkoff.svg',
+    website: 'https://www.linkedin.com/company/rinkoff/about/',
+  },
+  {
+    title: 'partners:sections.media',
+    name: 'darik',
+    image: '/img/partners/darik.svg',
+    website: 'https://dariknews.bg/',
+  },
 ]
 
 export const techAndMarketingPartners: TechAndMarketingPartners[] = [
   {
     title: 'partners:sections.tech',
     items: ['softuni', 'mentormate', 'eightyEight'],
+    websites: ['https://softuni.bg/', 'https://mentormate.com/', 'https://google.com'],
   },
   {
     title: 'partners:sections.marketing',
     items: ['allChannels', 'brainDonors', 'crossRoads'],
+    websites: [
+      'https://www.all-channels.com/',
+      'https://www.braindonors.agency/',
+      'https://crossroadsbulgaria.com/',
+    ],
   },
 ]
 
-export const organizationLogos: string[] = [
-  'pulsFoundation',
-  'yanika',
-  'parakids',
-  'trotoara',
-  'bear',
-  'visuallyImpairedChildren',
-  'gorata',
+export const organizations: Organizations[] = [
+  {
+    name: 'pulsFoundation',
+    website: 'https://www.google.bg/',
+  },
+  {
+    name: 'yanika',
+    website: 'https://www.yanikabg.com/',
+  },
+  {
+    name: 'parakids',
+    website: 'https://parakids.org/',
+  },
+  {
+    name: 'trotoara',
+    website: 'https://trotoara.com/',
+  },
+  {
+    name: 'bear',
+    website: 'https://plushenomeche.org/',
+  },
+  {
+    name: 'visuallyImpairedChildren',
+    website: 'https://google.bg/',
+  },
+  {
+    name: 'gorata',
+    website: 'https://gorata.bg/',
+  },
 ]
