c21e412b0afb9ee6361223c6a1ddab1714929d91
dimitar.nizamov <dimitur2204@gmail.com>
2023-02-26 17:33:20 +0100

    add stringified error in the 500 view

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -2,5 +2,10 @@ import { NextPageContext } from 'next'
 import NextError from 'next/error'
 
 export default function NotFound(ctx: NextPageContext) {
-  return <NextError statusCode={ctx.err?.statusCode || 500} />
+  return (
+    <>
+      {JSON.stringify(ctx.err)}
+      <NextError statusCode={ctx.err?.statusCode || 500} />
+    </>
+  )
 }
