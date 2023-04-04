b660299e0fd07499af10ecc50e6880e6079e42e4
ani-kalpachka <ani.kalpachka888@abv.bg>
2023-04-04 15:33:22 +0300

    Add new color in theme

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -23,6 +23,7 @@ const colors = {
     main: '#32A9FE',
     mainDark: darken('#32A9FE', 0.2),
     dark: '#294E85',
+    skyBlue: '#B0E5FF',
   },
   yellow: {
     main: '#FFCB57',
@@ -63,6 +64,9 @@ export const themeOptions: ThemeOptions = {
       main: colors.blue.dark,
       light: colors.blue.mainDark,
       dark: darken(colors.blue.dark, 0.2),
+    },
+    text: {
+      primary: colors.blue.skyBlue,
     },
   },
   shape: {
@@ -195,7 +199,7 @@ export const themeOptions: ThemeOptions = {
     button: { textTransform: 'initial' },
   },
 }
-// https://material-ui.com/customization/default-theme/#default-theme
+
 const theme: Theme = createTheme(themeOptions)
 const materialTheme = responsiveFontSizes(theme)
 const podkrepiTheme = {
