558ca3f296f34113da0a081153f4ead7be08353c
Ani <ani_kalpachka888@abv.bg>
2023-04-16 18:05:02 +0300

    Apply the new Homepage design - Active Campaigns section and Completed Camapigns sections (#1401)
    * Update Homepage design - initial changes
    
    * Extract ActiveCampaigns component
    
    * Arrange CampaignCard's grid
    
    * Update ActiveCampaigns section UI
    
    * Finalize ActiveCamapigns section fot desktop
    
    * Finalize ActiveCampaigns section for mobile and tablet
    
    * Update responsive styles for ActiveCamapigns section
    
    * Rearrange ActiveCampaigns, create CompletedCampaigns
    
    * Implement Completed campaigns carousel - initial changes
    
    * Update completed campaigns caroucel
    
    * Fix build error
    
    * Fix build error
    
    * Update styles
    
    * rearrange files
    
    * Update styles
    
    * Add see all button, add linkt to campaign cards
    
    * Update inlined values
    
    * Remove unnecessary props
    
    * Add type to event handler
    
    * Add new color in theme
    
    * Remove unnecessary comment
    
    * Remove unused dependency
    
    * Revert dependency changes
    
    * Update dependencies
    
    ---------
    
    Co-authored-by: ani-kalpachka <ani.kalpachka888@abv.bg>

--- src/components/client/index/IndexPage.tsx
+++ src/components/client/index/IndexPage.tsx
@@ -1,24 +1,24 @@
 import { useTranslation } from 'next-i18next'
+
 import Layout from 'components/client/layout/Layout'
-import Jumbotron from './sections/Jumbotron/Jumbotron'
-import CampaignsSection from './sections/CampaignsSection/CampaignsSection'
+import ActiveCampaignsSection from './sections/ActiveCampaignsSection/ActiveCampaignsSection'
+import CompletedCampaignsSection from './sections/CompletedCampaignsSection/CompletedCampaignsSection'
 import HowWeWorkSection from './sections/HowWeWorkSection/HowWeWorkSection'
 import TeamMembersSection from './sections/TeamMembersSection/TeamMembersSection'
 import JoinPodkrepiBgSection from './sections/JoinPodkrepiBgSection/JoinPodkrepiBgSection'
 import FaqSection from './sections/FaqSection/FaqSection'
 
 export default function IndexPage() {
-  const { t } = useTranslation()
-
+  const { t } = useTranslation('index')
   return (
     <Layout
       maxWidth={false}
       disableOffset
       disableGutters
-      title={t('index:title2')}
-      metaDescription={t('index:metaDescription')}>
-      <Jumbotron />
-      <CampaignsSection />
+      title={t('title')}
+      metaDescription={t('metaDescription')}>
+      <ActiveCampaignsSection />
+      <CompletedCampaignsSection />
       <HowWeWorkSection />
       <TeamMembersSection />
       <JoinPodkrepiBgSection />
