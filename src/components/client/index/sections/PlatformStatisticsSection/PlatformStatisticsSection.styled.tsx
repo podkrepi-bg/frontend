4bf99bac6e441f0fb5995bebd310e75ef69f55b9
ani-kalpachka <ani.kalpachka888@abv.bg>
2023-05-02 10:15:30 +0300

    Update homepage statistics - inital changes

--- src/components/client/index/sections/PlatformStatisticsSection/PlatformStatisticsSection.styled.tsx
+++ src/components/client/index/sections/PlatformStatisticsSection/PlatformStatisticsSection.styled.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Typography, Grid } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
 import theme from 'common/theme'
@@ -6,17 +6,14 @@ import LinkButton from 'components/common/LinkButton'
 
 export const Root = styled('section')(() => ({
   backgroundColor: theme.palette.secondary.light,
-
-  [theme.breakpoints.up('sm')]: {
-    marginTop: theme.spacing(10),
-  },
+  marginTop: theme.spacing(10),
 
   [theme.breakpoints.up('md')]: {
     padding: theme.spacing(0),
   },
 }))
 
-export const SectionGridWrapper = styled('section')(() => ({
+export const SectionGridWrapper = styled(Grid)(() => ({
   display: 'flex',
   margin: '0 auto',
   maxWidth: theme.spacing(162),
@@ -28,9 +25,13 @@ export const SectionGridWrapper = styled('section')(() => ({
 }))
 
 export const Heading = styled(Typography)(() => ({
-  display: 'inline-block',
   color: theme.palette.primary.dark,
-  margin: theme.spacing(2),
+  textAlign: 'center',
+  fontWeight: 500,
+  margin: theme.spacing(2, 0),
+  fontFamily: 'Montserrat, sans-serif',
+  fontSize: theme.typography.pxToRem(35),
+  letterSpacing: '-1px',
 }))
 
 export const Subtitle = styled(Typography)(() => ({
@@ -40,17 +41,39 @@ export const Subtitle = styled(Typography)(() => ({
   margin: theme.spacing(1, 2),
 }))
 
+export const HelpButtonWrapper = styled(Grid)(() => ({
+  display: 'fkex',
+  justifyContent: 'center',
+}))
+
 export const HelpThoseInNeedButton = styled(LinkButton)(() => ({
+  display: 'block',
   fontWeight: 600,
   borderRadius: theme.borders.round,
   backgroundColor: theme.palette.primary.light,
-  minWidth: theme.spacing(45),
-  fontSize: theme.typography.pxToRem(17),
+  minWidth: theme.spacing(3.75),
+  fontSize: theme.typography.pxToRem(15),
+  marginBottom: theme.spacing(5),
+  boxShadow:
+    '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)',
 
-  [theme.breakpoints.up('md')]: {
-    margin: theme.spacing(4),
+  '& span': {
+    display: 'none',
   },
+
   '&:hover': {
     backgroundColor: theme.palette.primary.main,
+    boxShadow:
+      '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)',
+  },
+
+  [theme.breakpoints.up('md')]: {
+    fontSize: theme.typography.pxToRem(17),
+    margin: theme.spacing(4),
+    minWidth: theme.spacing(50),
+
+    '& span': {
+      display: 'inline-block',
+    },
   },
 }))
--- src/components/client/index/sections/PlatformStatisticsSection/PlatformStatisticsSection.tsx
+++ src/components/client/index/sections/PlatformStatisticsSection/PlatformStatisticsSection.tsx
@@ -1,43 +1,45 @@
 import React from 'react'
 import { useTranslation } from 'next-i18next'
+
 import ArrowForwardSharp from '@mui/icons-material/ArrowForwardSharp'
 import { Grid } from '@mui/material'
 
 import { routes } from 'common/routes'
+import StatisticsSection from './StatisticsSection/StatisticsSection'
 
 import {
-  HelpThoseInNeedButton,
   Heading,
+  HelpButtonWrapper,
+  HelpThoseInNeedButton,
   Root,
   SectionGridWrapper,
   Subtitle,
 } from './PlatformStatisticsSection.styled'
-import StatisticsSection from './StatisticsSection/StatisticsSection'
 
 export default function PlatformStatisticsSection() {
   const { t } = useTranslation('index')
 
   return (
     <Root>
-      <Grid component="section" justifyContent="center">
-        <SectionGridWrapper>
-          <Grid
-            sx={(theme) => ({
-              margin: '0 auto',
-              maxWidth: theme.spacing(67),
-            })}>
-            <Heading variant="h4">{t('platform-statistics.heading')}</Heading>
-            <Subtitle variant="subtitle1">{t('platform-statistics.text')}</Subtitle>
+      <SectionGridWrapper>
+        <Grid
+          sx={(theme) => ({
+            margin: '0 auto',
+            maxWidth: theme.spacing(67),
+          })}>
+          <Heading variant="h4">{t('platform-statistics.heading')}</Heading>
+          <Subtitle>{t('platform-statistics.text')}</Subtitle>
+          <HelpButtonWrapper>
             <HelpThoseInNeedButton
               href={routes.campaigns.index}
               variant="contained"
               endIcon={<ArrowForwardSharp />}>
               {t('platform-statistics.donate-to-those-in-need')}
             </HelpThoseInNeedButton>
-          </Grid>
-          <StatisticsSection />
-        </SectionGridWrapper>
-      </Grid>
+          </HelpButtonWrapper>
+        </Grid>
+        <StatisticsSection />
+      </SectionGridWrapper>
     </Root>
   )
 }
