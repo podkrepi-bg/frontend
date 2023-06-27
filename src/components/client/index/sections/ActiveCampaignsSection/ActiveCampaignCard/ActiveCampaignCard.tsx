9ac86714a4888d4c950d3afaee4bab9af8fb580c
Ani <ani_kalpachka888@abv.bg>
2023-06-27 10:43:13 +0300

    Update Campaign cards on Homepage (#1452)
    * Improve campaign cards - initial changes
    
    * Update sum wrapper on campaign card
    
    * Update donate button and layout
    
    * Add progress bar to active campaign cards
    
    * Rearrange components, remove donation button hover effect
    
    * Update card alignment
    
    * Fixed mobile layout
    
    * Update donate button
    
    * Update Donate button
    
    * Update styles
    
    * Add Learn more button
    
    * Fix console errors
    
    * update campaign titles, update spaces, remove unnecessary comments
    
    * Update buttons
    
    * Update card buttons
    
    * Update sum styles
    
    * Extract styles, update inlined values
    
    * Update completed campaigns - initial changes
    
    * Add successful campaig label
    
    * Update successful campaign label
    
    * update translation
    
    * Fix build error
    
    * Fix build error
    
    * Fix test error
    
    * Revert test error
    
    * Revert test function
    
    * Remove card testid
    
    * Remove campaign data-testedit
    
    * Add testids
    
    * Update ActiveCampaignCard.tsx
    
    renamed data-testid to make it unique from /campaigns route
    
    * Update CompletedCampaignsSection.tsx
    
    renamed data-testid to completed-campaign-index to make it unique
    
    ---------
    
    Co-authored-by: ani-kalpachka <ani.kalpachka888@abv.bg>
    Co-authored-by: igoychev <igoychev@gmail.com>

--- src/components/client/index/sections/ActiveCampaignsSection/ActiveCampaignCard/ActiveCampaignCard.tsx
+++ src/components/client/index/sections/ActiveCampaignsSection/ActiveCampaignCard/ActiveCampaignCard.tsx
@@ -1,82 +1,80 @@
 import { useTranslation, i18n } from 'next-i18next'
-import Link from 'next/link'
-
 import { CampaignResponse } from 'gql/campaigns'
 
-import { Favorite } from '@mui/icons-material'
-import { CardActionArea, Grid } from '@mui/material'
+import { Grid } from '@mui/material'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 
 import { routes } from 'common/routes'
-import theme from 'common/theme'
 import { campaignListPictureUrl } from 'common/util/campaignImageUrls'
-import { CampaignState } from 'components/client/campaigns/helpers/campaign.enums'
-
-import { CampaignTitle, Content, SupportNowButton } from './ActiveCampaignCard.styled'
 import { moneyPublic } from 'common/util/money'
+import CampaignProgress from 'components/client/campaigns/CampaignProgress'
+
 import {
-  SumWrapper,
+  CampaignProgressWrapper,
+  CampaignTitle,
+  DonateButton,
+  LearnMoreButton,
+  Root,
   Sum,
-  SumText,
-} from '../../CompletedCampaignsSection/CompletedCampaignsSection.styled'
+  SumNumber,
+  SumWrapper,
+} from './ActiveCampaignCard.styled'
 
 type Props = { campaign: CampaignResponse; index: number }
 
 export default function ActiveCampaignCard({ campaign, index }: Props) {
   const { t } = useTranslation('campaigns')
-  const { state: campaignState, allowDonationOnComplete, slug, title } = campaign
+  const { id, slug, title, summary, targetAmount: target } = campaign
   const campaignImagesUrl = campaignListPictureUrl(campaign)
   const reachedAmount = moneyPublic(campaign.summary.reachedAmount)
   const targetAmount = moneyPublic(campaign.targetAmount)
+  const reached = summary ? summary.reachedAmount : 0
 
   return (
-    <CardActionArea
-      LinkComponent={Link}
-      href={routes.campaigns.viewCampaignBySlug(slug)}
-      data-testid={`campaign-card-${index}`}
-      sx={{
-        background: `linear-gradient(180deg, rgba(81, 81, 81, 0) 50%, rgba(0, 0, 0, 0.78) 80%, #000000 100%), url(${campaignImagesUrl})`,
-        height: '100%',
-        backgroundSize: 'cover',
-        border: `1px solid ${theme.palette.common.white}`,
-
-        '&:hover': {
-          background: `linear-gradient(180deg, rgba(81, 81, 81, 0) -8.88%, rgba(0, 0, 0, 0.78) 80%, #000000 100%), url(${campaignImagesUrl})`,
+    <Root data-testid={`completed-campaign-${index}`}>
+      <Grid
+        sx={{
+          background: `url(${campaignImagesUrl})`,
+          height: '100%',
           backgroundSize: 'cover',
-
-          'a button': {
-            display: 'flex',
-            margin: '0 auto',
-          },
-        },
-      }}>
+        }}
+      />
       <SumWrapper>
-        <SumText>{t('campaign.reached')}</SumText>
         <Sum>
-          {i18n.language === 'bg'
-            ? reachedAmount.split(',')[0] + ' лв.'
-            : reachedAmount.split('.')[0]}
+          {t('campaign.reached')}{' '}
+          <SumNumber>
+            {i18n.language === 'bg'
+              ? reachedAmount.split(',')[0] + ' лв.'
+              : reachedAmount.split('.')[0]}
+          </SumNumber>
         </Sum>
-        <SumText>{t('campaign.from')}</SumText>
-        <Sum>
-          {i18n.language === 'bg'
-            ? targetAmount.split(',')[0] + ' лв.'
-            : targetAmount.split('.')[0]}
+        <Sum style={{ fontWeight: 400 }}>
+          {t('campaign.target')}{' '}
+          <SumNumber>
+            {i18n.language === 'bg'
+              ? targetAmount.split(',')[0] + ' лв.'
+              : targetAmount.split('.')[0]}
+          </SumNumber>
         </Sum>
       </SumWrapper>
-      <Content>
-        <CampaignTitle>{title}</CampaignTitle>
-        <Grid textAlign="center">
-          <SupportNowButton
-            fullWidth
-            href={routes.campaigns.oneTimeDonation(slug)}
-            disabled={campaignState === CampaignState.complete && !allowDonationOnComplete}
-            variant="contained"
-            color="secondary"
-            endIcon={<Favorite color="error" />}>
-            {t('cta.support-now')}
-          </SupportNowButton>
-        </Grid>
-      </Content>
-    </CardActionArea>
+      <CampaignProgressWrapper width={1}>
+        <CampaignProgress campaignId={id} raised={reached} target={target} />
+      </CampaignProgressWrapper>
+      <CampaignTitle>{title}</CampaignTitle>
+      <Grid>
+        <DonateButton
+          fullWidth
+          href={routes.campaigns.oneTimeDonation(slug)}
+          variant="contained"
+          color="secondary">
+          {t('cta.support')}
+        </DonateButton>
+        <LearnMoreButton
+          href={routes.campaigns.viewCampaignBySlug(slug)}
+          endIcon={<ArrowForwardIcon color="warning" fontSize="medium" />}>
+          {t('campaign.learn-more')}
+        </LearnMoreButton>
+      </Grid>
+    </Root>
   )
 }
