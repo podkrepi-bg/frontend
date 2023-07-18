15063a0239d1b07dd7906e05c2a632c3b833754e
Aleksandar Petkov <sashko506@gmail.com>
2023-07-18 09:19:17 +0300

    FirstStep.tsx: Improve otherAmount input field (#1478)
    * FirstStep.tsx: Improve otherAmount input field
    -Added an upper donation limit of BGN 1.500.000(Stripe allows up to $1M for a single transaction, so we are close enough);
    -Fixed conflicting labels with input borders;
    -Fixed Firefox incompatibility, which caused the input field to accept letters and symbols
    
    * Firstep.tsx: Allow for dot-style decimal point
    -Fixes e2e tests
    
    * FirstStep.tsx: Duplicate otherAmount to paypal's input
    Consistency reasons
    
    * FirstStep.tsx: Block float input if limit is reached
    
    * FirstStep.tsx: Move number input  field to its own component
    
    * NumberInputField.tsx: Remove min property
    Breaks the float numbers.
    
    * NumberInputField.tsx: Fix Copy/Paste/Select All for macs

---
+++ src/components/common/form/NumberInputField.tsx
@@ -0,0 +1,101 @@
+import { InputAdornment } from '@mui/material'
+import FormTextField from './FormTextField'
+
+import { isInteger, useField } from 'formik'
+import { useTranslation } from 'next-i18next'
+import { moneyPublic } from 'common/util/money'
+
+type Props = {
+  name?: string
+  limit?: number
+}
+
+export default function NumberInputField({
+  name = 'otherAmount',
+  limit = Number.MAX_SAFE_INTEGER,
+}: Props) {
+  const { t, i18n } = useTranslation('one-time-donation')
+  const [, meta, { setValue, setError }] = useField(name)
+  const decimalSeparator = (1.1).toLocaleString(i18n.lang).charAt(1)
+
+  return (
+    <FormTextField
+      name={name}
+      type="number"
+      value={meta.value === 1 ? '' : meta.value}
+      label={t('first-step.amount')}
+      lang={i18n.language}
+      onKeyDown={(e) => {
+        if (meta.error && e.key !== 'Backspace' && e.key !== 'Delete' && !isInteger(meta.value)) {
+          e.preventDefault()
+          return
+        }
+        if (decimalSeparator !== e.key && (e.key === '.' || e.key === ',')) {
+          e.preventDefault()
+          return
+        }
+
+        if (
+          (e.key.charCodeAt(0) >= 48 && e.key.charCodeAt(0) <= 57) ||
+          (isInteger(meta.value) && e.key === decimalSeparator) ||
+          (e.ctrlKey && e.key === 'v') ||
+          (e.ctrlKey && e.key === 'c') ||
+          (e.ctrlKey && e.key === 'a') ||
+          (e.metaKey && e.key === 'a') ||
+          (e.metaKey && e.key === 'v') ||
+          (e.metaKey && e.key === 'c') ||
+          e.key === 'Backspace' ||
+          e.key === 'Delete' ||
+          e.key === 'ArrowUp' ||
+          e.key === 'ArrowDown' ||
+          e.key === 'ArrowLeft' ||
+          e.key === 'ArrowRight' ||
+          e.code === 'NumpadDecimal'
+        ) {
+          return
+        }
+
+        e.preventDefault()
+      }}
+      onPaste={async (e) => {
+        e.preventDefault()
+        const value = e.clipboardData.getData('Text')
+        const transformedValue: string = value.replace(/ /g, '') as string
+        if (!isNaN(Number(transformedValue))) {
+          setValue(transformedValue)
+          return
+        }
+        setValue(1)
+      }}
+      onChange={(e) => {
+        const amount = e.target.value
+        if (isNaN(Number(amount))) {
+          setError(t('first-step.only-numbers'))
+          return
+        }
+        if (Number(amount) > limit) {
+          setError(`${t('first-step.transaction-limit')} ${moneyPublic(limit, 'BGN', 1)}`)
+          return
+        } else if (Number(amount) < 1) {
+          setValue(1)
+          return
+        }
+
+        setValue(amount)
+      }}
+      InputLabelProps={{ shrink: true }}
+      InputProps={{
+        inputProps: {
+          max: limit,
+          inputMode: 'decimal',
+        },
+        style: { padding: 7 },
+        endAdornment: (
+          <InputAdornment variant="filled" position="end">
+            {t('first-step.BGN')}
+          </InputAdornment>
+        ),
+      }}
+    />
+  )
+}
