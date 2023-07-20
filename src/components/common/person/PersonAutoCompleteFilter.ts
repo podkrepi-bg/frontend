07f1ffc6124fea4a352ecf9750ca9f9e9368b50b
Alexander Petkov <sashko506@gmail.com>
2023-07-20 12:18:19 +0300

    personFilter: Expand filter rules to look for last name

--- src/components/common/person/PersonAutoCompleteFilter.ts
+++ src/components/common/person/PersonAutoCompleteFilter.ts
@@ -2,7 +2,7 @@ import { PersonResponse } from 'gql/person'
 import { FilterOptionsState } from '@mui/material'
 
 /**
- * Custom function to filter person related autocomplete inputs, based on either firstname or email
+ * Custom function to filter person related autocomplete inputs, based on either firstname, lastname or email
  * @param options AutoComplete prop
  * @param state AutoComplete prop
  * @returns
@@ -11,11 +11,13 @@ export const personFilter = (
   options: PersonResponse[],
   state: FilterOptionsState<PersonResponse>,
 ) => {
-  const displayOptions = options.filter(
-    (option) =>
-      option.firstName.toLowerCase().trim().includes(state.inputValue.toLowerCase().trim()) ||
-      option.email.toLowerCase().trim().includes(state.inputValue.toLowerCase().trim()),
-  )
+  const displayOptions = options.filter((option) => {
+    const name = `${option.firstName.toLowerCase()} ${option.lastName.toLowerCase()}`
+    return (
+      name.includes(state.inputValue.toLowerCase()) ||
+      option.email.toLowerCase().includes(state.inputValue.toLowerCase())
+    )
+  })
 
   return displayOptions
 }
