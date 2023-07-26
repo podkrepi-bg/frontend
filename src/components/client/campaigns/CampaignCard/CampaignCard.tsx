021a4a6dd4b9bb9f3a5c4ff000d3ccf457855a2d
ani-kalpachka <ani.kalpachka888@abv.bg>
2023-07-26 15:59:29 +0300

    Remove fragment wrapper

--- src/components/client/campaigns/CampaignCard/CampaignCard.tsx
+++ src/components/client/campaigns/CampaignCard/CampaignCard.tsx
@@ -42,59 +42,57 @@ export default function ActiveCampaignCard({ campaign, index }: Props) {
   const reached = summary ? summary.reachedAmount : 0
 
   return (
-    <>
-      <Root data-testid={`completed-campaign-${index}`}>
-        <Link href={routes.campaigns.viewCampaignBySlug(slug)} sx={{ position: 'relative' }}>
-          <CardMedia
-            component="img"
-            height="100%"
-            image={campaignImagesUrl}
-            alt={title}
-            sx={{
-              maxHeight: theme.spacing(42.5),
+    <Root data-testid={`completed-campaign-${index}`}>
+      <Link href={routes.campaigns.viewCampaignBySlug(slug)} sx={{ position: 'relative' }}>
+        <CardMedia
+          component="img"
+          height="100%"
+          image={campaignImagesUrl}
+          alt={title}
+          sx={{
+            maxHeight: theme.spacing(42.5),
 
-              [theme.breakpoints.up('lg')]: {
-                aspectRatio: '2',
-                height: theme.spacing(22.3),
-                maxHeight: 'inherit',
-              },
+            [theme.breakpoints.up('lg')]: {
+              aspectRatio: '2',
+              height: theme.spacing(22.3),
+              maxHeight: 'inherit',
+            },
 
-              [theme.breakpoints.up(1430)]: {
-                height: theme.spacing(27.9),
-              },
-            }}
-          />
-          <StyledContent>
-            <SumWrapper>
-              <Sum>
-                <SumNumber>
-                  {i18n.language === 'bg'
-                    ? reachedAmount.split(',')[0] + ' лв.'
-                    : reachedAmount.split('.')[0]}
-                </SumNumber>
-              </Sum>
-              <Sum>
-                <SumNumber>
-                  {i18n.language === 'bg'
-                    ? targetAmount.split(',')[0] + ' лв.'
-                    : targetAmount.split('.')[0]}
-                </SumNumber>
-              </Sum>
-            </SumWrapper>
-            <CampaignProgress campaignId={id} raised={reached} target={target} />
-            <CampaignTitle>{title}</CampaignTitle>
-          </StyledContent>
-        </Link>
-        <StyledCardActions disableSpacing>
-          <DonateButton
-            href={routes.campaigns.oneTimeDonation(slug)}
-            disabled={campaignState === CampaignState.complete && !allowDonationOnComplete}
-            variant="contained"
-            color="secondary">
-            {t('cta.support')}
-          </DonateButton>
-        </StyledCardActions>
-      </Root>
-    </>
+            [theme.breakpoints.up(1430)]: {
+              height: theme.spacing(27.9),
+            },
+          }}
+        />
+        <StyledContent>
+          <SumWrapper>
+            <Sum>
+              <SumNumber>
+                {i18n.language === 'bg'
+                  ? reachedAmount.split(',')[0] + ' лв.'
+                  : reachedAmount.split('.')[0]}
+              </SumNumber>
+            </Sum>
+            <Sum>
+              <SumNumber>
+                {i18n.language === 'bg'
+                  ? targetAmount.split(',')[0] + ' лв.'
+                  : targetAmount.split('.')[0]}
+              </SumNumber>
+            </Sum>
+          </SumWrapper>
+          <CampaignProgress campaignId={id} raised={reached} target={target} />
+          <CampaignTitle>{title}</CampaignTitle>
+        </StyledContent>
+      </Link>
+      <StyledCardActions disableSpacing>
+        <DonateButton
+          href={routes.campaigns.oneTimeDonation(slug)}
+          disabled={campaignState === CampaignState.complete && !allowDonationOnComplete}
+          variant="contained"
+          color="secondary">
+          {t('cta.support')}
+        </DonateButton>
+      </StyledCardActions>
+    </Root>
   )
 }
