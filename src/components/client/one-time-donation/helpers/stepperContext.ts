5dca5de272e18c13c4e407f91723c4eef7cc5a87
Alexander Petkov <sashko506@gmail.com>
2023-07-31 19:40:41 +0300

    one-time-donation: Save donation steps inside sessionStorage Saves the selected values from formik inside sessionStorage. The values are saved until either step 3 of donation has been reached, or until the browser tab has been closed.

--- src/components/client/one-time-donation/helpers/stepperContext.ts
+++ src/components/client/one-time-donation/helpers/stepperContext.ts
@@ -1,9 +1,12 @@
 import { createContext } from 'react'
 import { CampaignResponse } from 'gql/campaigns'
+import { OneTimeDonation } from 'gql/donations'
 
 type Steps = {
   step: number
   setStep: React.Dispatch<React.SetStateAction<number>>
   campaign: CampaignResponse
+  updateDonationSession: (value: OneTimeDonation, step: number) => void
+  clearDonationSession: () => void
 }
 export const StepsContext = createContext<Steps>({} as Steps)
