73462e1933f54679ef871f72aac7daee64fc33ac
Bogsana <bogomil_cvetkov@abv.bg>
2023-08-09 08:58:12 +0300

    fix broken import

--- src/components/client/layout/Footer/SubscribeBtn.tsx
+++ src/components/client/layout/Footer/SubscribeBtn.tsx
@@ -1,4 +1,4 @@
-import { , Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { SubscribeLinkWrapper } from './Footer.styled'
 import { useState } from 'react'
