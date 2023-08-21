80b1964c8b61716c08f00ec03ab1c92964f026db
Ani <ani_kalpachka888@abv.bg>
2023-08-21 17:51:25 +0300

    Add Beneficiary to campaign details (#1563)
    * Add beneficiary to campaign details page - inital changes
    
    * Update beneficiary info
    
    * Update organizer info
    
    * Update responsive styles
    
    * Update Campaign info status
    
    * Update responsive styles
    
    * Rearrange components
    
    * Update beneficiary and organizer wrapper
    
    * Update campaign info status
    
    ---------
    
    Co-authored-by: ani-kalpachka <ani.kalpachka888@abv.bg>

---
+++ src/components/client/campaigns/CampaignInfo/CampaignInfoBeneficiary.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { CampaignResponse } from 'gql/campaigns'
+import { useTranslation } from 'next-i18next'
+
+import { Stack, Typography } from '@mui/material'
+
+import { beneficiaryCampaignPictureUrl } from 'common/util/campaignImageUrls'
+import { BeneficiaryType } from 'components/admin/beneficiary/BeneficiaryTypes'
+
+import { Avatar, BeneficiaryOrganizerRoot, Label } from './CampaignInfo.styled'
+
+type Props = {
+  campaign: CampaignResponse
+}
+
+export default function CampaignInfoBeneficiary({ campaign }: Props) {
+  const beneficiaryAvatarSource = beneficiaryCampaignPictureUrl(campaign)
+  const { t } = useTranslation('campaigns')
+
+  return (
+    <BeneficiaryOrganizerRoot>
+      <Stack direction="column">
+        <Label variant="body2">{t('campaign.beneficiary.label')}</Label>
+        <Typography variant="subtitle2" component="p">
+          {campaign.beneficiary.type === BeneficiaryType.individual
+            ? campaign.beneficiary.person?.firstName + ' ' + campaign.beneficiary.person?.lastName
+            : campaign.beneficiary.company.companyName}
+        </Typography>
+      </Stack>
+      <Avatar
+        src={beneficiaryAvatarSource}
+        alt={`${t('campaign.image-of')} ${
+          campaign.beneficiary.type === BeneficiaryType.individual
+            ? campaign.beneficiary.person?.firstName + ' ' + campaign.beneficiary.person?.lastName
+            : campaign.beneficiary.company.companyName
+        }`}
+        width={100}
+        height={100}
+      />
+    </BeneficiaryOrganizerRoot>
+  )
+}
---
+++ src/components/client/campaigns/CampaignInfo/CampaignInfoStatus.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { CampaignResponse } from 'gql/campaigns'
+import { useTranslation } from 'next-i18next'
+
+import { bg, enUS } from 'date-fns/locale'
+import { Grid } from '@mui/material'
+import { Assessment } from '@mui/icons-material'
+
+import { getExactDate } from 'common/util/date'
+
+import {
+  StatusText,
+  StatusLabel,
+  RowWrapper,
+  InfoStatusWrapper,
+  ExpensesButton,
+} from './CampaignInfo.styled'
+
+type Props = {
+  campaign: CampaignResponse
+  showExpensesLink: boolean
+}
+
+export default function CampaignInfoStatus({ campaign, showExpensesLink }: Props) {
+  const { t, i18n } = useTranslation()
+  const locale = i18n.language == 'bg' ? bg : enUS
+
+  return (
+    <Grid>
+      {showExpensesLink && (
+        <ExpensesButton startIcon={<Assessment />} href={'#expenses'}>
+          {t('campaigns:campaign.financial-report')}
+        </ExpensesButton>
+      )}
+      <InfoStatusWrapper>
+        <Grid item xs={12} md={6}>
+          <StatusLabel>{campaign.campaignType.name}</StatusLabel>
+          <RowWrapper>
+            <StatusLabel variant="body2">{t('campaigns:campaign.status')}</StatusLabel>
+            <StatusText>{t(`campaigns:campaign-status.${campaign.state}`)}</StatusText>
+          </RowWrapper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <RowWrapper>
+            <StatusLabel>{t('campaigns:campaign.start-date')}</StatusLabel>
+            <StatusText>{getExactDate(campaign.startDate, locale)}</StatusText>
+          </RowWrapper>
+          <RowWrapper>
+            <StatusLabel> {t('campaigns:campaign.end-date')}</StatusLabel>
+            <StatusText>
+              {campaign.endDate
+                ? getExactDate(campaign.endDate, locale)
+                : t('campaigns:campaign.indefinite')}
+            </StatusText>
+          </RowWrapper>
+        </Grid>
+      </InfoStatusWrapper>
+    </Grid>
+  )
+}
