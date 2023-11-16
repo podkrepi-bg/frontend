d6e2d8c3e23bc7339c6bb403bfa0838cbcbd7b63
Anton Angelov <123360440+tongo-angelov@users.noreply.github.com>
2023-11-16 11:52:39 +0200

    added campaign donation chart (#1667)

---
+++ src/components/client/campaigns/CampaignStatistics/HourlyDonationsChart.tsx
@@ -0,0 +1,111 @@
+import React from 'react'
+
+import { useTranslation } from 'next-i18next'
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip as ChartTooltip,
+  Filler,
+  Legend,
+  ChartOptions,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+
+import { useCampaignHourlyDonations } from 'common/hooks/campaigns'
+import { Box, Stack, Typography } from '@mui/material'
+import { CampaignHourlyDonations } from 'gql/campaigns'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  ChartTooltip,
+  Filler,
+  Legend,
+  ChartDataLabels,
+)
+
+ChartTooltip.positioners.cursor = function (chartElements, coordinates) {
+  return coordinates
+}
+
+type Props = {
+  campaignId: string
+}
+
+export default function HourlyDonationsChart({ campaignId }: Props) {
+  const { t } = useTranslation()
+  const { data: statistics } = useCampaignHourlyDonations(campaignId)
+  if (!statistics?.length) return <div />
+
+  const finalData: CampaignHourlyDonations[] = []
+
+  let lastHour = 0
+
+  statistics.forEach((period) => {
+    while (lastHour < period.hour) {
+      finalData.push({ hour: lastHour, count: 0 })
+      lastHour += 1
+    }
+    finalData.push(period)
+    lastHour += 1
+  })
+
+  while (lastHour < 24) {
+    finalData.push({ hour: lastHour, count: 0 })
+    lastHour += 1
+  }
+
+  const options: ChartOptions<'bar'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        displayColors: false,
+        position: 'cursor',
+      },
+      datalabels: {
+        formatter: function (value: number) {
+          return value ? value : ''
+        },
+        display: true,
+        color: 'black',
+      },
+    },
+  }
+  const data = {
+    labels: finalData.map((donation) =>
+      new Date(1000 * 60 * 60 * donation.hour).toISOString().slice(11, 16),
+    ),
+    datasets: [
+      {
+        fill: true,
+        label: t('campaigns:statistics.count'),
+        data: finalData.map((donation) => donation.count),
+        borderWidth: 2,
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  }
+
+  return (
+    <Box>
+      <Stack style={{ marginBottom: '10px' }}>
+        <Typography variant="h6">{t('campaigns:statistics.hourlyTitle')}</Typography>
+        <Typography variant="caption">{t('campaigns:statistics.hourlyDesc')}</Typography>
+      </Stack>
+      <Bar options={options} data={data} />
+    </Box>
+  )
+}
---
+++ src/components/client/campaigns/StatisticsPage.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import { Container, Stack, Typography } from '@mui/material'
+import { useTranslation } from 'next-i18next'
+
+import { useViewCampaign } from 'common/hooks/campaigns'
+import Layout from 'components/client/layout/Layout'
+import CenteredSpinner from 'components/common/CenteredSpinner'
+import NotFoundPage from 'pages/404'
+import CumulativeDonationsChart from './CampaignStatistics/CumulativeDonationsChart'
+import GroupedDonationsChart from './CampaignStatistics/GroupedDonationsChart'
+import UniqueDonationsChart from './CampaignStatistics/UniqueDonationsChart'
+import HourlyDonationsChart from './CampaignStatistics/HourlyDonationsChart'
+import Link from 'components/common/Link'
+import { routes } from 'common/routes'
+
+type Props = { slug: string }
+
+export default function StatisticsPage({ slug }: Props) {
+  const { t } = useTranslation()
+  const { data: campaignResponse, isLoading, isError } = useViewCampaign(slug)
+  if (isLoading || !campaignResponse)
+    return (
+      <>
+        {/* {isLoading && <CenteredSpinner size={'2rem'} />} */}
+        {isError && <NotFoundPage />}
+      </>
+    )
+
+  const campaign = campaignResponse.campaign
+  const campaignTitle = campaign.title
+  const hasDonations = !!campaign.summary.reachedAmount
+
+  return (
+    <Layout maxWidth={false} metaTitle={campaignTitle} metaDescription={campaignTitle}>
+      <Container maxWidth="lg">
+        <Link href={routes.campaigns.viewCampaignBySlug(slug)}>
+          <Typography>{t('campaigns:statistics.backButton')}</Typography>
+        </Link>
+        <Stack style={{ marginBottom: '30px' }}>
+          <Typography variant="h4" component="h1">
+            {campaignTitle}
+          </Typography>
+          <Typography variant="caption">{t('campaigns:statistics.cachedMessage')}</Typography>
+        </Stack>
+        {hasDonations && (
+          <Stack style={{ gap: 50 }}>
+            <CumulativeDonationsChart
+              campaignId={campaign.id}
+              startDate={campaign.startDate}
+              endDate={campaign.endDate}
+              targetAmount={campaign.targetAmount}
+            />
+            <GroupedDonationsChart
+              campaignId={campaign.id}
+              startDate={campaign.startDate}
+              endDate={campaign.endDate}
+            />
+            <UniqueDonationsChart campaignId={campaign.id} />
+            <HourlyDonationsChart campaignId={campaign.id} />
+          </Stack>
+        )}
+      </Container>
+    </Layout>
+  )
+}
