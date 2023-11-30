4e0381a5110deac6381cc5313dd8e07003c679d7
Alexander Petkov <sashko506@gmail.com>
2023-11-30 19:08:14 +0200

    Remove legacy hotjar implementation Hotjar is now loaded through the GTM

--- src/components/client/index/IndexPage.tsx
+++ src/components/client/index/IndexPage.tsx
@@ -10,9 +10,6 @@ import PartnersSection from './sections/PartnersSection/PartnersSection'
 import TeamMembersSection from './sections/TeamMembersSection/TeamMembersSection'
 import JoinPodkrepiBgSection from './sections/JoinPodkrepiBgSection/JoinPodkrepiBgSection'
 import FaqSection from './sections/FaqSection/FaqSection'
-import dynamic from 'next/dynamic'
-
-const HotJar = dynamic(() => import('common/hotjar/HotJar'), { ssr: false })
 
 export default function IndexPage() {
   const { t } = useTranslation('index')
@@ -23,7 +20,6 @@ export default function IndexPage() {
       disableGutters
       title={t('title')}
       metaDescription={t('metaDescription')}>
-      <HotJar />
       <ActiveCampaignsSection />
       <CompletedCampaignsSection />
       <PlatformStatisticsSection />
