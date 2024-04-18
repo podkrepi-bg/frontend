d3de96fa6174699cdaad46322d2aa015773b5f8b
Alexander Petkov <sashko506@gmail.com>
2024-04-18 20:41:35 +0300

    build: Drop react-i18next We use next-i18next already

--- src/components/client/auth/profile/AffiliateProgramTab.tsx
+++ src/components/client/auth/profile/AffiliateProgramTab.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, CircularProgress, Link, TableBody, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'next-i18next'
 import ProfileTab from './ProfileTab'
 import { ProfileTabs } from './tabs'
 import {
