cb28fcbae22189792f233faa3be420ae926c7c80
Teodora Zhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-24 14:22:01 +0300

    feat: add remark in the stepper (#1861)

--- src/components/client/campaign-application/CampaignApplicationForm.tsx
+++ src/components/client/campaign-application/CampaignApplicationForm.tsx
@@ -10,6 +10,7 @@ import CampaignApplicationOrganizer from './steps/CampaignApplicationOrganizer'
 import CampaignApplicationDetails from './steps/CampaignApplicationDetails'
 import CampaignApplication from './steps/CampaignApplication'
 import CampaignApplicationFormActions from './CampaignApplicationFormActions'
+import CampaignApplicationRemark from './CampaignApplicationRemark'
 import stepsHandler from './helpers/stepsHandler'
 
 import {
@@ -48,22 +49,29 @@ export default function CampaignApplicationForm() {
   }, [])
 
   return (
-    <GenericForm<Record<string, string>> onSubmit={handleSubmit} initialValues={initialValues}>
-      <StyledCampaignApplicationStepper activeStep={activeStep} connector={<StyledStepConnector />}>
-        {steps.map((step) => (
-          <StyledCampaignApplicationStep key={step.title}>
-            <StepLabel StepIconComponent={CampaignApplicationStepperIcon} />
-          </StyledCampaignApplicationStep>
-        ))}
-      </StyledCampaignApplicationStepper>
-      <ActionsContainer container spacing={5}>
-        <Grid container item xs={12}>
-          {activeStep < steps.length && steps[activeStep].component}
-        </Grid>
-        <Grid container item spacing={3}>
-          <CampaignApplicationFormActions activeStep={activeStep} onBack={handleBack} />
-        </Grid>
-      </ActionsContainer>
-    </GenericForm>
+    <>
+      <GenericForm<Record<string, string>> onSubmit={handleSubmit} initialValues={initialValues}>
+        <StyledCampaignApplicationStepper
+          activeStep={activeStep}
+          connector={<StyledStepConnector />}>
+          {steps.map((step) => (
+            <StyledCampaignApplicationStep key={step.title}>
+              <StepLabel StepIconComponent={CampaignApplicationStepperIcon} />
+            </StyledCampaignApplicationStep>
+          ))}
+        </StyledCampaignApplicationStepper>
+        <ActionsContainer container spacing={5}>
+          <Grid container item xs={12}>
+            {activeStep < steps.length && steps[activeStep].component}
+          </Grid>
+          <Grid container item spacing={3}>
+            <CampaignApplicationFormActions activeStep={activeStep} onBack={handleBack} />
+          </Grid>
+        </ActionsContainer>
+      </GenericForm>
+      {(activeStep === Steps.ORGANIZER || activeStep === Steps.CAMPAIGN) && (
+        <CampaignApplicationRemark />
+      )}
+    </>
   )
 }
