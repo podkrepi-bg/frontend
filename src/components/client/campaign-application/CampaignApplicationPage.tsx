9a5fa5ab9b05b00ca909c9e94ee27590bd57071c
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-07-03 17:21:04 +0300

    feat: prefill campaign application organizer form values from current user

--- src/components/client/campaign-application/CampaignApplicationPage.tsx
+++ src/components/client/campaign-application/CampaignApplicationPage.tsx
@@ -1,10 +1,24 @@
+import { CircularProgress, Grid } from '@mui/material'
+import { useSession } from 'next-auth/react'
+import { useViewPersonByKeylockId } from 'common/hooks/person'
 import Layout from '../layout/Layout'
 import CampaignApplicationForm from './CampaignApplicationForm'
 
 export default function CampaignApplicationPage() {
+  const { data: session } = useSession()
+  const { data: person, isLoading } = useViewPersonByKeylockId(session?.user?.sub as string)
+
+  if (isLoading) {
+    return (
+      <Grid container justifyContent="center" alignContent="center" sx={{ height: '100vh' }}>
+        <CircularProgress />
+      </Grid>
+    )
+  }
+
   return (
     <Layout maxWidth="md" sx={{ paddingInline: 5 }}>
-      <CampaignApplicationForm />
+      {person && <CampaignApplicationForm person={person} />}
     </Layout>
   )
 }
