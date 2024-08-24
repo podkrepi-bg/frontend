a04b28dd922bcf9d8855cb54c9524ff2ccc2643c
ani-kalpachka <ani_kalpacka888@gmail.com>
2024-08-24 18:50:07 +0300

    Remove unused code, update image

--- src/components/client/about/AboutPage.tsx
+++ src/components/client/about/AboutPage.tsx
@@ -17,7 +17,6 @@ export default function AboutPage() {
       <ActiveMembersSection />
       <SupervisoryBoardSection />
       <AboutTheTeamSection />
-      {/* <PrinciplesThatUniteUs /> */}
     </Layout>
   )
 }
