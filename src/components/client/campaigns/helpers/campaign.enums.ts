4a736268f2c3a9c6a234879d2eee441e73d04814
Aleksandar <aleksandar.petkov@lab08.com>
2024-09-03 10:50:54 +0300

    feat: Add new campaign statuses

--- src/components/client/campaigns/helpers/campaign.enums.ts
+++ src/components/client/campaigns/helpers/campaign.enums.ts
@@ -1,15 +1,11 @@
 export enum CampaignState {
-  initial = 'initial',
   draft = 'draft',
-  pendingvalidation = 'pending_validation',
-  approved = 'approved',
-  rejected = 'rejected',
   active = 'active',
-  activependingvalidation = 'active_pending_validation',
+  partially_financed = 'partially_financed',
   suspended = 'suspended',
   complete = 'complete',
-  disabled = 'disabled',
-  error = 'error',
+  blocked = 'blocked',
+  paused = 'paused',
   deleted = 'deleted',
 }
 
