c3456ade9b206092cf87dccd046e07bd5c290bc6
Viktor Stefanov <victorystefanov@gmail.com>
2024-09-19 18:51:25 +0300

    edit formatDate fn to return same date format for both en and bg language

--- src/common/util/date.ts
+++ src/common/util/date.ts
@@ -5,10 +5,13 @@ export const formatDate = 'dd MMM yyyy'
 export const formatDatetime = 'dd MMM yyyy HH:mm:ss'
 
 export const formatDateString = (dateString: string | Date, language?: string) => {
-  if (language) {
-    return Intl.DateTimeFormat(language).format(new Date(dateString))
-  }
-  return new Date(dateString).toLocaleDateString()
+  const date = new Date(dateString)
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+
+  return `${day}.${month}.${year}`
 }
 
 const matchLocale = (language?: string): Locale => {
