50595ea1bb4e39bdb983c9d79d2133675afb6fb4
Aleksandar Petkov <aleksandar.petkov@lab08.com>
2025-08-07 01:26:31 +0300

    chore: Fix alignment

--- src/components/client/index/sections/ActiveCampaignsSection/ActiveCampaignCard/ActiveCampaignCard.tsx
+++ src/components/client/index/sections/ActiveCampaignsSection/ActiveCampaignCard/ActiveCampaignCard.tsx
@@ -43,7 +43,7 @@ export default function ActiveCampaignCard({ campaign, index }: Props) {
             width: '100%',
             aspectRatio: 1.5,
             [theme.breakpoints.up('lg')]: {
-              maxHeight: index === 0 ? theme.spacing(71.375) : theme.spacing(27.65),
+              maxHeight: index === 0 ? theme.spacing(71.72) : theme.spacing(27.65),
               aspectRatio: index === 0 ? 0.9 : 1,
             },
           }}>
