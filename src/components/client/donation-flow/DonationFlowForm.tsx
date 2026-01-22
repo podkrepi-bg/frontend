c1f181fe2a616d6246505936e1f26aa732bfbb44
Aleksandar Petkov <aleksandar.petkov@lab08.com>
2026-01-22 22:44:39 +0200

    fix: Always send personId when logged in

--- src/components/client/donation-flow/DonationFlowForm.tsx
+++ src/components/client/donation-flow/DonationFlowForm.tsx
@@ -194,7 +194,7 @@ export function DonationFlowForm() {
                 return_url: `${window.location.origin}/${routes.campaigns.donationStatus(
                   campaign.slug,
                 )}`,
-                personId: !values.isAnonymous && session?.user && person?.id ? person.id : null,
+                personId: session?.user && person?.id ? person.id : null,
               },
             },
           })
