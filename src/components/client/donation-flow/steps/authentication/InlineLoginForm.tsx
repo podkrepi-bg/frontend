a7fbeefc4534e8be318061df2eaa7af59ec47c6e
Aleksandar Petkov <sashko506@gmail.com>
2026-04-16 01:45:24 +0300

    fix(donation): Await user data to refetch after login/register (#2153)

--- src/components/client/donation-flow/steps/authentication/InlineLoginForm.tsx
+++ src/components/client/donation-flow/steps/authentication/InlineLoginForm.tsx
@@ -59,7 +59,7 @@ function InlineLoginForm() {
       }
       if (resp?.ok) {
         // Refetch person data so personId is available for subscription metadata
-        await queryClient.invalidateQueries([endpoints.account.me.url])
+        await queryClient.refetchQueries([endpoints.account.me.url])
         setLoading(false)
         setFieldValue('isAnonymous', false)
         AlertStore.show(t('auth:alerts.welcome'), 'success')
